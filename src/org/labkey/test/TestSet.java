85a5e1c5d0a5e04abb7268df4bd04411ed134cd7
Josh Eckels <jeckels@labkey.com>
2008-08-29 17:43:57 +0000

    6467 : pathmapping for the enterprise pipeline on the webserver is case sensitive 6466 : property names in enterprise pipeline spring configuration files do not match between locations 6403 : upgrade script for pipeline module fails on staging. 6402 : experiment SQL upgrade script took 14minutes to complete on staging 6389 : Need some way of synchronizing running jobs with pipeline.StatusFiles 6102 : Unnecessary warning in create run group 6061 : Cluster job handling at server restart (reliability)
    Also, start copying native binaries into <LABKEY_ROOT>/build/deploy/bin. Point to that location for the pipeline tools directory if we aren't pointing at a different directory that actually exists.
    
    Try reenabling the PipelineBVT again.
    
    SVN r9408 |2008-08-29 17:43:57 +0000

--- src/org/labkey/test/TestSet.java
+++ src/org/labkey/test/TestSet.java
@@ -48,7 +48,7 @@ public enum TestSet
         SampleSetTest.class,
         MS2BvtTest.class,
         MS1Bvt.class,
-//        PipelineBvtTest.class,
+        PipelineBvtTest.class,
         CaBigTest.class,
         FileContentTest.class,
         DataRegionTest.class,
