a56e35eb592ac1e8ff5c0120bff010d8dc981c6e
Adam Rauch <adam@labkey.com>
2009-01-19 03:09:54 +0000

    Log selectTreeItem() path in server & browser to help track down BVT issue.
    SVN r10110 |2009-01-19 03:09:54 +0000

--- src/org/labkey/test/BaseFlowTest.java
+++ src/org/labkey/test/BaseFlowTest.java
@@ -151,10 +151,12 @@ abstract public class BaseFlowTest extends BaseSeleniumWebTest
 
     protected void selectTreeItem(String treeCmpId, String path)
     {
+        log("selectTreeItem path: " + path);
         String result = selenium.getEval(
                 "var ext = selenium.browserbot.getCurrentWindow().Ext;\n" +
                 "var tree = ext.getCmp('" + treeCmpId + "');\n" +
                 "tree.selectPath('/<root>' + '"+ path + "', 'text');\n" +
+                "selenium.browserbot.getCurrentWindow().document.getElementById('labkey-main-title').innerHTML='" + path + "';\n" +
                 "\"OK\"");
         assertEquals("OK", result);
         sleep(500);
