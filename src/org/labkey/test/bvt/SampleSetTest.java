60b295ce51964eecaa73c057cd6126260aef48b4
Josh Eckels <jeckels@labkey.com>
2009-03-27 23:37:08 +0000

    Tests for 9.1 sample set parenting features
    SVN r10733 |2009-03-27 23:37:08 +0000

--- src/org/labkey/test/bvt/SampleSetTest.java
+++ src/org/labkey/test/bvt/SampleSetTest.java
@@ -28,8 +28,24 @@ public class SampleSetTest extends BaseSeleniumWebTest
     private static final String FOLDER_NAME = "SampleSetTestFolder";
     private static final String PROJECT_SAMPLE_SET_NAME = "ProjectSampleSet";
     private static final String FOLDER_SAMPLE_SET_NAME = "FolderSampleSet";
+    private static final String FOLDER_CHILDREN_SAMPLE_SET_NAME = "FolderChildrenSampleSet";
+    private static final String FOLDER_GRANDCHILDREN_SAMPLE_SET_NAME = "FolderGrandchildrenSampleSet";
 
     protected static final String PIPELINE_PATH = "/sampledata/xarfiles/expVerify";
+    private static final String AMBIGUOUS_CHILD_SAMPLE_SET_TSV = "Name\tParent\tOtherProp\n" +
+            "SampleSetBVTChildA\tSampleSetBVT11\t1.1\n" +
+            "SampleSetBVTChildB\tSampleSetBVT4\t2.2\n";
+
+    private static final String CHILD_SAMPLE_SET_TSV = "Name\tParent\tOtherProp\n" +
+            "SampleSetBVTChildA\tSampleSetBVT11\t1.1\n" +
+            "SampleSetBVTChildB\tFolderSampleSet.SampleSetBVT4\t2.2\n";
+
+    private static final String REPARENTED_CHILD_SAMPLE_SET_TSV = "Name\tParent\tOtherProp\n" +
+            "SampleSetBVTChildA\tSampleSetBVT13\t1.111\n" +
+            "SampleSetBVTChildB\tFolderSampleSet.SampleSetBVT14\t2.222\n";
+
+    private static final String GRANDCHILD_SAMPLE_SET_TSV = "Name\tParent\tOtherProp\n" +
+            "SampleSetBVTGrandchildA\tSampleSetBVTChildA,SampleSetBVTChildB\t11.11\n";
 
     public String getAssociatedModuleDirectory()
     {
@@ -38,7 +54,6 @@ public class SampleSetTest extends BaseSeleniumWebTest
 
     protected void doCleanup()
     {
-        try {deleteFolder(PROJECT_NAME, FOLDER_NAME); } catch (Throwable t) {}
         try {deleteProject(PROJECT_NAME); } catch (Throwable t) {}
     }
 
@@ -53,10 +68,10 @@ public class SampleSetTest extends BaseSeleniumWebTest
         clickNavButton("Import Sample Set");
         setFormElement("name", PROJECT_SAMPLE_SET_NAME);
         setFormElement("data", "KeyCol\tIntCol\tStringCol\tDateCol\tBoolCol\n" +
-                "1\t100\ta\t1/1/2000\tTRUE\n" +
-                "2\t200\tb\t2/2/2000\tFALSE\n" +
-                "3\t300\tc\t3/3/2000\tTRUE\n" +
-                "4\t400\td\t4/4/2000\tFALSE");
+                "SampleSetBVT1\t100\ta\t1/1/2000\tTRUE\n" +
+                "SampleSetBVT2\t200\tb\t2/2/2000\tFALSE\n" +
+                "SampleSetBVT3\t300\tc\t3/3/2000\tTRUE\n" +
+                "SampleSetBVT4\t400\td\t4/4/2000\tFALSE");
         clickNavButton("Submit");
 
         clickLinkWithText(FOLDER_NAME);
@@ -64,12 +79,14 @@ public class SampleSetTest extends BaseSeleniumWebTest
         clickNavButton("Import Sample Set");
         setFormElement("name", FOLDER_SAMPLE_SET_NAME);
         setFormElement("data", "KeyCol-Folder\tIntCol-Folder\tStringCol-Folder\n" +
-                "11\t101\taa\n" +
-                "12\t102\tbb\n" +
-                "13\t103\tcc\n" +
-                "14\t104\tdd");
+                "SampleSetBVT11\t101\taa\n" +
+                "SampleSetBVT4\t102\tbb\n" +
+                "SampleSetBVT12\t102\tbb\n" +
+                "SampleSetBVT13\t103\tcc\n" +
+                "SampleSetBVT14\t104\tdd");
         clickNavButton("Submit");
 
+        // Do some manual derivation
         clickLinkWithText("Sample Sets");
         assertTextPresent(PROJECT_SAMPLE_SET_NAME);
         assertTextPresent(FOLDER_NAME);
@@ -104,8 +121,8 @@ public class SampleSetTest extends BaseSeleniumWebTest
         selenium.select("targetSampleSetId", "label=FolderSampleSet in /SampleSetTestProject/SampleSetTestFolder");
         clickNavButton("Next");
 
-        setFormElement("outputSample1_KeyColFolder", "15");
-		setFormElement("outputSample2_KeyColFolder", "16");
+        setFormElement("outputSample1_KeyColFolder", "SampleSetBVT15");
+		setFormElement("outputSample2_KeyColFolder", "SampleSetBVT16");
 		selenium.click("outputSample1_IntColFolderCheckBox");
 		setFormElement("outputSample1_IntColFolder", "500a");
 		setFormElement("outputSample1_StringColFolder", "firstOutput");
@@ -142,16 +159,16 @@ public class SampleSetTest extends BaseSeleniumWebTest
         setFormElement("outputSample1_DateCol", "1/1/2007");
         clickNavButton("Submit");
 
-        assertLinkPresentWithText("Derive sample from 16");
-        assertLinkPresentWithText("11");
-        assertLinkPresentWithText("12");
-        assertLinkPresentWithText("13");
-        assertLinkPresentWithText("14");
+        assertLinkPresentWithText("Derive sample from SampleSetBVT16");
+        assertLinkPresentWithText("SampleSetBVT11");
+        assertLinkPresentWithText("SampleSetBVT12");
+        assertLinkPresentWithText("SampleSetBVT13");
+        assertLinkPresentWithText("SampleSetBVT14");
 
-        clickLinkWithText("11");
+        clickLinkWithText("SampleSetBVT11");
 
-        assertLinkPresentWithText("Derive sample from 16");
-        assertLinkPresentWithText("Derive 2 samples from 11, 12, 13, 14");
+        assertLinkPresentWithText("Derive sample from SampleSetBVT16");
+        assertLinkPresentWithText("Derive 2 samples from SampleSetBVT11, SampleSetBVT12, SampleSetBVT13, SampleSetBVT14, SampleSetBVT4");
 
         clickLinkWithText(FOLDER_NAME);
         clickLinkWithText(FOLDER_SAMPLE_SET_NAME);
@@ -167,5 +184,94 @@ public class SampleSetTest extends BaseSeleniumWebTest
         clickNavButton("Show All Materials");
         assertTextPresent("ProjectSampleSet");
         assertTextPresent("200");
+
+        // Try to derive samples using the parent column
+        clickTab("Experiment");
+        clickLinkWithText("Sample Sets");
+        clickNavButton("Import Sample Set");
+        setFormElement("name", FOLDER_CHILDREN_SAMPLE_SET_NAME);
+        setFormElement("data", AMBIGUOUS_CHILD_SAMPLE_SET_TSV);
+        selectOptionByText("parentCol", "Parent");
+        clickNavButton("Submit");
+        assertTextPresent("More than one match for parent material");
+
+        // Try again with a qualified sample name
+        setFormElement("data", CHILD_SAMPLE_SET_TSV);
+        selectOptionByText("parentCol", "Parent");
+        clickNavButton("Submit");
+        assertTextPresent("SampleSetBVTChildA");
+        clickLinkWithText("SampleSetBVTChildB");
+
+        // Make sure that the parent got wired up
+        clickLinkWithText("SampleSetBVT4");
+        // Check out the run
+        clickLinkWithText("Derive sample from SampleSetBVT4");
+        assertLinkPresentWithText("SampleSetBVT4");
+        assertLinkPresentWithText("SampleSetBVTChildB");
+
+        // Make a grandchild set, but first try to insert as a duplicate set name
+        clickTab("Experiment");
+        clickLinkWithText("Sample Sets");
+        clickNavButton("Import Sample Set");
+        setFormElement("name", FOLDER_CHILDREN_SAMPLE_SET_NAME);
+        setFormElement("data", GRANDCHILD_SAMPLE_SET_TSV);
+        selectOptionByText("parentCol", "Parent");
+        clickNavButton("Submit");
+
+        assertTextPresent("A sample set with that name already exists");
+        setFormElement("name", FOLDER_GRANDCHILDREN_SAMPLE_SET_NAME);
+        clickNavButton("Submit");
+
+        clickLinkWithText("SampleSetBVTGrandchildA");
+        // Filter out any child materials, though there shouldn't be any
+        setFilter("childMaterials", "Name", "Is Blank");
+        // Check for parents and grandparents
+        assertTextPresent("SampleSetBVTChildB");
+        assertTextPresent("SampleSetBVT4");
+        assertTextPresent("SampleSetBVT11");
+
+        // Verify that we've chained things together properly
+        clickLinkWithText("SampleSetBVTChildA");
+        // Filter out any child materials so we can just check for parents
+        setFilter("childMaterials", "Name", "Is Blank");
+        assertTextPresent("SampleSetBVT11");
+        assertLinkNotPresentWithText("SampleSetBVTGrandchildA");
+        // Switch to filter out any parent materials so we can just check for children
+        setFilter("parentMaterials", "Name", "Is Blank");
+        clearFilter("childMaterials", "Name");
+        assertLinkNotPresentWithText("SampleSetBVT11");
+        assertTextPresent("SampleSetBVTGrandchildA");
+
+        // Go up the chain one more hop
+        clearAllFilters("parentMaterials", "Name");
+        clickLinkWithText("SampleSetBVT11");
+        // Filter out any child materials so we can just check for parents
+        setFilter("childMaterials", "Name", "Is Blank");
+        assertLinkNotPresentWithText("SampleSetBVTChildA");
+        assertLinkNotPresentWithText("SampleSetBVTGrandchildA");
+        // Switch to filter out any parent materials so we can just check for children
+        setFilter("parentMaterials", "Name", "Is Blank");
+        clearFilter("childMaterials", "Name");
+        assertTextPresent("SampleSetBVTChildA");
+        assertTextPresent("SampleSetBVTGrandchildA");
+
+        clickLinkWithText(FOLDER_CHILDREN_SAMPLE_SET_NAME);
+        clickNavButton("Import More Samples");
+        clickCheckboxById("replaceOverwriteChoice", true);
+        setFormElement("data", REPARENTED_CHILD_SAMPLE_SET_TSV);
+        clickNavButton("Submit");
+
+        clickLinkWithText("SampleSetBVTChildB");
+        assertTextPresent("2.222");
+        assertLinkNotPresentWithText("SampleSetBVT4");
+        // Filter out any child materials so we can just check for parents
+        setFilter("childMaterials", "Name", "Is Blank");
+        assertLinkPresentWithText("SampleSetBVT14");
+        assertLinkNotPresentWithText("SampleSetBVTGrandchildA");
+        // Switch to filter out any parent materials so we can just check for children
+        setFilter("parentMaterials", "Name", "Is Blank");
+        clearFilter("childMaterials", "Name");
+        assertLinkNotPresentWithText("SampleSetBVT14");
+        assertLinkPresentWithText("SampleSetBVTGrandchildA");
     }
 }
--- src/org/labkey/test/bvt/StudyBvtTest.java
+++ src/org/labkey/test/bvt/StudyBvtTest.java
@@ -18,7 +18,6 @@ package org.labkey.test.bvt;
 
 import com.thoughtworks.selenium.SeleniumException;
 import org.labkey.test.Locator;
-import org.labkey.test.util.PasswordUtil;
 import org.labkey.test.drt.StudyTest;
 
 import java.io.File;
