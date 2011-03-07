096ad87adf90387ce097f82811e59d2f1ea525be
Kevin Krouse <kevink@labkey.com>
2011-03-07 22:40:30 +0000

    Issue 11432: Add client-provided context object to validation scripts
    LABKEY.Query.insert/update/delete/saveRows support an experimental 'extraContext' parameter
    that will end up in the table transformation/validation script environment.
    
    SVN r15762 |2011-03-07 22:40:30 +0000

--- modules/simpletest/queries/lists/People.js
+++ modules/simpletest/queries/lists/People.js
@@ -5,12 +5,12 @@
  */
 
 var console = require("console");
-console.log("** evaluating: " + this['javax.script.filename']);
+console.log("** evaluating: " + this['javax.script.filename'] + ", schema=" + schemaName + ", table=" + tableName);
+if (extraContext)
+    console.log("extraContext:", extraContext);
 
 function beforeInsert(row, errors)
 {
-    console.log("beforeInsert: " + row);
-
     // Test row map is case-insensitive
     if (row.Name != row.nAmE)
         throw new Error("beforeInsert row properties must be case-insensitive.");
@@ -18,8 +18,6 @@ function beforeInsert(row, errors)
 
 function afterInsert(row, errors)
 {
-    console.log("afterInsert: " + row);
-
     // Test row map is case-insensitive
     if (row.Name != row.nAmE)
         throw new Error("afterInsert row properties must be case-insensitive.");
@@ -27,8 +25,6 @@ function afterInsert(row, errors)
 
 function beforeUpdate(row, oldRow, errors)
 {
-    console.log("beforeUpdate: " + row);
-
     // Test row map is case-insensitive
     if (row.Name != row.nAmE)
         throw new Error("beforeUpdate row properties must be case-insensitive.");
@@ -40,8 +36,6 @@ function beforeUpdate(row, oldRow, errors)
 
 function afterUpdate(row, oldRow, errors)
 {
-    console.log("afterUpdate: " + row);
-
     // Test row map is case-insensitive
     if (row.Name != row.nAmE)
         throw new Error("afterUpdate row properties must be case-insensitive.");
@@ -53,8 +47,6 @@ function afterUpdate(row, oldRow, errors)
 
 function beforeDelete(row, errors)
 {
-    console.log("beforeDelete: " + row);
-
     // Test row map is case-insensitive
     if (row.Name != row.nAmE)
         throw new Error("beforeDelete row properties must be case-insensitive.");
@@ -62,15 +54,12 @@ function beforeDelete(row, errors)
 
 function afterDelete(row, errors)
 {
-    console.log("afterDelete: " + row);
-
     // Test row map is case-insensitive
     if (row.Name != row.nAmE)
         throw new Error("afterDelete row properties must be case-insensitive.");
 }
 
 var {Debug, trace} = require("simpletest/Debug");
-init         = Debug.addBefore(init, trace);
 beforeInsert = Debug.addBefore(beforeInsert, trace);
 afterInsert  = Debug.addBefore(afterInsert, trace);
 beforeUpdate = Debug.addBefore(beforeUpdate, trace);
