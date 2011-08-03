68f5927f1a1c7aba2c4b16da59c53c58a471ff07
Trey Chadick <tchad@labkey.com>
2011-08-03 00:28:11 +0000

    Test fix.  Some xpath changes.
    SVN r16943 |2011-08-03 00:28:11 +0000

--- src/org/labkey/test/tests/ListTest.java
+++ src/org/labkey/test/tests/ListTest.java
@@ -669,7 +669,7 @@ public class ListTest extends BaseSeleniumWebTest
         clickNavButton("Add Conditional Format", 0);
         ExtHelper.waitForExtDialog(this, "Apply Conditional Format Where BoolCol", WAIT_FOR_JAVASCRIPT);
         setFormElement("value_1", "true");
-        click(Locator.id("filterPanelOKButton"));
+        ExtHelper.clickExtButton(this, "Apply Conditional Format Where BoolCol", "OK", 0);
         checkCheckbox("Bold");
         checkCheckbox("Italic");
         checkCheckbox("Strikethrough");
@@ -691,19 +691,19 @@ public class ListTest extends BaseSeleniumWebTest
         // If greater than 5, Bold
         clickNavButton("Add Conditional Format", 0);
         ExtHelper.waitForExtDialog(this, "Apply Conditional Format Where IntCol", WAIT_FOR_JAVASCRIPT);
-        selectOptionByText("compare_1", "Is Greater Than");
+        ExtHelper.selectComboBoxItem(this, "Filter Type", "Is Greater Than");
         setFormElement("value_1", "5");
-        click(Locator.id("filterPanelOKButton"));
+        ExtHelper.clickExtButton(this, "Apply Conditional Format Where IntCol", "OK", 0);
         checkCheckbox("Bold");
         // If greater than 7, strikethrough
         clickNavButton("Add Conditional Format", 0);
         ExtHelper.waitForExtDialog(this, "Apply Conditional Format Where IntCol", WAIT_FOR_JAVASCRIPT);
-        selectOptionByText("compare_1", "Is Greater Than");
+        ExtHelper.selectComboBoxItem(this, "Filter Type", "Is Greater Than");
         setFormElement("value_1", "7");
-        click(Locator.id("filterPanelOKButton"));
+        ExtHelper.clickExtButton(this, "Apply Conditional Format Where IntCol", "OK", 0);
         // Switch the order of filters so that >7 takes precedence over >5
+        checkCheckbox("Strikethrough", 1);
         dragAndDrop(Locator.xpath("//div[text()='Is Greater Than 5']"), Locator.xpath("//div[text()='Is Greater Than 7']"));
-        checkCheckbox("Strikethrough");
 
         clickNavButton("Save", 0);
         waitAndClickNavButton("Done");
@@ -714,11 +714,12 @@ public class ListTest extends BaseSeleniumWebTest
         assertElementNotPresent(Locator.xpath("//td[text() = 'true' and not(contains(@style, 'line-through'))]"));
         assertElementNotPresent(Locator.xpath("//td[text() = 'true' and not(contains(@style, 'bold'))]"));
         assertElementNotPresent(Locator.xpath("//td[text() = 'true' and not(contains(@style, 'italic'))]"));
-        assertElementNotPresent(Locator.xpath("//td[text() = 'true' and not(contains(@style, 'color: rgb(0, 255, 255)'))]")); // Cyan text
-        assertElementNotPresent(Locator.xpath("//td[text() = 'true' and not(contains(@style, 'background-color: rgb(255, 0, 0)'))]")); // Red background
+        assertElementNotPresent(Locator.xpath("//td[text() = 'true' and not(contains(@style, 'color: #00FFFF'))]")); // Cyan text
+        assertElementNotPresent(Locator.xpath("//td[text() = 'true' and not(contains(@style, 'background-color: #FF0000'))]")); // Red background
         assertElementNotPresent(Locator.xpath("//td[text() = 'false' and @style]")); // No style on false items
-        assertElementPresent(Locator.xpath("//td[text()='6' and contains(@style, 'bold')]"));
-        assertElementPresent(Locator.xpath("//td[text()='8' and contains(@style, 'line-through')]"));
+        assertElementPresent(Locator.xpath("//td[text()='5' and not(contains(@style, 'bold')) and not(contains(@style, 'line-through'))]"));
+        assertElementPresent(Locator.xpath("//td[text()='6' and contains(@style, 'bold') and not(contains(@style, 'line-through'))]"));
+        assertElementPresent(Locator.xpath("//td[text()='8' and contains(@style, 'line-through') and not(contains(@style, 'bold'))]"));
 
         // Check for appropriate tooltips
         assertTextNotPresent("Formatting applied because column > 5.");
