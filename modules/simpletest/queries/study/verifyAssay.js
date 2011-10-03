5f37016ddb61b431d728dc206f3dc802a0f92199
Matthew Bellew <matthewb@labkey.com>
2011-10-03 23:51:29 +0000

    trigger script for study test
    SVN r17492 |2011-10-03 23:51:29 +0000

---
+++ modules/simpletest/queries/study/verifyAssay.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2010-2011 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+var console = require("console");
+console.log("** evaluating: " + this['javax.script.filename'] + ", schema=" + schemaName + ", table=" + tableName);
+
+var LABKEY = require("labkey");
+
+function beforeInsert(row, errors)
+{
+	// Initial caps
+	if (row.TextField)
+		row.TextField = row.TextField.substring(0,1).toUpperCase() + row.TextField.substring(1);
+	// ALL CAPS
+	if (row.SampleId)	
+		row.SampleId = row.SampleId.toUpperCase();
+}
+
+function afterInsert(row, errors)
+{
+}
+
+function beforeUpdate(row, oldRow, errors)
+{
+}
+
+function afterUpdate(row, oldRow, errors)
+{
+}
+
+function beforeDelete(row, errors)
+{
+}
+
+function afterDelete(row, errors)
+{
+}
+
