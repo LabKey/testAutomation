7aff50353d65be945dae6201ed0f7e575e18f742
Josh Eckels <jeckels@labkey.com>
2012-02-21 20:49:31 +0000

    Link Description column on job list to job results/data
    SVN r19079 |2012-02-21 20:49:31 +0000

--- src/org/labkey/test/ms2/AbstractMS2SearchEngineTest.java
+++ src/org/labkey/test/ms2/AbstractMS2SearchEngineTest.java
@@ -116,7 +116,7 @@ public abstract class AbstractMS2SearchEngineTest extends MS2TestBase
 
         waitForPipelineJobsToComplete(1, SAMPLE_BASE_NAME + " (test2)", false);
 
-        clickAndWait(Locator.raw("//td[contains(text(), 'test2')]/../td/a"));
+        clickAndWait(Locator.raw("//a[contains(text(), '" + SAMPLE_BASE_NAME + " (test2)')]/../../td/a"));
 
         log("View log file.");
 
