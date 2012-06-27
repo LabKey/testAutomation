42a593413818623f1cd04d1bac0137b0bafb4b02
Josh Eckels <jeckels@labkey.com>
2012-06-27 00:16:06 +0000

    Add coverage for spectra count-based flat peptide export features to existing LibraTest.
    Fix bug doing MS2 compares in containers with a name that contains #
    
    SVN r20920 |2012-06-27 00:16:06 +0000

--- src/org/labkey/test/tests/LibraTest.java
+++ src/org/labkey/test/tests/LibraTest.java
@@ -15,6 +15,7 @@
  */
 package org.labkey.test.tests;
 
+import org.labkey.test.BaseSeleniumWebTest;
 import org.labkey.test.Locator;
 import org.labkey.test.util.CustomizeViewsHelper;
 import org.labkey.test.util.ExtHelper;
@@ -78,13 +79,79 @@ public class LibraTest extends MS2Test
         addNormalizationCount();
 
         CustomizeViewsHelper.saveCustomView(this, standardView);
-        System.out.print("foo");
 
         checkForITRAQNormalization();
 
         proteinProphetTest();
         groupTest();
         specificProteinTest();
+
+        spectraCountTest();
+    }
+
+    private void spectraCountTest()
+    {
+        clickLinkWithText(getProjectName());
+        checkAllOnPage("MS2SearchRuns");
+        waitForElement(Locator.navButton("Compare"), WAIT_FOR_JAVASCRIPT);
+        clickNavButton("Compare", 0);
+        clickLinkWithText("Spectra Count");
+        clickRadioButtonById("SpectraCountPeptide");
+        clickNavButton("Compare");
+        assertTextPresent("-.MM'EILRGSPALSAFR.I");
+        assertLinkPresentWithTextCount("itraq/iTRAQ (Libra)", 27);
+
+        // Try setting a target protein
+        clickLinkWithText("Spectra Count Options");
+        setFormElement("targetProtein", "gi|34392343");
+        clickNavButton("Compare");
+        assertLinkPresentWithTextCount("itraq/iTRAQ (Libra)", 1);
+        assertTextPresent("R.TDTGEPM'GR.G");
+        clickLinkContainingText("gi|34392343");
+        assertTextPresent("84,731");
+        goBack();
+
+        // Customize view to pull in other columns
+        CustomizeViewsHelper.openCustomizeViewPanel(this);
+        CustomizeViewsHelper.addCustomizeViewColumn(this, "TrimmedPeptide");
+        CustomizeViewsHelper.addCustomizeViewColumn(this, new String[] {"Protein", "ProtSequence"});
+        CustomizeViewsHelper.addCustomizeViewColumn(this, new String[] {"Protein", "BestName"});
+        CustomizeViewsHelper.addCustomizeViewColumn(this, new String[] {"Protein", "Mass"});
+        CustomizeViewsHelper.saveDefaultView(this);
+        assertTextPresent("84731", "MPEETQAQDQPMEEEEVETFAFQAEIAQLM");
+
+        // Try a TSV export
+        addUrlParameter("exportAsWebPage=true");
+        clickExportToText();
+        assertTextPresent("# Target protein: gi|34392343", "R.TDTGEPM'GR.G", "84731", "MPEETQAQDQPMEEEEVETFAFQAEIAQLM");
+
+        // Try filtering based on a custom view using a different grouping
+        goBack();
+        clickLinkWithText("Spectra Count Options");
+        clickLinkWithText("Create or Edit View", false);
+        waitForElement(Locator.xpath("//button[text()='Save']"), BaseSeleniumWebTest.WAIT_FOR_JAVASCRIPT);
+        CustomizeViewsHelper.addCustomizeViewFilter(this, "Hyper", "Hyper", "Is Greater Than", "250");
+        assertRadioButtonSelected("spectraConfig", "SpectraCountPeptide");
+        CustomizeViewsHelper.saveCustomView(this, "HyperFilter");
+        clickRadioButtonById("SpectraCountPeptideCharge");
+        setFormElement(Locator.id("PeptidesFilter.viewName"), "HyperFilter");
+        setFormElement("targetProtein", "");
+        clickNavButton("Compare");
+        assertLinkPresentWithTextCount("itraq/iTRAQ (Libra)", 12);
+        assertTextPresent("-.MM'EILRGSPALSAFR.I", "R.TDTGEPM'GR.G");
+        assertTextNotPresent("R.AEGTFPGK.I", "R.ILEKSGSPER.I");
+
+        // Try a TSV export
+        addUrlParameter("exportAsWebPage=true");
+        clickExportToText();
+        assertTextPresent("# Peptide filter: (Hyper > 250)", "-.MM'EILRGSPALSAFR.I", "R.TDTGEPM'GR.G");
+        assertTextNotPresent("R.AEGTFPGK.I", "R.ILEKSGSPER.I");
+
+        // Validate that it remembers our options
+        goBack();
+        clickLinkWithText("Spectra Count Options");
+        assertRadioButtonSelected("spectraConfig", "SpectraCountPeptideCharge");
+        assertFormElementEquals(Locator.id("PeptidesFilter.viewName"), "HyperFilter");
     }
 
     protected void newWindowTest(String linkToClick, String verificationString, String... additionalChecks)
