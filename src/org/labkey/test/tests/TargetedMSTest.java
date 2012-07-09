9ec5054b00fca823dc831d2538d4ed99611d5922
Elizabeth Vannostrand <elvan@labkey.com>
2012-07-09 23:28:37 +0000

    fix targetedMS on postgres
    SVN r21080 |2012-07-09 23:28:37 +0000

--- src/org/labkey/test/tests/TargetedMSTest.java
+++ src/org/labkey/test/tests/TargetedMSTest.java
@@ -29,7 +29,7 @@ public class TargetedMSTest extends BaseSeleniumWebTest
         goToProjectHome();
         addWebPart("Data Pipeline");
         clickNavButton("Process and Import Data");
-        sleep(10000); //pipeline browser takes forever to load in linux
+        waitForText("MRMer", 5*defaultWaitForPage);
         selectPipelineFileAndImportAction("MRMer/" + SKY_FILE, "Import Skyline Results");
         waitForText("Targeted MS Runs ");
         waitForTextWithRefresh(SKY_FILE, defaultWaitForPage);
