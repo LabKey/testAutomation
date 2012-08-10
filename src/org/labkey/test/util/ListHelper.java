cc955d024de578417414b58e7e5373af352bb72e
Trey Chadick <tchad@labkey.com>
2012-08-10 16:32:57 +0000

    Fix NPE
    SVN r21581 |2012-08-10 16:32:57 +0000

--- src/org/labkey/test/util/ListHelper.java
+++ src/org/labkey/test/util/ListHelper.java
@@ -117,13 +117,13 @@ public class ListHelper
 
         public LookupInfo(String folder, String schema, String table)
         {
-            _folder = (folder.equals("") ? null : folder);
+            _folder = ("".equals(folder) ? null : folder);
             //container must exactly match an item in the dropdown
             if(_folder != null && !_folder.startsWith("/"))
                 _folder = "/" + _folder;
 
-            _schema = (schema.equals("") ? null : schema);
-            _table = (table.equals("") ? null : table);
+            _schema = ("".equals(schema) ? null : schema);
+            _table = ("".equals(table) ? null : table);
         }
 
         public String getFolder()
