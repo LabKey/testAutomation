370232f4981aa3005afb2d0edf3f79f0ed9ae1f0
Trey Chadick <tchad@labkey.com>
2012-09-10 22:08:07 +0000

    Remove unneeded import
    SVN r21947 |2012-09-10 22:08:07 +0000

--- src/org/labkey/test/tests/ReagentTest.java
+++ src/org/labkey/test/tests/ReagentTest.java
@@ -18,7 +18,6 @@ package org.labkey.test.tests;
 import org.junit.Assert;
 import org.labkey.test.BaseSeleniumWebTest;
 import org.labkey.test.Locator;
-import org.labkey.test.util.PostgresOnlyTest;
 
 /**
  * Created by IntelliJ IDEA.
