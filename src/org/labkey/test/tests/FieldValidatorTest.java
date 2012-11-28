23bc1db7c70e700a88965e82d50d8fabf1877fe2
Trey Chadick <tchad@labkey.com>
2012-11-28 00:30:10 +0000

    Use correct helper
    SVN r23299 |2012-11-28 00:30:10 +0000

--- src/org/labkey/test/tests/FieldValidatorTest.java
+++ src/org/labkey/test/tests/FieldValidatorTest.java
@@ -64,7 +64,7 @@ public class FieldValidatorTest extends BaseWebDriverTest
         log("Test upload data");
         clickButton("Import Data");
         setFormElement(Locator.name("text"), TEST_DATA_FAIL);
-        _listHelper.submitImportTsv_error(null);
+        _listHelper.submitImportTsv_success();
         waitForText(SEX_ERROR_MSG);
         assertTextPresent(ID_ERROR_MSG);
         assertTextPresent(AGE_ERROR_MSG);
