298ab187d2e42eebbbd01833a1ee4399f29677cf
Josh Eckels <jeckels@labkey.com>
2012-12-25 00:40:49 +0000

    Issue 16736: Experiment.DeriveSamplesChooseTargetAction: AssertionError on bad 'rowId'
    Converted to use NotFoundException instead
    
    SVN r23767 |2012-12-25 00:40:49 +0000

--- src/org/labkey/test/util/Crawler.java
+++ src/org/labkey/test/util/Crawler.java
@@ -152,7 +152,6 @@ public class Crawler
             new ControllerActionId("flow-editscript", "editGateTree"), // TODO: 16733: flow-editscript.EditGateTreeAction: NumberFormatException on bad 'scriptId'
             new ControllerActionId("project", "customizeWebPart"), // TODO: 16734: Project.CustomizeWebPartAction: ClassNotFoundException on bad 'webPartId'
             new ControllerActionId("idri", "formulationDetails"), // TODO: 16735: Idri.FormulationDetailsAction: NullPointerException on bad 'rowId'
-            new ControllerActionId("experiment", "deriveSamplesChooseTarget"), // TODO: 16736: Experiment.DeriveSamplesChooseTargetAction: AssertionError on bad 'rowId'
             new ControllerActionId("user", "deactivateUsers"), // TODO: 16737: User.DeactivateUsersAction: AssertionError on bad 'userId'
             new ControllerActionId("user", "deleteUsers"), // TODO: 16769: User.DeleteUsersAction: AssertionError on bad 'userId'
             new ControllerActionId("study-designer", "designer")); // TODO: 16768: study-designer.DesignerAction: IllegalArgumentException on bad 'panel'
