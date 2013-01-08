9a6cbcd53b304632651e0b263b150f7eeee5b8ac
Adam Rauch <adam@labkey.com>
2013-01-08 05:59:14 +0000

    Update licenses
    SVN r23902 |2013-01-08 05:59:14 +0000

--- modules/scriptpad/resources/reports/schemas/consoleOnly.r
+++ modules/scriptpad/resources/reports/schemas/consoleOnly.r
@@ -1,3 +1,18 @@
+##
+#  Copyright (c) 2012-2013 LabKey Corporation
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+##
 # This sample code returns the query data in tab-separated values format, which LabKey then
 # renders as HTML. Replace this code with your R script. See the Help tab for more details.
 print("I am console output - hear me roar!");
