a7bcf3c1587bedb83012b2bb1214efbdd44e87d5
Rylan Bauermeister <rylanb@labkey.com>
2013-09-18 17:05:28 +0000

    Automated tests for table truncation buttons
    SVN r28429 |2013-09-18 17:05:28 +0000

---
+++ src/org/labkey/test/tests/TruncationTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2009-2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.labkey.test.tests;
+
+import org.junit.experimental.categories.Category;
+import org.labkey.test.Locator;
+import org.labkey.test.categories.Specimen;
+import org.labkey.test.categories.Study;
+
+import java.io.File;
+
+@Category({Study.class, Specimen.class})
+public class TruncationTest extends StudyBaseTest
+{
+    private final File LIST_ARCHIVE =  new File(getLabKeyRoot() + getStudySampleDataPath() + "/searchTest.lists.zip");
+    private final String STUDY_NAME = "Study 001";
+
+    @Override
+    protected void doCreateSteps()
+    {
+        importStudy();
+        waitForPipelineJobsToComplete(1, "study import", false);
+        goToProjectHome();
+        addWebPart("Lists");
+        _listHelper.importListArchive(getFolderName(), LIST_ARCHIVE);
+        truncateList();
+        truncateDataset();
+        ensureTruncateVisibility();
+    }
+
+    protected void truncateList()
+    {
+        click(Locator.linkContainingText("Delete All Rows"));
+        click(Locator.ext4Button("Yes"));
+        waitForText("2 rows deleted");
+        click(Locator.ext4Button("OK"));
+        click(Locator.linkContainingText("view data"));
+        waitForText("No data to show.");
+    }
+
+    protected void truncateDataset()
+    {
+        click(Locator.linkContainingText(STUDY_NAME));
+        waitAndClick(Locator.linkContainingText("Manage Datasets"));
+        waitForText("Create New Dataset");
+        waitAndClick(Locator.linkContainingText("DEM-1"));
+        waitAndClick(Locator.linkContainingText("Delete All Rows"));
+        click(Locator.ext4Button("Yes"));
+        waitForText("24 rows deleted");
+        click(Locator.ext4Button("OK"));
+        click(Locator.linkContainingText("View Data"));
+        waitForText("No data to show.");
+    }
+
+    protected void ensureTruncateVisibility()
+    {
+        impersonateRole("Editor");
+        click(Locator.linkContainingText(STUDY_NAME));
+        waitAndClick(Locator.xpath("//span[text()='Lists']"));
+        assertTextNotPresent("Delete All Rows");
+        stopImpersonatingRole();
+    }
+
+    @Override
+    protected void doVerifySteps()
+    {
+        //Do nothing
+    }
+}
