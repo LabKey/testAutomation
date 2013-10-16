211f8f896ce2928b6c6f2530d407bbf8af40cc4e
Ryan Standley <rstandley@labkey.com>
2013-10-16 22:38:00 +0000

    adding class for hidra tests
    SVN r28953 |2013-10-16 22:38:00 +0000

---
+++ src/org/labkey/test/categories/Hidra.java
@@ -0,0 +1,12 @@
+package org.labkey.test.categories;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: RyanS
+ * Date: 10/16/13
+ * Time: 11:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class Hidra extends Test
+{
+}
