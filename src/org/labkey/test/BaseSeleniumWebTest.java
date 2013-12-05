42b5f24077460ff81319e6620b5bf9bea45517b1
Trey Chadick <tchad@labkey.com>
2013-12-05 23:24:13 +0000

    Don't use CssLocator. Getting false positive
    SVN r29809 |2013-12-05 23:24:13 +0000

--- src/org/labkey/test/BaseSeleniumWebTest.java
+++ src/org/labkey/test/BaseSeleniumWebTest.java
@@ -556,8 +556,8 @@ public abstract class BaseSeleniumWebTest implements Cleanable, WebTest
 
         assertSignOutAndMyAccountPresent();
 
-        if (isElementPresent(Locator.css(".labkey-nav-page-header").withText("Startup Modules"))||
-            isElementPresent(Locator.css(".labkey-nav-page-header").withText("Upgrade Modules")))
+        if (isElementPresent(Locator.tag("span").withClass("labkey-nav-page-header").withText("Startup Modules"))||
+            isElementPresent(Locator.tag("span").withClass("labkey-nav-page-header").withText("Upgrade Modules")))
         {
             waitForElement(Locator.id("status-progress-bar").withText("Module startup complete"));
             clickButton("Next");
