744e89ca0074f5aefd4fafa178950b526eae7506
Trey Chadick <tchad@labkey.com>
2013-12-16 17:40:11 +0000

    Test workaround for 19217: DeleteContainerCommand: Container name gets incorrectly parsed if it matches a module name
    SVN r29946 |2013-12-16 17:40:11 +0000

--- src/org/labkey/test/util/APIContainerHelper.java
+++ src/org/labkey/test/util/APIContainerHelper.java
@@ -121,7 +121,8 @@ public class APIContainerHelper extends AbstractContainerHelper
         }
         catch (CommandException e)
         {
-            if (e.getMessage().contains("Not Found"))
+            if (e.getMessage().contains("Not Found") ||
+                e.getMessage().contains("The path / is not deletable.")) // TODO: 19217: DeleteContainerCommand: Container name gets incorrectly parsed if it matches a module name
             {
                 if (failIfNotFound)
                     fail("Container not found: " + path);
