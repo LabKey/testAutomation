96a4b883bf2421e97f0a90f0b4341f86b3a20d61
Alan Vezina <alanv@labkey.com>
2014-01-08 22:54:40 +0000

    Add test cases for setAes to chartingAPITest  - setAesAPITest.html required for wiki webpart used in test.
    Fix issue with setAes when removing aesthetics.
    
    SVN r30199 |2014-01-08 22:54:40 +0000

--- src/org/labkey/test/tests/ChartingAPITest.java
+++ src/org/labkey/test/tests/ChartingAPITest.java
@@ -60,31 +60,6 @@ public class ChartingAPITest extends ClientAPITest
             "Boxplot No Outliers, All Points"
     };
 
-    protected static final String SCATTER_ONE = "Scatter Plot One";
-    protected static final String SCATTER_TWO = "Scatter Plot Two (Custom)";
-    protected static final String BOX_ONE = "Box Plot One";
-    protected static final String BOX_TWO = "Box Plot Two (Custom)";
-    protected static final String BOX_THREE = "Box Plot Three (Custom, Broken)";
-
-    protected static final String SCATTER_ONE_TEXT = "0\n200\n400\n600\n800\n1000\n1200\n1400\n800\n1000\n1200\n1400\n1600\n1800\n2000\nScatter Plot One\nCD4+ (cells/mm3)\nLymphs (cells/mm3)";
-    protected static final String SCATTER_TWO_TEXT = "0\n200\n400\n600\n800\n1000\n1200\n1400\n800\n1000\n1200\n1400\n1600\n1800\n2000\nScatter Plot Two (Custom)\nCD4\nLymphs\nFemales\nMales";
-    protected static final String BOX_ONE_TEXT = "Males\nFemales\n600\n800\n1000\n1200\n1400\n1600\n1800\n2000\n2200\nBox Plot One\nGender\nLymphs (cells/mm3)";
-    protected static final String BOX_TWO_TEXT = "Males\nFemales\n200\n400\n600\n800\n1000\n1200\n1400\n1600\nBox Plot Two (Custom)\nGender\nCD4\nFemales";
-
-    protected static final String TIME_CHART_1 = "Luminex: 249318596";
-    protected static final String TIME_CHART_1_TEXT_1 = "10\n15\n20\n25\n30\n35\n70\n700\n50\n100\n150\n200\n250\n300\n350\nLuminex: 249318596\nWeeks Since Demographic Start Date\nFluorescence Intensity\nObserved Concentration\n249318596 Fi IL-10\n249318596 Fi IL-2\n249318596 Fi TNF-alpha\n249318596 ObsConc IL-10\n249318596 ObsConc IL-2\n249318596 ObsConcTNF-alpha";
-    protected static final String TIME_CHART_1_TEXT_2 = "0\n5\n10\n15\n20\n25\n30\n70\n700\n7000\n50\n100\n150\n200\n250\n300\n350\nLuminex: 249320107\nWeeks Since Demographic Start Date\nFluorescence Intensity\nObserved Concentration\n249320107 Fi IL-10\n249320107 Fi IL-2\n249320107 Fi TNF-alpha\n249320107 ObsConc IL-10\n249320107 ObsConc IL-2\n249320107 ObsConcTNF-alpha";
-    protected static final String TIME_CHART_1_TEXT_3 = "0\n5\n10\n15\n20\n25\n30\n80\n90\n100\n200\n300\n50\n100\n150\n200\n250\n300\n350\nLuminex: 249320127\nWeeks Since Demographic Start Date\nFluorescence Intensity\nObserved Concentration\n249320127 Fi IL-10\n249320127 Fi IL-2\n249320127 Fi TNF-alpha\n249320127 ObsConc IL-10\n249320127 ObsConc IL-2\n249320127 ObsConcTNF-alpha";
-    protected static final String TIME_CHART_2 = "Luminex Two";
-    protected static final String TIME_CHART_2_TEXT_1 = "1\n2\n3\n4\n5\n0.2\n0.4\n0.6\n0.8\n1.0\n1.2\n1.4\n1.6\n1.8\n2.0\nLuminex Two\nVisits\nFI\n249318596\n249320127";
-    protected static final String TIME_CHART_3 = "Male";
-    protected static final String TIME_CHART_3_TEXT_1 = "1\n2\n3\n4\n5\n0\n5\n10\n15\n20\n25\n30\n35\n40\n45\n50\nMale\nVisit\nObs Conc\nMale";
-    protected static final String TIME_CHART_3_TEXT_2 = "1\n2\n3\n4\n5\n0\n50\n100\n150\n200\n250\n300\nFemale\nVisit\nObs Conc\nFemale";
-    protected static final String TIME_CHART_4 = "Luminex Four";
-    protected static final String TIME_CHART_4_TEXT_1 = "200\n400\n600\n800\n1000\n1200\n1400\nLuminex Four\nDays Since Start Date\nFi\n249318596";
-    protected static final String TIME_CHART_5 = "Luminex Five";
-    protected static final String TIME_CHART_5_TEXT_1 = "0\n50\n100\n150\n200\n1000\n2000\n3000\n4000\n5000\n6000\n7000\nLuminex Five\nDays Since Start Date\nFi\n249318596 IL-10\n249318596 IL-2\n249318596 IL-6\n249318596 TNF-alpha\n249320107 IL-10\n249320107 IL-2\n249320107 IL-6\n249320107 TNF-alpha\n249320127 IL-10\n249320127 IL-2\n249320127 IL-6\n249320127 TNF-alpha";
-
     @Override
     public BrowserType bestBrowser()
     {
@@ -119,6 +94,7 @@ public class ChartingAPITest extends ClientAPITest
         chartAPITest();
         genericChartHelperTest();
         timeChartHelperTest();
+        setAesAPITest();
     }
 
     @LogMethod(category = LogMethod.MethodType.SETUP)
@@ -192,6 +168,17 @@ public class ChartingAPITest extends ClientAPITest
         }
     }
 
+    protected static final String SCATTER_ONE = "Scatter Plot One";
+    protected static final String SCATTER_TWO = "Scatter Plot Two (Custom)";
+    protected static final String BOX_ONE = "Box Plot One";
+    protected static final String BOX_TWO = "Box Plot Two (Custom)";
+    protected static final String BOX_THREE = "Box Plot Three (Custom, Broken)";
+
+    protected static final String SCATTER_ONE_TEXT = "0\n200\n400\n600\n800\n1000\n1200\n1400\n800\n1000\n1200\n1400\n1600\n1800\n2000\nScatter Plot One\nCD4+ (cells/mm3)\nLymphs (cells/mm3)";
+    protected static final String SCATTER_TWO_TEXT = "0\n200\n400\n600\n800\n1000\n1200\n1400\n800\n1000\n1200\n1400\n1600\n1800\n2000\nScatter Plot Two (Custom)\nCD4\nLymphs\nFemales\nMales";
+    protected static final String BOX_ONE_TEXT = "Males\nFemales\n600\n800\n1000\n1200\n1400\n1600\n1800\n2000\n2200\nBox Plot One\nGender\nLymphs (cells/mm3)";
+    protected static final String BOX_TWO_TEXT = "Males\nFemales\n200\n400\n600\n800\n1000\n1200\n1400\n1600\nBox Plot Two (Custom)\nGender\nCD4\nFemales";
+
     @LogMethod(category = LogMethod.MethodType.VERIFICATION)
     private void genericChartHelperTest()
     {
@@ -217,6 +204,20 @@ public class ChartingAPITest extends ClientAPITest
         waitForText("The measure Cohort was not found. It may have been renamed or removed.");
         checkExportedChart(BOX_THREE, null, true, 0);
     }
+
+    protected static final String TIME_CHART_1 = "Luminex: 249318596";
+    protected static final String TIME_CHART_1_TEXT_1 = "10\n15\n20\n25\n30\n35\n70\n700\n50\n100\n150\n200\n250\n300\n350\nLuminex: 249318596\nWeeks Since Demographic Start Date\nFluorescence Intensity\nObserved Concentration\n249318596 Fi IL-10\n249318596 Fi IL-2\n249318596 Fi TNF-alpha\n249318596 ObsConc IL-10\n249318596 ObsConc IL-2\n249318596 ObsConcTNF-alpha";
+    protected static final String TIME_CHART_1_TEXT_2 = "0\n5\n10\n15\n20\n25\n30\n70\n700\n7000\n50\n100\n150\n200\n250\n300\n350\nLuminex: 249320107\nWeeks Since Demographic Start Date\nFluorescence Intensity\nObserved Concentration\n249320107 Fi IL-10\n249320107 Fi IL-2\n249320107 Fi TNF-alpha\n249320107 ObsConc IL-10\n249320107 ObsConc IL-2\n249320107 ObsConcTNF-alpha";
+    protected static final String TIME_CHART_1_TEXT_3 = "0\n5\n10\n15\n20\n25\n30\n80\n90\n100\n200\n300\n50\n100\n150\n200\n250\n300\n350\nLuminex: 249320127\nWeeks Since Demographic Start Date\nFluorescence Intensity\nObserved Concentration\n249320127 Fi IL-10\n249320127 Fi IL-2\n249320127 Fi TNF-alpha\n249320127 ObsConc IL-10\n249320127 ObsConc IL-2\n249320127 ObsConcTNF-alpha";
+    protected static final String TIME_CHART_2 = "Luminex Two";
+    protected static final String TIME_CHART_2_TEXT_1 = "1\n2\n3\n4\n5\n0.2\n0.4\n0.6\n0.8\n1.0\n1.2\n1.4\n1.6\n1.8\n2.0\nLuminex Two\nVisits\nFI\n249318596\n249320127";
+    protected static final String TIME_CHART_3 = "Male";
+    protected static final String TIME_CHART_3_TEXT_1 = "1\n2\n3\n4\n5\n0\n5\n10\n15\n20\n25\n30\n35\n40\n45\n50\nMale\nVisit\nObs Conc\nMale";
+    protected static final String TIME_CHART_3_TEXT_2 = "1\n2\n3\n4\n5\n0\n50\n100\n150\n200\n250\n300\nFemale\nVisit\nObs Conc\nFemale";
+    protected static final String TIME_CHART_4 = "Luminex Four";
+    protected static final String TIME_CHART_4_TEXT_1 = "200\n400\n600\n800\n1000\n1200\n1400\nLuminex Four\nDays Since Start Date\nFi\n249318596";
+    protected static final String TIME_CHART_5 = "Luminex Five";
+    protected static final String TIME_CHART_5_TEXT_1 = "0\n50\n100\n150\n200\n1000\n2000\n3000\n4000\n5000\n6000\n7000\nLuminex Five\nDays Since Start Date\nFi\n249318596 IL-10\n249318596 IL-2\n249318596 IL-6\n249318596 TNF-alpha\n249320107 IL-10\n249320107 IL-2\n249320107 IL-6\n249320107 TNF-alpha\n249320127 IL-10\n249320127 IL-2\n249320127 IL-6\n249320127 TNF-alpha";
 
     @LogMethod(category = LogMethod.MethodType.VERIFICATION)
     private void timeChartHelperTest()
@@ -262,7 +263,50 @@ public class ChartingAPITest extends ClientAPITest
         click(Locator.input("next-btn"));
         waitForText("Please select either \"Show Individual Lines\" or \"Show Mean\".");
     }
+    protected static final String BOX_PLOT_COLOR_SHAPE = "Box Plot - Change outlier color/shape";
+    protected static final String SCATTER_PLOT_SHAPE_COLOR_X = "Scatter - add shape/color change x";
+    protected static final String SCATTER_PLOT_SHAPE_COLOR_X_SVG_BEFORE = "800\n1000\n1200\n1400\n1600\n1800\n2000\n0\n200\n400\n600\n800\n1000\n1200\n1400\nScatter - add shape/color change x";
+    protected static final String SCATTER_PLOT_SHAPE_COLOR_X_SVG_AFTER = "0\n200\n400\n600\n800\n1000\n1200\n1400\n0\n200\n400\n600\n800\n1000\n1200\n1400\nScatter - add shape/color change x\n103866\n110349\n119180\n125478\nMales\nFemales";
+    protected static final String SCATTER_HOVER_CLICK = "Scatter with hover and click";
+    protected static final String LINE_ERROR_COLOR_Y = "Line/Error - Add color, change Y";
+    protected static final String LINE_ERROR_COLOR_Y_SVG = "0\n5\n10\n15\n20\n0\n10\n20\n30\n40\n50\n60\n70\n80\n90\n100\nLine/Error - Add color, change Y\nAlan\nTrey\nNick";
 
+    @LogMethod(category = LogMethod.MethodType.VERIFICATION)
+    private void setAesAPITest()
+    {
+        Locator nextBtn = Locator.input("next-btn");
+        Locator setAesBtn = Locator.input("set-aes-btn");
+        File chartTestFile = new File(getApiFileRoot(), "setAesAPITest.html");
+        createAPITestWiki("setAesTestWiki", chartTestFile, false);
+
+        waitForText(BOX_PLOT_COLOR_SHAPE);
+        click(setAesBtn);
+        waitForText("119180");
+        assertTextPresent("Females");
+
+        click(nextBtn);
+        waitForText(SCATTER_PLOT_SHAPE_COLOR_X);
+        assertSVG(SCATTER_PLOT_SHAPE_COLOR_X_SVG_BEFORE);
+        click(setAesBtn);
+        waitForText("103866");
+        assertSVG(SCATTER_PLOT_SHAPE_COLOR_X_SVG_AFTER);
+
+        click(nextBtn);
+        waitForText(SCATTER_HOVER_CLICK);
+        click(Locator.css("svg g a path"));
+        waitForText("Look a click handler!");
+        clickButton("OK", 0);
+        click(setAesBtn);
+        click(Locator.css("svg g a path"));
+        waitForText("The click handler has changed!");
+        clickButton("OK", 0);
+
+        click(nextBtn);
+        waitForText(LINE_ERROR_COLOR_Y);
+        click(setAesBtn);
+        waitForText("Alan");
+        assertSVG(LINE_ERROR_COLOR_Y_SVG);
+    }
 
     private void checkExportedChart(String title, String svgText)
     {
