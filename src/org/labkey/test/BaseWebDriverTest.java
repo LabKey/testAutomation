a9667f6cb3f536f27fced9f6e0b005776fa719d0
Trey Chadick <tchad@labkey.com>
2014-03-18 00:04:08 +0000

    Use UTF escaped characters for tricky test characters
    SVN r31535 |2014-03-18 00:04:08 +0000

--- src/org/labkey/test/BaseWebDriverTest.java
+++ src/org/labkey/test/BaseWebDriverTest.java
@@ -180,10 +180,10 @@ public abstract class BaseWebDriverTest extends BaseSeleniumWebTest implements C
 
     public static final double DELTA = 10E-10;
 
-    public static final String TRICKY_CHARACTERS = "><&/%\\' \"1äöü";
-    public static final String TRICKY_CHARACTERS_NO_QUOTES = "></% 1äöü";
+    public static final String TRICKY_CHARACTERS = "><&/%\\' \"1\u00E4\u00F6\u00FC";
+    public static final String TRICKY_CHARACTERS_NO_QUOTES = "></% 1\u00E4\u00F6\u00FC";
 
-    public static String TRICKY_CHARACTERS_FOR_PROJECT_NAMES = "\u2603~!@$&()_+{}-=[],.#äöü";
+    public static String TRICKY_CHARACTERS_FOR_PROJECT_NAMES = "\u2603~!@$&()_+{}-=[],.#\u00E4\u00F6\u00FC";
 
     public static final String INJECT_CHARS_1 = "\"'>--><script>alert('8(');</script>;P";
     public static final String INJECT_CHARS_2 = "\"'>--><img src=xss onerror=alert(\"8(\")>\u2639";
