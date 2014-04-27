17d4f1a1a289532f82f2d324875d4e0478dd9edf
Josh Eckels <jeckels@labkey.com>
2014-04-27 16:49:35 +0000

    LabKey JDBC driver enhancements:
    Register driver in static initializer in LabKeyDriver
    
    Recognize "jdbc:labkey:" JDBC URLs
    
    Fix a few bugs
    
    Add automated tests
    
    Support setting timeout as connection/clientInfo property
    
    SVN r32163 |2014-04-27 16:49:35 +0000

---
+++ src/org/labkey/test/LabKeyJDBCTest.java
@@ -0,0 +1,162 @@
+package org.labkey.test;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.labkey.test.categories.BVT;
+import org.labkey.test.util.PasswordUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by jeckels on 4/27/14.
+ */
+@Category(BVT.class)
+public class LabKeyJDBCTest
+{
+    @BeforeClass
+    public static void registerDriver() throws ClassNotFoundException
+    {
+        Class.forName("org.labkey.remoteapi.query.jdbc.LabKeyDriver");
+    }
+
+    public Connection _connection;
+
+    @Before
+    public void setup() throws SQLException
+    {
+        _connection = DriverManager.getConnection("jdbc:labkey:" + WebTestHelper.getBaseURL(), PasswordUtil.getUsername(), PasswordUtil.getPassword());
+        _connection.setCatalog("/home");
+    }
+
+    @Test
+    public void testCatalogs() throws SQLException
+    {
+        assertEquals("Didn't remember catalog", "/home", _connection.getCatalog());
+        ResultSet rs = _connection.getMetaData().getCatalogs();
+        boolean foundHome = false;
+        boolean foundShared = false;
+
+        while (rs.next())
+        {
+            String catalog = rs.getString("TABLE_CAT");
+            foundHome |= "/home".equalsIgnoreCase(catalog);
+            foundShared |= "/shared".equalsIgnoreCase(catalog);
+        }
+
+        assertTrue("Didn't find /home", foundHome);
+        assertTrue("Didn't find /shared", foundShared);
+    }
+
+    @Test
+    public void testSchemas() throws SQLException
+    {
+        ResultSet rs = _connection.getMetaData().getSchemas();
+        boolean foundCore = false;
+        boolean foundPipeline = false;
+
+        while (rs.next())
+        {
+            String schema = rs.getString("TABLE_SCHEM");
+            foundCore |= "core".equalsIgnoreCase(schema);
+            foundPipeline |= "pipeline".equalsIgnoreCase(schema);
+        }
+
+        assertTrue("Didn't find core schema", foundCore);
+        assertTrue("Didn't find pipeline schema", foundPipeline);
+    }
+
+    @Test
+    public void testQueries() throws SQLException
+    {
+        ResultSet rs = _connection.getMetaData().getTables("/home", "core", null, null);
+        boolean foundContainers = false;
+        boolean foundGroups = false;
+
+        while (rs.next())
+        {
+            assertEquals("Wrong catalog", "/home", rs.getString("TABLE_CAT"));
+            assertEquals("Wrong schema", "core", rs.getString("TABLE_SCHEM"));
+            String table = rs.getString("TABLE_NAME");
+            foundContainers |= "containers".equalsIgnoreCase(table);
+            foundGroups |= "groups".equalsIgnoreCase(table);
+        }
+
+        assertTrue("Didn't find containers query", foundContainers);
+        assertTrue("Didn't find groups query", foundGroups);
+    }
+
+    @Test
+    public void testSQL() throws SQLException
+    {
+        ResultSet rs = _connection.createStatement().executeQuery("SELECT * FROM core.Containers");
+
+        assertTrue("Need one row", rs.next());
+        // Also test column case-insensitivity
+        assertTrue("Should be home container", "home".equalsIgnoreCase(rs.getString("name")));
+        assertTrue("Should be home container", "home".equalsIgnoreCase(rs.getString("NAME")));
+        assertTrue("Should be home container", "home".equalsIgnoreCase(rs.getString(rs.findColumn("name"))));
+        assertTrue("Should be home container", "home".equalsIgnoreCase(rs.getString(rs.findColumn("NAME"))));
+
+        assertTrue("Should have RowId", rs.getInt(rs.findColumn("rowid")) > 0);
+        assertTrue("Should have RowId", rs.getInt(rs.findColumn("ROWID")) > 0);
+        assertTrue("Should have RowId", rs.getInt("rowid") > 0);
+        assertTrue("Should have RowId", rs.getInt("ROWID") > 0);
+
+        assertFalse("Should be a single row", rs.next());
+    }
+
+    @Test
+    public void testColumns() throws SQLException
+    {
+        ResultSet rs = _connection.getMetaData().getColumns("/home", "core", "containers", null);
+        boolean foundEntityId = false;
+        boolean foundRowId = false;
+        boolean foundName = false;
+
+        while (rs.next())
+        {
+            assertEquals("Wrong catalog", "/home", rs.getString("TABLE_CAT"));
+            assertEquals("Wrong schema", "core", rs.getString("TABLE_SCHEM"));
+            assertEquals("Wrong table", "containers", rs.getString("TABLE_NAME"));
+            String column = rs.getString("COLUMN_NAME");
+            if ("entityid".equalsIgnoreCase(column))
+            {
+                foundEntityId = true;
+                assertEquals("Wrong type for EntityId", Types.VARCHAR, rs.getInt("DATA_TYPE"));
+                assertEquals("Wrong auto-increment for EntityId", "NO", rs.getString("IS_AUTOINCREMENT"));
+                assertEquals("Wrong auto-generated for EntityId", "YES", rs.getString("IS_GENERATEDCOLUMN"));
+            }
+            if ("rowid".equalsIgnoreCase(column))
+            {
+                foundRowId = true;
+                assertEquals("Wrong type for RowId", Types.INTEGER, rs.getInt("DATA_TYPE"));
+                assertEquals("Wrong auto-increment for RowId", "YES", rs.getString("IS_AUTOINCREMENT"));
+                assertEquals("Wrong auto-generated for RowId", "YES", rs.getString("IS_GENERATEDCOLUMN"));
+            }
+            if ("name".equalsIgnoreCase(column))
+            {
+                foundName = true;
+                assertEquals("Wrong type for Name", Types.VARCHAR, rs.getInt("DATA_TYPE"));
+                assertEquals("Wrong auto-increment for Name", "NO", rs.getString("IS_AUTOINCREMENT"));
+                assertEquals("Wrong auto-generated for Name", "NO", rs.getString("IS_GENERATEDCOLUMN"));
+            }
+        }
+
+        assertTrue("Didn't find EntityId column", foundEntityId);
+        assertTrue("Didn't find Name column", foundName);
+        assertTrue("Didn't find RowId column", foundRowId);
+    }
+
+
+
+}
