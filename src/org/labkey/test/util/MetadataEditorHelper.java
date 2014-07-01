71dfc12d89d0a504e696819587c0f0468566a972
Trey Chadick <tchad@labkey.com>
2014-07-01 00:35:36 +0000

    Create a starting point for metadata domain editor helpers Many such helpers currently live in ListHelper despite not being list-specific
    SVN r33246 |2014-07-01 00:35:36 +0000

---
+++ src/org/labkey/test/util/MetadataEditorHelper.java
@@ -0,0 +1,51 @@
+package org.labkey.test.util;
+
+import org.labkey.test.BaseWebDriverTest;
+import org.labkey.test.Locator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+//TODO: Move domain helpers that aren't list-specific from ListHelper
+public class MetadataEditorHelper
+{
+    private BaseWebDriverTest _test;
+
+    public MetadataEditorHelper(BaseWebDriverTest test)
+    {
+        _test = test;
+    }
+
+    public void save()
+    {
+        _test.clickButton("Save", 0);
+        _test.waitForText("Save successful.");
+    }
+
+    public int getFieldIndexForName(String name)
+    {
+        Locator.XPathLocator fieldRow = Locator.tagWithClass("table", "labkey-wp").withPredicate(Locator.tagWithClass("tr", "labkey-wp-header").withText("Metadata Properties"))
+                .append(Locator.tagWithClass("table", "labkey-pad-cells"))
+                .append(Locator.tag("tr"));
+
+        List<WebElement> allRows = fieldRow.waitForElements(_test.getDriver(), BaseWebDriverTest.WAIT_FOR_JAVASCRIPT);
+        WebElement headerRow = allRows.get(0);
+        List<WebElement> fieldRows = allRows.subList(1, allRows.size());
+
+        int nameColumnIndex = getCellIndexFromRow(headerRow, "Name");
+
+        WebElement desiredRow = fieldRow.withPredicate(Locator.xpath("td").index(nameColumnIndex).withText(name)).findElement(_test.getDriver());
+
+        return fieldRows.indexOf(desiredRow);
+    }
+
+    private int getCellIndexFromRow(WebElement tr, String cellText)
+    {
+        List<WebElement> headerCells = tr.findElements(By.xpath("td"));
+
+        WebElement desiredCell = tr.findElement(Locator.xpath("td").withText(cellText).toBy());
+
+        return headerCells.indexOf(desiredCell);
+    }
+}
