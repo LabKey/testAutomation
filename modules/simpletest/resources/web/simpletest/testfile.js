03cebf81f533ada8e88573e471cd2b7e85e61795
Nick Kerr <nicka@labkey.com>
2014-07-30 03:21:28 +0000

    Fix more Ext dependent views.
    SVN r33654 |2014-07-30 03:21:28 +0000

--- modules/simpletest/resources/web/simpletest/testfile.js
+++ modules/simpletest/resources/web/simpletest/testfile.js
@@ -5,13 +5,11 @@
  */
 //module.xml should require this module's context to be present
 //if not, the alert should fail the test
-Ext.onReady(function(){
-    if(LABKEY.getModuleContext('simpletest') === null)
-        alert('Simpletest module context not present');
+if(LABKEY.getModuleContext('simpletest') === null)
+    alert('Simpletest module context not present');
 
-    if(LABKEY.getModuleContext('core') === null)
-        alert('Core module context not present');
+if(LABKEY.getModuleContext('core') === null)
+    alert('Core module context not present');
 
-    //we also test for this value
-    LABKEY.moduleContext.simpletest.scriptLoaded = true;
-});
+//we also test for this value
+LABKEY.moduleContext.simpletest.scriptLoaded = true;
