c58a6b21c0881101e6e79b86b09c9665e701a7e8
Ryan Standley <rstandley@labkey.com>
2014-08-28 23:20:19 +0000

    adding oconnor category
    SVN r34123 |2014-08-28 23:20:19 +0000

---
+++ src/org/labkey/test/categories/OConnor.java
@@ -0,0 +1,12 @@
+package org.labkey.test.categories;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: RyanS
+ * Date: 8/19/14
+ * Time: 3:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OConnor extends Test
+{
+}
