eb559f55f1fd7da502c2a84b7672e9e15b36ec03
Adam Rauch <adam@labkey.com>
2014-10-27 15:31:52 +0000

    Update licenses
    SVN r35010 |2014-10-27 15:31:52 +0000

--- data/api/folderTest.js
+++ data/api/folderTest.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 // Designed to be run by JavaScriptExecutor#executeAsyncScript
 var callback = arguments[arguments.length - 1]; // See WebDriver documentation
 
