bf4b2b17c5fa5e9bf64d69b3139401c7815aa8de
Aaron Robinson <aaronr@labkey.com>
2014-10-30 22:58:07 +0000

    Ignoring ERR_CACHE_MISS in chrome which occurs on any page with a filecontent webpart.
    SVN r35111 |2014-10-30 22:58:07 +0000

--- chromeextensions/jsErrorChecker/jserrors.js
+++ chromeextensions/jsErrorChecker/jserrors.js
@@ -5,34 +5,35 @@
  */
 window.addEventListener('error', function(text, url, line)
 {
-    var excludedErrors = ["ext-all-sandbox"];
+    // NOTE: https://code.google.com/p/chromium/issues/detail?id=424599
+    var excludedErrors = ["ext-all-sandbox", "ERR_CACHE_MISS"];
 
     // Event parsing logic from https://github.com/barbushin/javascript-error-notifier
-	if(text.filename) {
-		url = text.filename;
-	}
-	if(text.lineno) {
-		line = text.lineno;
-	}
-	if(text.target.chrome && !url && !line) {
-		return; // ignore handling Google Chrome extensions errors
-	}
-	if(text.message) {
-		text = text.message;
-	}
-	else {
-		// draft fix of http://code.google.com/p/chromium/issues/detail?id=8939
-		if(text.target && text.target.src) {
-			url = window.location.href;
-			text = 'File not found: ' + text.target.src;
-		}
-	}
-	text = (typeof(text) != 'string' ? 'Unknown JavaScript error' : text.replace(/^Uncaught /g, ''));
+    if(text.filename) {
+        url = text.filename;
+    }
+    if(text.lineno) {
+        line = text.lineno;
+    }
+    if(text.target.chrome && !url && !line) {
+        return; // ignore handling Google Chrome extensions errors
+    }
+    if(text.message) {
+        text = text.message;
+    }
+    else {
+        // draft fix of http://code.google.com/p/chromium/issues/detail?id=8939
+        if(text.target && text.target.src) {
+            url = window.location.href;
+            text = 'File not found: ' + text.target.src;
+        }
+    }
+    text = (typeof(text) != 'string' ? 'Unknown JavaScript error' : text.replace(/^Uncaught /g, ''));
     
     var error = text + "\n" + url + "\n" + line;
-	
+
     for (var i = excludedErrors.length - 1; i >= 0; i--) {
-		if (error.indexOf(excludedErrors[i]) != -1) {return;}
+        if (error.indexOf(excludedErrors[i]) != -1) {return;}
     }
 
     chrome.runtime.sendMessage({command: 'get'}, function(response){
