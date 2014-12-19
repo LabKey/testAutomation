3d686908b9a2f9c2692981c95358b814f4fd01b6
Aaron Robinson <aaronr@labkey.com>
2014-12-19 00:40:00 +0000

    Spec 22010: Show annotations in QC plots - Added new page and component objects. - Extended test to populate both QCAnnotations and QCAnnotationTypes.
    SVN r35678 |2014-12-19 00:40:00 +0000

---
+++ src/org/labkey/test/components/InsertPage.java
@@ -0,0 +1,50 @@
+package org.labkey.test.components;
+
+import org.labkey.test.BaseWebDriverTest;
+import org.labkey.test.Locator;
+
+/*
+ * Copyright (c) 2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class InsertPage
+{
+    protected BaseWebDriverTest _test;
+    protected String _title;
+
+    public InsertPage(BaseWebDriverTest test, String title)
+    {
+        _test = test;
+        _title = title;
+        waitForReady();
+    }
+
+
+    // consider getTitle here...
+
+    protected void waitForReady() {}
+
+    protected Elements elements()
+    {
+        return new Elements();
+    }
+
+    protected class Elements
+    {
+        public Locator.XPathLocator title = Locator.tagWithId("span", "labkey-nav-trail-current-page");
+        public Locator.XPathLocator body = Locator.tagWithClass("table", "labkey-proj");
+        public Locator.XPathLocator submit = body.append(Locator.lkButton("Submit"));
+        public Locator.XPathLocator cancel = body.append(Locator.lkButton("Cancel"));
+    }
+}
