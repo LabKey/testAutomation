b9658a8f32b446626eebe4a5eb06180491ea331b
Cory Nathe <cnathe@labkey.com>
2015-01-30 20:34:39 +0000

    OlapTestJson page component to use in HICORTest
    SVN r36282 |2015-01-30 20:34:39 +0000

---
+++ src/org/labkey/test/pages/OlapTestJson.java
@@ -0,0 +1,69 @@
+package org.labkey.test.pages;
+
+import org.labkey.test.BaseWebDriverTest;
+import org.labkey.test.Locator;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by cnathe on 1/30/2015.
+ */
+public class OlapTestJson
+{
+    private BaseWebDriverTest _test;
+
+    public OlapTestJson(BaseWebDriverTest test)
+    {
+        _test = test;
+    }
+
+    public void goToPage(String path, String configId, String schemaName, String cubeName)
+    {
+        _test.beginAt("/olap/" + path + "/testJson.view?configId=" + configId + "&schemaName=" + schemaName + "&cubeName=" + cubeName);
+    }
+
+    public void submitQueryAsJson(String query)
+    {
+        clear();
+        setType("json");
+        setQuery(query);
+        submit();
+
+        Locator.XPathLocator table = Locator.tagWithClass("table", "labkey-data-region");
+        _test.waitForElement(table);
+    }
+
+    public void compareResults(String[][] results)
+    {
+        Locator.XPathLocator table = Locator.tagWithClass("table", "labkey-data-region");
+
+        for (int i = 0; i < results.length; i++)
+        {
+            for (int j = 0; j < results[i].length; j++)
+            {
+                assertEquals(results[i][j], _test.getTableCellText(table, i, j));
+            }
+        }
+    }
+
+    private void setType(String value)
+    {
+        _test.selectOptionByValue(Locator.name("type"), value);
+    }
+
+    private void setQuery(String query)
+    {
+        _test.setFormElement(Locator.name("query"), query);
+    }
+
+    private void submit()
+    {
+        _test.click(Locator.inputById("submitbtn"));
+    }
+
+    private void clear()
+    {
+        _test.click(Locator.inputById("clearbtn"));
+        _test.waitForElementToDisappear(Locator.tagWithClass("table", "labkey-data-region"));
+    }
+}
