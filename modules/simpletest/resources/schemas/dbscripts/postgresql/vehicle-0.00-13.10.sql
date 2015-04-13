9f807b968ed824c73758c52b4a7ad965cca84bc3
Adam Rauch <adam@labkey.com>
2015-04-13 20:02:25 +0000

    13.10 is now the earliest version that 15.20 will upgrade.
    Change all bootstrap scripts to *-0.00-13.10.sql. Eliminate 13.10 java upgrade code (just nab). 
    
    Obsolete all the 14.30 incremental scripts
    
    Create 14.30-15.10 rollup script for peptideInventory, missed for 15.10 release because of a casing discrepancy
    
    SVN r37471 |2015-04-13 20:02:25 +0000

---
+++ modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-0.00-13.10.sql
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2009-2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* vehicle-0.00-1.00.sql */
+
+CREATE SCHEMA vehicle;
+
+CREATE TABLE vehicle.Colors
+(
+    Name VARCHAR(30) NOT NULL,
+    Hex TEXT,
+
+    CONSTRAINT PK_Colors PRIMARY KEY (Name)
+);
+
+CREATE TABLE vehicle.Manufacturers
+(
+    RowId SERIAL NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+
+    CONSTRAINT PK_Manufacturers PRIMARY KEY (RowId)
+);
+
+CREATE TABLE vehicle.Models
+(
+    RowId SERIAL NOT NULL,
+    ManufacturerId INT NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+
+    CONSTRAINT PK_Models PRIMARY KEY (RowId),
+    CONSTRAINT FK_Models_Manufacturers FOREIGN KEY (ManufacturerId) REFERENCES vehicle.Manufacturers(RowId)
+);
+
+CREATE TABLE vehicle.Vehicles
+(
+    RowId SERIAL NOT NULL,
+    Container ENTITYID NOT NULL,
+    CreatedBy USERID NOT NULL,
+    Created TIMESTAMP NOT NULL,
+    ModifiedBy USERID NOT NULL,
+    Modified TIMESTAMP NOT NULL,
+
+    ModelId INT NOT NULL,
+    Color VARCHAR(30) NOT NULL,
+
+    ModelYear INT NOT NULL,
+    Milage INT NOT NULL,
+    LastService TIMESTAMP NOT NULL,
+
+    CONSTRAINT PK_Vehicles PRIMARY KEY (RowId),
+    CONSTRAINT FK_Vehicles_Models FOREIGN KEY (ModelId) REFERENCES vehicle.Models(RowId),
+    CONSTRAINT FK_Vehicles_Colors FOREIGN KEY (Color) REFERENCES vehicle.Colors(Name)
+);
+
+/* vehicle-11.30-12.10.sql */
+
+-- add container constraint
+ALTER TABLE vehicle.Vehicles
+    ADD CONSTRAINT FK_Vehicles_Container FOREIGN KEY (Container) REFERENCES core.Containers (EntityId);
+
+/* vehicle-12.30-13.10.sql */
+
+CREATE TABLE vehicle.emissiontest (
+  rowid SERIAL,
+  name varchar(100),
+  container entityid,
+  parentTest int,
+  vehicleId int,
+  result boolean,
+
+  CONSTRAINT PK_emissiontest PRIMARY KEY (rowid),
+  CONSTRAINT FK_emissiontest_container FOREIGN KEY (container) REFERENCES core.containers (entityid)
+);
