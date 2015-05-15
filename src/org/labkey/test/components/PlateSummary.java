8fe34669821cc9c297ca1194d1833ac8112f4013
Ryan Standley <rstandley@labkey.com>
2015-05-15 21:49:38 +0000

    new component wrapper for elispot plate summary
    SVN r37989 |2015-05-15 21:49:38 +0000

---
+++ src/org/labkey/test/components/PlateSummary.java
@@ -0,0 +1,86 @@
+package org.labkey.test.components;
+
+import org.labkey.test.BaseWebDriverTest;
+import org.labkey.test.Locator;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlateSummary
+{
+    protected BaseWebDriverTest _test;
+    protected Measurement _measurement;
+    protected int _index;
+
+    public PlateSummary(BaseWebDriverTest test, int plateSummaryIndex)
+    {
+        _test = test;
+        _test.waitForElement(Locator.css("#plate-summary-div-1 table"));
+        _measurement = Measurement.SPOTCOUNT;
+        _index = plateSummaryIndex;
+    }
+
+    public String getCellValue(int row, int col)
+    {
+        return getRowValues(row).get(col);
+    }
+
+    public List<String> getRowValues(int row)
+    {
+        //first row is header, so add 1 to skip it
+        row = row + 1;
+        List<String> values = new ArrayList<>();
+        List<WebElement> rowEl = Locator.xpath("(//table[@class='plate-summary-grid'])[" + _index +  "]//tr[" + row + "]/td/div/a[@class='" + _measurement.locatorClass + "']").findElements(_test.getDriver());
+        for(WebElement el : rowEl)
+        {
+            values.add(el.getText());
+        }
+        return values;
+    }
+
+    public List<String> getColumnValues(int col)
+    {
+        List<String> values = new ArrayList<>();
+        for(int row = 2; row < getRowCount(); row++)
+        {
+            values.add(getRowValues(row).get(col));
+        }
+        return values;
+    }
+
+    public void selectSampleWellGroup(String sampleWellGroup)
+    {
+        _test._ext4Helper.selectRadioButton(sampleWellGroup);
+    }
+
+    public void selectAntigenWellGroup(String antigenWellGroup)
+    {
+        _test._ext4Helper.selectRadioButton(antigenWellGroup);
+    }
+
+    public void selectMeasurement(Measurement measurement)
+    {
+        _test._ext4Helper.selectRadioButton(measurement.label);
+        _measurement = measurement;
+        //_test.longWait().until(ExpectedConditions.visibilityOfElementLocated(Locator.tagWithClass("a", measurement.locatorClass).toBy()));
+        _test.waitForElement(Locator.tagWithClass("a", measurement.locatorClass).notHidden());
+    }
+
+    private int getRowCount()
+    {
+        return Locator.xpath("(//table[@class='plate-summary-grid'])[" + _index + "]//tr").findElements(_test.getDriver()).size();
+    }
+
+    public enum Measurement
+    {
+        SPOTCOUNT("labkey-cls-spotcount", "Spot Count"), ACTIVITY("labkey-cls-activity", "Activity"), INTENSITY("labkey-cls-intensity", "Intensity");
+        public String locatorClass;
+        public String label;
+        private Measurement(String locatorClass, String label)
+        {
+            this.locatorClass = locatorClass;
+            this.label = label;
+        }
+    }
+}
