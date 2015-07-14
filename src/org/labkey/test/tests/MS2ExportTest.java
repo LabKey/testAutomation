5f5f72c1a63a4a61f7fe7755d9b6d7fd5587e3f9
Tym Loving <tyml@labkey.com>
2015-07-14 23:24:28 +0000

    Issue 22265 & 22138 automation, plus Excel bulk export test
    SVN r38778 |2015-07-14 23:24:28 +0000

--- src/org/labkey/test/tests/MS2ExportTest.java
+++ src/org/labkey/test/tests/MS2ExportTest.java
@@ -15,12 +15,24 @@
  */
 package org.labkey.test.tests;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.experimental.categories.Category;
 import org.labkey.test.Locator;
 import org.labkey.test.categories.DailyA;
 import org.labkey.test.categories.MS2;
 import org.labkey.test.util.DataRegionTable;
+import org.labkey.test.util.ExcelHelper;
+import org.labkey.test.util.LabKeyExpectedConditions;
 import org.labkey.test.util.LogMethod;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Category({MS2.class, DailyA.class})
 public class MS2ExportTest extends AbstractMS2ImportTest
@@ -39,7 +51,69 @@ public class MS2ExportTest extends AbstractMS2ImportTest
     protected void verifyMS2()
     {
         validateBulkExport();
+        validatePeptideComparisonExport();
     }
+
+    private void validatePeptideComparisonExport()
+    {
+        clickAndWait(Locator.linkWithText("MS2 Dashboard"));
+        clickAndWait(Locator.linkWithText("MS2 Runs"));
+        DataRegionTable RunsTable = new DataRegionTable("MS2SearchRuns", this);
+        RunsTable.checkAllOnPage();
+        waitForElement(Locator.lkButton("Compare"), 2000);
+        clickButton("Compare", 0);
+        clickAndWait(Locator.linkWithText("Peptide"));
+        clickButton("Compare");
+        assert (getCompExcelExportRowCount()==114);
+
+
+    }
+    private int getCompExcelExportRowCount()
+    {
+        File export = compExcelExport();
+        try
+        {
+            Workbook wb = ExcelHelper.create(export);
+            Sheet sheet = wb.getSheetAt(0);
+            return sheet.getLastRowNum(); // +1 for zero-based, -1 for header row
+        }
+        catch (IOException | InvalidFormatException fail)
+        {
+            throw new RuntimeException("Error reading exported grid file", fail);
+        }
+    }
+    private int getBulkExcelExportRowCount()
+    {
+        File export = bulkExcelExport();
+        try
+        {
+            Workbook wb = ExcelHelper.create(export);
+            Sheet sheet = wb.getSheetAt(0);
+            return sheet.getLastRowNum(); // +1 for zero-based, -1 for header row
+        }
+        catch (IOException | InvalidFormatException fail)
+        {
+            throw new RuntimeException("Error reading exported grid file", fail);
+        }
+    }
+
+
+    private File bulkExcelExport()
+    {
+        checkRadioButton(Locator.radioButtonByNameAndValue("exportFormat", "Excel"));
+        selectOptionByText(Locator.name("viewParams"), "<Standard View>");
+        Locator.XPathLocator buttonLocator = getButtonLocator("Export");
+        return clickAndWaitForDownload(buttonLocator,1)[0];
+    }
+
+    private File compExcelExport()
+    {
+        clickButton("Export", 0);
+        _extHelper.clickSideTab("Excel");
+        return clickAndWaitForDownload(Locator.lkButton("Export to Excel"), 1)[0];
+    }
+
+
 
     private void validateBulkExport()
     {
@@ -49,6 +123,8 @@ public class MS2ExportTest extends AbstractMS2ImportTest
         clickButton("MS2 Export");
 
         assertTextPresent("BiblioSpec");
+        //int rc = getExcelExportRowCount();
+        assert (getBulkExcelExportRowCount()==116);
 
         Runnable tsvPeptideValidator = new Runnable()
         {
