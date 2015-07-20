2c602dc320f4cf026ab4a4c4ace7280af0f5fb0f
Adam Rauch <adam@labkey.com>
2015-07-20 17:32:58 +0000

    Update licenses
    SVN r38832 |2015-07-20 17:32:58 +0000

--- data/api/maxRows.js
+++ data/api/maxRows.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 window.gridView = new LABKEY.ext.EditorGridPanel({
     store: new LABKEY.ext.Store({
         schemaName : 'lists',
