91887cadb08c04be4abac79fcd00f8411b52c7e3
Jim Piper <jimp@labkey.com>
2016-06-02 16:27:10 +0000

    Spec 26228: automated test for Data Views report ordering
    SVN r44441 |2016-06-02 16:27:10 +0000

---
+++ src/org/labkey/test/tests/DataViewsReportOrderingTest.java
@@ -0,0 +1,187 @@
+package org.labkey.test.tests;
+
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.labkey.test.BaseWebDriverTest;
+import org.labkey.test.Locator;
+import org.labkey.test.TestTimeoutException;
+import org.labkey.test.categories.DailyA;
+import org.labkey.test.TestFileUtils;
+import org.labkey.test.components.ext4.Window;
+import org.labkey.test.util.PortalHelper;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+@Category({DailyA.class})
+public class DataViewsReportOrderingTest extends BaseWebDriverTest
+{
+    private static final String ORIGINAL_WEBPART_TITLE = "Data Views";
+    private final PortalHelper _portalHelper = new PortalHelper(this);
+
+    @Override
+    protected void doCleanup(boolean afterTest) throws TestTimeoutException
+    {
+        super.doCleanup(afterTest);
+    }
+
+    @BeforeClass
+    public static void setupProject()
+    {
+        DataViewsReportOrderingTest init = (DataViewsReportOrderingTest) getCurrentTest();
+        init.doSetup();
+    }
+
+    private void doSetup()
+    {
+        _containerHelper.createProject(getProjectName(), "Study");
+        importFolderFromZip(TestFileUtils.getSampleData("studies/LabkeyDemoStudy.zip"));
+    }
+
+    @Before
+    public void preTest()
+    {
+        goToProjectHome();
+    }
+
+    @Test
+    public void testReportOrdering()
+    {
+        // test by user the Reorder Reports window to reverse the list of reports for each category that has reports
+
+        // navigate to Data Views web part
+        clickAndWait(Locator.linkContainingText("Clinical and Assay Data"));
+
+        // navigate to Manage Views page
+        _portalHelper.clickWebpartMenuItem(ORIGINAL_WEBPART_TITLE, false, "Manage Views");
+
+        // bring up the Reorder Reports popup
+        click(Locator.linkContainingText("Reorder Reports And Charts"));
+
+        // confirm popup appears
+        _extHelper.waitForExtDialog("Reorder Reports and Charts");
+
+        // use the ReorderReportsWindow component to reverse the order of the reports in each category
+        ReorderReportsWindow reorderReportsWindow = new ReorderReportsWindow(getDriver());
+        HashMap categorReportsOriginalOrder = new HashMap<String, List<String>>();
+        List<WebElement> categories = reorderReportsWindow.getCategories();
+        for (WebElement category : categories)
+        {
+            String categoryText = category.getText();
+            List<String> reports = reorderReportsWindow.getReportsForCategory(categoryText);
+            String[] reportsA = new String[reports.size()];
+            reportsA = reports.toArray(reportsA);
+            if (null != reportsA && reportsA.length > 0)
+            {
+                assertTextPresentInThisOrder(reportsA);  // confirm reports were in original order on Manage Views page
+            }
+            categorReportsOriginalOrder.put(categoryText, reports);
+            // reverse the ordering of reports in this category
+            for (int x = 1; x < reports.size(); x++)
+            {
+                reorderReportsWindow.dragAndDrop(reports.get(x), reports.get(x-1));   // move each report to the postion of the first report
+            }
+        }
+
+        // when the Done button is clicked the Manage Views page will update to show reversed reports
+        reorderReportsWindow.done();
+        reorderReportsWindow.waitForClose();
+        _ext4Helper.waitForMaskToDisappear();
+
+        // verify that the Manage Views displays the reports in the reverse order from the original
+        for (Object key : categorReportsOriginalOrder.keySet())
+        {
+            // determine what the reverse order should be for the reports in each category and verify that it appears on Manage Veiws page
+            String[] reportsBackwards = reverseReports((List<String>)categorReportsOriginalOrder.get(key));
+            if (null != reportsBackwards && reportsBackwards.length > 0)
+            {
+                assertTextPresentInThisOrder(reportsBackwards);  // confirm reports in reverse order on Manage Views page
+            }
+        }
+
+    }
+
+    private String[] reverseReports(List<String> reportsOriginalOrder)
+    {
+        String[] reverseReports = new String[reportsOriginalOrder.size()];
+        for (int x = 0; x < reportsOriginalOrder.size(); x++)
+        {
+            reverseReports[x] = reportsOriginalOrder.get(reportsOriginalOrder.size() - x -1);
+        }
+        return reverseReports;
+    }
+
+    @Override
+    protected BrowserType bestBrowser()
+    {
+        return BrowserType.CHROME;
+    }
+
+    @Override
+    protected String getProjectName()
+    {
+        return "DataViewsReportOrderingTest Project";
+    }
+
+    @Override
+    public List<String> getAssociatedModules()
+    {
+        return Arrays.asList("<module>");
+    }
+
+    public static class ReorderReportsWindow extends Window
+    {
+        protected ReorderReportsWindow(WebDriver driver)
+        {
+            super(Window().withTitleContaining("Reorder Reports and Charts"), driver);
+        }
+
+        public List<WebElement> getCategories()
+        {
+            return findElements(Locator.xpath("//tr/td/div/span").withClass("x4-tree-node-text"));
+        }
+
+        public List<String> getReportsForCategory(String category)
+        {
+            selectCategory(category);
+            List<WebElement> reportWebElements = findElements(Locator.xpath("//tr/td/div").withClass("x4-grid-cell-inner").withoutClass("x4-grid-cell-inner-treecolumn"));
+            List<String> reportStrings = new ArrayList<>();
+            for (WebElement reportWebElement : reportWebElements)
+            {
+                reportStrings.add(reportWebElement.getText());
+            }
+            return reportStrings;
+        }
+
+        public void selectCategory(String category)
+        {
+            Actions builder = new Actions(getWrapper().getDriver());
+            builder.click(findElement(Locator.xpath("//tr/td/div/span").withClass("x4-tree-node-text").withText(category))).build().perform();
+        }
+
+        public void done()
+        {
+            clickButton("Done", 0);
+        }
+
+        public void dragAndDrop(String fromReport, String toReport)
+        {
+            WebElement fromEl = findElement(Locator.xpath("//tr/td/div").withClass("x4-grid-cell-inner").withoutClass("x4-grid-cell-inner-treecolumn").withText(fromReport));
+            WebElement toEl   = findElement(Locator.xpath("//tr/td/div").withClass("x4-grid-cell-inner").withoutClass("x4-grid-cell-inner-treecolumn").withText(toReport));
+            dragAndDrop(fromEl, toEl);
+        }
+
+        public void dragAndDrop(WebElement fromEl, WebElement toEl)
+        {
+            Actions builder = new Actions(getWrapper().getDriver());
+            builder.dragAndDrop(fromEl, toEl).pause(500).build().perform(); // needs to pause a bit for ui move animation otherwise test fails
+        }
+    }
+
+}
