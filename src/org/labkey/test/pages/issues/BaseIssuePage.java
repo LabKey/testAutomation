de2258bc7bb601d2d67120c333d7ac02cb9dd69e
Trey Chadick <tchad@labkey.com>
2016-06-06 22:42:46 +0000

    Missed file
    SVN r44514 |2016-06-06 22:42:46 +0000

--- src/org/labkey/test/pages/issues/BaseIssuePage.java
+++ src/org/labkey/test/pages/issues/BaseIssuePage.java
@@ -1,13 +1,19 @@
 package org.labkey.test.pages.issues;
 
-import org.labkey.test.components.ComponentElements;
+import com.google.common.collect.ImmutableList;
+import org.labkey.test.Locator;
 import org.labkey.test.components.labkey.FormItem;
 import org.labkey.test.pages.LabKeyPage;
-import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.labkey.test.components.labkey.ReadOnlyFormItem.ReadOnlyFormItem;
 
@@ -80,6 +86,11 @@ public abstract class BaseIssuePage extends LabKeyPage
         return elements().resolvedDate.getValue();
     }
 
+    public List<IssueComment> getComments()
+    {
+        return elements().getComments();
+    }
+
     protected Elements elements()
     {
         if (_elements == null)
@@ -89,14 +100,8 @@ public abstract class BaseIssuePage extends LabKeyPage
 
     protected abstract Elements newElements();
 
-    protected class Elements extends ComponentElements
+    protected class Elements extends ElementCache
     {
-        @Override
-        protected SearchContext getContext()
-        {
-            return getDriver();
-        }
-
         protected FormItem<String> status = ReadOnlyFormItem(getDriver()).withLabel("Status").findWhenNeeded();
         protected FormItem<String> assignedTo = ReadOnlyFormItem(getDriver()).withLabel("Assigned To").findWhenNeeded();
         protected FormItem<String> priority = ReadOnlyFormItem(getDriver()).withLabel("Pri").findWhenNeeded();
@@ -110,12 +115,86 @@ public abstract class BaseIssuePage extends LabKeyPage
         protected FormItem<String> closedDate = ReadOnlyFormItem(getDriver()).withLabel("Closed").findWhenNeeded();
 
         private Map<String, FormItem> customFormItems = new TreeMap<>();
-
         protected FormItem getCustomFormItem(String label)
         {
             if (!customFormItems.containsKey(label))
                 customFormItems.put(label, ReadOnlyFormItem(getDriver()).withLabel(label).findWhenNeeded());
             return customFormItems.get(label);
+        }
+
+        private List<IssueComment> issueComments;
+        protected List<IssueComment> getComments()
+        {
+            if (issueComments == null)
+            {
+                List<WebElement> commentEls = Locator.css("div.currentIssue").findElements(this);
+                issueComments = new ArrayList<>();
+                for (WebElement commentEl : commentEls)
+                {
+                    issueComments.add(new IssueComment(commentEl));
+                }
+                issueComments = ImmutableList.copyOf(issueComments);
+            }
+
+            return issueComments;
+        }
+    }
+
+    public class IssueComment
+    {
+        private final WebElement component;
+        private String user;
+        private String timestamp;
+        private String comment;
+        private Map<String, String> fieldChanges;
+
+        IssueComment(WebElement component)
+        {
+            this.component = component;
+        }
+
+        public String getUser()
+        {
+            if (user == null)
+                user = Locator.css(".comment-created-by").findElement(component).getText();
+            return user;
+        }
+
+        public String getTimestamp()
+        {
+            if (timestamp == null)
+                timestamp = Locator.css(".comment-created").findElement(component).getText();
+            return timestamp;
+        }
+
+        public String getComment()
+        {
+            if (comment == null)
+                comment = Locator.css(".labkey-wiki").findElement(component).getText();
+            return comment;
+        }
+
+        public Map<String, String> getFieldChanges()
+        {
+            if (fieldChanges == null)
+            {
+                fieldChanges = new HashMap<>();
+                List<String> changes = getTexts(Locator.css(".issues-Changes tr").findElements(component));
+
+                for (String change : changes)
+                {
+                    Pattern pattern = Pattern.compile("(.+)\uc2bb(.*)");
+                    Matcher matcher = pattern.matcher(change);
+
+                    if (matcher.find())
+                    {
+                        String field = matcher.group(1).trim();
+                        String value = matcher.group(2).trim();
+                        fieldChanges.put(field, value);
+                    }
+                }
+            }
+            return fieldChanges;
         }
     }
 }
