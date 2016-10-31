24929dab2b0eba367aa08be18b8bb386a4fb712f
Trey Chadick <tchad@labkey.com>
2016-10-31 20:26:08 +0000

    Fix Locator to work with Argos
    SVN r47372 |2016-10-31 20:26:08 +0000

--- src/org/labkey/test/util/Ext4Helper.java
+++ src/org/labkey/test/util/Ext4Helper.java
@@ -908,7 +908,7 @@ public class Ext4Helper
             {
                 loc = loc.append(Locator.tagWithClass("*", cls));
             }
-            return loc.append(getGridRow(columnVal).withText(columnVal));
+            return loc.append(getGridRow(columnVal));
         }
 
         public static Locator.XPathLocator getGridRow()
