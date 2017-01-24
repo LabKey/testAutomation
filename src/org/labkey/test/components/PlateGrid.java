02272da62021fb72012198bde4b40eb6a1ed880f
Dan Duffek <danield@labkey.com>
2017-01-24 01:37:28 +0000

    #28563 NabQC Test Automation
    SVN r48864 |2017-01-24 01:37:28 +0000

---
+++ src/org/labkey/test/components/PlateGrid.java
@@ -0,0 +1,125 @@
+package org.labkey.test.components;
+
+import org.labkey.test.Locator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PlateGrid
+{
+    public final String EXCLUDED_PLATE_SUMMARY_XPATH = "//table[contains(@class, 'plate-summary')]//tbody//tr//th[contains(@class, 'labkey-data-region-header-container')][text()='$']/ancestor::table[contains(@class, 'plate-summary')]";
+
+    private WebDriver _driver;
+    private WebElement[][] _gridValues;
+    private Map<String, Integer> _rowsIndex;
+    private Map<String, Integer> _colsIndex;
+    private String _plateGridXpath;
+
+    private int ROW_OFFSET = 1;
+    private int COL_OFFSET = 1;
+
+    public PlateGrid(WebDriver driver, String plateId)
+    {
+        _driver = driver;
+        doInit(EXCLUDED_PLATE_SUMMARY_XPATH.replace("$", plateId));
+    }
+
+    public PlateGrid(WebDriver driver)
+    {
+        _driver = driver;
+        doInit("//table[contains(@class, 'plate-summary')]");
+    }
+
+    private void doInit(String xPath)
+    {
+        int rows, cols;
+
+        _plateGridXpath = xPath;
+
+        // If there is a header the ROW_OFFSET needs to be adjusted.
+        ROW_OFFSET = Locator.findElements(_driver, Locator.xpath(_plateGridXpath + "//th")).size() + ROW_OFFSET;
+
+        rows = Locator.findElements(_driver, Locator.xpath(_plateGridXpath)).get(0).findElements(By.tagName("tr")).size() - ROW_OFFSET;
+        cols = Locator.findElements(_driver, Locator.xpath(_plateGridXpath)).get(0).findElements(By.tagName("tr")).get(1).findElements(By.tagName("td")).size() - COL_OFFSET;
+
+        _gridValues = new WebElement[cols][rows];
+
+        char rowName = 'A';
+        _rowsIndex = new HashMap<>();
+        for(int i=0; i<rows; i++)
+        {
+            _rowsIndex.put(Character.toString(rowName), i);
+            rowName++;
+        }
+
+        _colsIndex = new HashMap<>();
+        for(int j=0; j<cols; j++)
+        {
+            _colsIndex.put(Integer.toString(j+1), j);
+        }
+
+        _gridValues = populateGrid();
+    }
+
+    private WebElement[][] populateGrid()
+    {
+        int rowCount, columnCount;
+        List<WebElement> rows = Locator.findElements(_driver, Locator.xpath(_plateGridXpath)).get(0).findElements(By.tagName("tr"));
+        rowCount = rows.size() - ROW_OFFSET;
+        columnCount = Locator.findElements(_driver, Locator.xpath(_plateGridXpath)).get(0).findElements(By.tagName("tr")).get(1).findElements(By.tagName("td")).size() - COL_OFFSET;
+
+        WebElement[][] plateGrid = new WebElement[rowCount][columnCount];
+
+        for(int row = ROW_OFFSET; row < rows.size(); row++)
+        {
+            List<WebElement> cols = rows.get(row).findElements(By.tagName("td"));
+            for(int col = COL_OFFSET; col < cols.size(); col++ )
+            {
+                plateGrid[row- ROW_OFFSET][col- COL_OFFSET] = cols.get(col);
+            }
+        }
+
+        return plateGrid;
+    }
+
+    public WebElement getCellElement(String row, String col)
+    {
+
+        return _gridValues[_rowsIndex.get(row)][_colsIndex.get(col)];
+    }
+
+    public String getCellValue(String row, String col)
+    {
+        return _gridValues[_rowsIndex.get(row)][_colsIndex.get(col)].getText();
+    }
+
+    public boolean isCellExcluded(String row, String col)
+    {
+        String classValue = _gridValues[_rowsIndex.get(row)][_colsIndex.get(col)].getAttribute("class");
+        return classValue.toLowerCase().contains("excluded");
+    }
+
+    public List<WebElement> getExcludedCells()
+    {
+        return Locator.findElements(_driver, Locator.xpath(_plateGridXpath + "//td[contains(@class, 'excluded')]"));
+    }
+
+    public List<String> getExcludedValues()
+    {
+        List<String> values = new ArrayList<>();
+        List<WebElement> excludeCells =  getExcludedCells();
+
+        for(WebElement we : excludeCells)
+        {
+            values.add(we.getText());
+        }
+
+        return values;
+    }
+
+}
---
+++ src/org/labkey/test/pages/assay/RunQCPage.java
@@ -0,0 +1,182 @@
+package org.labkey.test.pages.assay;
+
+import org.labkey.test.Locator;
+import org.labkey.test.pages.LabKeyPage;
+import org.labkey.test.components.PlateGrid;
+import org.labkey.test.selenium.LazyWebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunQCPage<EC extends RunQCPage.ElementCache> extends LabKeyPage<EC>
+{
+    WebDriver _driver;
+
+    public RunQCPage(WebDriver driver){
+        super(driver);
+        _driver = driver;
+    }
+
+    public WebElement getSpecimenSection(String sectionTitle)
+    {
+        String xpath = elementCache().DILUTION_SUMMARY_XPATH + "//tbody//tr//td[text()='" + sectionTitle + "']";
+        WebElement weSpecimen = new LazyWebElement(Locator.xpath(xpath), _driver);
+        return weSpecimen;
+    }
+
+    public void clickNext()
+    {
+        elementCache().next.click();
+        waitForElementToBeVisible(Locator.xpath("//div[@id='nabQCDiv']//div[contains(@class, 'x4-fit-item')][2]"));
+        sleep(500);
+    }
+
+    public void clickPrevious()
+    {
+        elementCache().previous.click();
+        waitForElementToBeVisible(Locator.xpath("//div[@id='nabQCDiv']//div[contains(@class, 'x4-fit-item')][1]"));
+        sleep(500);
+    }
+
+    public void clickCancel()
+    {
+        elementCache().cancel.click();
+        waitForPage();
+        waitForElement(Locator.xpath("//table[contains(@class, 'plate-summary')]"));
+    }
+
+    public void clickFinish()
+    {
+        elementCache().finish.click();
+        waitForPage();
+        waitForElement(Locator.xpath("//table[contains(@class, 'plate-summary')]"));
+        sleep(1000);
+    }
+
+    public void selectPlateItemsToIgnore(String plateTitle, List<String> valuesToIgnore)
+    {
+        String xpath = elementCache().PLATE_CONTROLS_VALUES_XPATH.replace("$", plateTitle);
+        for(String value : valuesToIgnore)
+        {
+            click(Locator.xpath(xpath + "//label[text()='" + value + "']"));
+        }
+    }
+
+    public void selectDilutionItemsToIgnore(String sectionTitle, List<String> valuesToIgnore)
+    {
+        String xpath = elementCache().DILUTION_SUMMARY_VALUES_XPATH.replace("$", sectionTitle);
+        for(String value : valuesToIgnore)
+        {
+            click(Locator.xpath(xpath + "//label[text()='" + value + "']"));
+        }
+    }
+
+    public List<String> getValuesFromDilution(String sectionTitle)
+    {
+
+        String xpath = elementCache().DILUTION_SUMMARY_ALL_VALUES_XPATH.replace("$", sectionTitle);
+        List<String> values = new ArrayList<>();
+        List<WebElement> elements = Locator.findElements(_driver, Locator.xpath(xpath));
+        for(WebElement we : elements)
+        {
+            values.add(we.getText());
+        }
+
+        return values;
+    }
+
+    public void setExcludedComment(String row, String column, String comment)
+    {
+        WebElement excludedRow = findExcludedRow(row, column);
+        setFormElement(excludedRow.findElement(By.className("field-exclusion-comment")), comment);
+    }
+
+    public void setExcludedComment(int index, String comment)
+    {
+        List<WebElement> excludedFields = getExcludedFields();
+        setFormElement(excludedFields.get(index).findElement(By.className("field-exclusion-comment")), comment);
+    }
+
+    public String getExcludedComment(String row, String column)
+    {
+        WebElement excludedRow = findExcludedRow(row, column);
+        return getFormElement(excludedRow.findElement(By.className("field-exclusion-comment")));
+    }
+
+    public String getExcludedComment(int index)
+    {
+        List<WebElement> excludedFields = getExcludedFields();
+        return getFormElement(excludedFields.get(index).findElement(By.className("field-exclusion-comment")));
+    }
+
+    public void removeExclusion(String row, String column)
+    {
+        WebElement excludedRow = findExcludedRow(row, column);
+        click(excludedRow.findElement(By.className("remove-exclusion")));
+    }
+
+    public List<WebElement> getExcludedFields()
+    {
+        return Locator.findElements(_driver, Locator.xpath(elementCache().EXCLUDED_FIELDS_XPATH));
+    }
+
+    private WebElement findExcludedRow(String row, String column)
+    {
+        List<WebElement> excludedFields = getExcludedFields();
+        WebElement excludedRow = null;
+
+        for(WebElement we : excludedFields)
+        {
+            if((we.findElements(By.tagName("td")).get(1).getText().toLowerCase().equals(row.toLowerCase())) &&
+                    (we.findElements(By.tagName("td")).get(2).getText().toLowerCase().equals(column.toLowerCase())))
+            {
+                excludedRow = we;
+                break;
+            }
+        }
+
+        return excludedRow;
+    }
+
+    public PlateGrid getPlateGrid(String plateId)
+    {
+        return new PlateGrid(_driver, plateId);
+    }
+
+    public PlateGrid getPlateGrid()
+    {
+        return new PlateGrid(_driver);
+    }
+
+    @Override
+    protected EC newElementCache()
+    {
+        return (EC) new RunQCPage.ElementCache();
+    }
+
+
+    protected class ElementCache extends LabKeyPage.ElementCache
+    {
+        public final String NEXT_XPATH = "//span[text()='Next']/following-sibling::span[@role='img']";
+        public final String PREVIOUS_XPATH = "//span[text()='Previous']/following-sibling::span[@role='img']";
+        public final String CANCEL_XPATH = "//span[text()='Cancel']/following-sibling::span[@role='img']";
+        public final String FINISH_XPATH = "//span[text()='Finish']/following-sibling::span[@role='img']";
+        public final String RUN_SUMMARY_XPATH = "//table[contains(@class, 'run-summary')]";
+        public final String PLATE_CONTROLS_XPATH = "//table[contains(@class, 'plate-controls')]";
+        public final String PLATE_CONTROLS_VALUES_XPATH = PLATE_CONTROLS_XPATH + "//tbody//tr//th[contains(@class, 'labkey-data-region-header-container')][text()='$']/ancestor::table[contains(@class, 'plate-controls')]";
+        public final String DILUTION_SUMMARY_XPATH = "//table[contains(@class, 'dilution-summary')]";
+        public final String DILUTION_SUMMARY_VALUES_XPATH = DILUTION_SUMMARY_XPATH + "//tbody//tr//td[text()='$']/ancestor::table[contains(@class, 'dilution-summary')]//table[contains(@class, 'labkey-data-region')]";
+        public final String DILUTION_SUMMARY_ALL_VALUES_XPATH = DILUTION_SUMMARY_VALUES_XPATH + "//td[@class='dilution-checkbox']//label[contains(@class, 'x4-form-cb-label')]";
+        public final String EXCLUDED_FIELD_WELLS_XPATH = "//table[contains(@class, 'field-exclusions')]";
+        public final String EXCLUDED_FIELDS_XPATH = EXCLUDED_FIELD_WELLS_XPATH + "//tr[contains(@class, 'field-exclusion')]";
+
+        public final WebElement next = new LazyWebElement(Locator.xpath(NEXT_XPATH), _driver);
+        public final WebElement previous = new LazyWebElement(Locator.xpath(PREVIOUS_XPATH), _driver);
+        public final WebElement cancel = new LazyWebElement(Locator.xpath(CANCEL_XPATH), _driver);
+        public final WebElement finish = new LazyWebElement(Locator.xpath(FINISH_XPATH), _driver);
+
+    }
+}
