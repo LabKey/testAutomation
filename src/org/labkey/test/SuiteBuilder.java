0c219363c70b047af86e981cb7215c83546e9db4
Trey Chadick <tchad@labkey.com>
2017-03-30 22:54:19 +0000

    Make sure test Runner doesn't try to run abstract classes
    SVN r50150 |2017-03-30 22:54:19 +0000

--- src/org/labkey/test/SuiteBuilder.java
+++ src/org/labkey/test/SuiteBuilder.java
@@ -20,9 +20,17 @@ import org.labkey.remoteapi.collections.CaseInsensitiveHashMap;
 import org.labkey.test.categories.Continue;
 import org.labkey.test.categories.Test;
 import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 
+import java.lang.reflect.Modifier;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -54,18 +62,24 @@ public class SuiteBuilder
         List<String> testPackages = new ArrayList<>(Arrays.asList(System.getProperty("test.packages", "org.labkey.test").split("[^0-9A-Za-z\\._]+")));
         List<String> extraTestPackages = Arrays.asList(System.getProperty("extra.test.packages", "").split("[^0-9A-Za-z\\._]+"));
         testPackages.addAll(extraTestPackages);
+        testPackages.removeAll(Arrays.asList("", null));
 
         Set<Class<?>> tests = new HashSet<>();
 
+        FilterBuilder filterBuilder = new FilterBuilder();
+        Collection<URL> packageUrls = new HashSet<>();
         for (String testPackage : testPackages)
         {
-            if (!testPackage.trim().isEmpty())
-            {
-                Reflections reflections = new Reflections(testPackage);
-                tests.addAll(reflections.getTypesAnnotatedWith(Category.class));
-            }
+            filterBuilder.includePackage(testPackage);
+            packageUrls.addAll(ClasspathHelper.forPackage(testPackage));
         }
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .filterInputsBy(filterBuilder)
+                .setUrls(packageUrls)
+                .setScanners(new TypeAnnotationsScanner(), new SubTypesScanner()));
+        tests.addAll(reflections.getTypesAnnotatedWith(Category.class));
 
+        tests.removeIf(clz -> Modifier.isAbstract(clz.getModifiers()));
         _suites.put(Continue.class.getSimpleName(), new HashSet<>()); // Not actually a suite, used to continue interrupted suite
         _suites.put(Test.class.getSimpleName(), new HashSet<>()); // Without this, Runner will crash if 'test.packages' property is misconfigured
 
