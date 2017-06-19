09ff0b406e3da67fccb221cb11b2f20c314135e5
Trey Chadick <tchad@labkey.com>
2017-06-19 19:27:35 +0000

    Add method to remove attachments
    SVN r51742 |2017-06-19 19:27:35 +0000

--- src/org/labkey/test/components/core/FilePicker.java
+++ src/org/labkey/test/components/core/FilePicker.java
@@ -5,8 +5,12 @@ import org.labkey.test.components.WebDriverComponent;
 import org.labkey.test.components.html.FileInput;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.io.File;
+import java.util.List;
+
+import static org.labkey.test.components.ext4.Window.Window;
 
 /**
  * Wraps FilePicker managed by 'internal/webapp/util.js'
@@ -41,6 +45,37 @@ public class FilePicker extends WebDriverComponent<FilePicker.ElementCache>
         return this;
     }
 
+    public FilePicker removeAllAttachments()
+    {
+        for (WebElement link : elementCache().findRemoveLinks())
+        {
+            clickRemove(link);
+        }
+        return this;
+    }
+
+    public FilePicker removeAttachment(int index)
+    {
+        clickRemove(elementCache().findRemoveLinks().get(index));
+        return this;
+    }
+
+    /**
+     * Click provided remove link and handle confirmation
+     */
+    private void clickRemove(WebElement removeLink)
+    {
+        boolean savedAttachment = removeLink.getAttribute("onclick") != null;
+        removeLink.click();
+
+        if (savedAttachment)
+        {
+            Window(getDriver()).withTitle("Remove Attachment").waitFor().clickButton("OK", true);
+        }
+
+        getWrapper().shortWait().until(ExpectedConditions.stalenessOf(removeLink));
+    }
+
     @Override
     public WebElement getComponentElement()
     {
@@ -63,6 +98,11 @@ public class FilePicker extends WebDriverComponent<FilePicker.ElementCache>
         protected FileInput findLastAttachmentInput()
         {
             return new FileInput(Locator.tag("tbody").childTag("tr").last().append(Locator.tag("input")).findElement(this), getDriver());
+        }
+
+        protected List<WebElement> findRemoveLinks()
+        {
+            return Locator.linkWithText("remove").findElements(this);
         }
 
         // Note: pickerLink is outside of pickerTable (usually sibling)
