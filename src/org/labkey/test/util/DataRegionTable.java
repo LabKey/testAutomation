6a0ba5fd07ba5a98cae1f4bb8739dd902ef1dc55
Chris Joosse <chrisj@labkey.com>
2017-07-26 13:51:38 +0000

    UX Refresh: always use PK to find row index,
    SVN r52459 |2017-07-26 13:51:38 +0000

--- src/org/labkey/test/util/DataRegionTable.java
+++ src/org/labkey/test/util/DataRegionTable.java
@@ -33,7 +33,6 @@ import org.labkey.test.components.ext4.Window;
 import org.labkey.test.components.html.BootstrapMenu;
 import org.labkey.test.components.html.Checkbox;
 import org.labkey.test.components.html.ModalDialog;
-import org.labkey.test.components.html.TextSelect;
 import org.labkey.test.components.study.DatasetFacetPanel;
 import org.labkey.test.selenium.RefindingWebElement;
 import org.openqa.selenium.NoSuchElementException;
@@ -756,7 +755,7 @@ public class DataRegionTable extends WebDriverComponent implements WebDriverWrap
             {
                 String value = IS_BOOTSTRAP_LAYOUT ?
                         _driver.getAttribute(Locator.xpath("//table[@id=" + Locator.xq(getTableId()) +"]//tr[" + (row+1) + "]//input[@name='.select']"), "value"):
-                        _driver.getAttribute(Locator.xpath("//table[@id=" + Locator.xq(getTableId()) +"]//tr[" + (row+5) + "]//input[@name='.select']"), "value");
+                        _driver.getAttribute(Locator.xpath("//table[@id=" + Locator.xq(getTableId()) +"]//tr[" + (row+3) + "]//input[@name='.select']"), "value");
                 _mapRows.put(value, row);
                 if (value.equals(pk))
                     return row;
@@ -820,21 +819,23 @@ public class DataRegionTable extends WebDriverComponent implements WebDriverWrap
         return getDataAsText(row, col);
     }
 
-    public void updateRow(int id, Map<String, String> data)
+    /* Key is the PK on the table, it is usually the contents of the 'value' attribute of the row selector checkbox  */
+    public void updateRow(String key, Map<String, String> data)
     {
-        updateRow(id, data, true);
+        updateRow(key, data, true);
     }
 
-    public void updateRow(int id, Map<String, String> data, boolean validateText)
+    /* Key is the PK on the table, it is usually the contents of the 'value' attribute of the row selector checkbox  */
+    public void updateRow(String key, Map<String, String> data, boolean validateText)
     {
         if (IS_BOOTSTRAP_LAYOUT)
         {
-            WebElement updateLink = updateLink(id);     // see if mousing over the link gets automation past the idea it's not clickable in the new UI
+            WebElement updateLink = updateLink(getRowIndex(key));     // see if mousing over the link gets automation past the idea it's not clickable in the new UI
             _driver.fireEvent(updateLink, WebDriverWrapper.SeleniumEvent.mouseover);
-            _driver.clickAndWait(updateLink(id));
+            _driver.clickAndWait(updateLink(getRowIndex(key)));
         }else
         {
-            _driver.clickAndWait(updateLink(id - 1));
+            _driver.clickAndWait(updateLink(getRowIndex(key)));
         }
 
         setRowData(data, validateText);
@@ -861,21 +862,6 @@ public class DataRegionTable extends WebDriverComponent implements WebDriverWrap
                     break;
                 case "file":
                     _driver.setFormElement(field, new File(data.get(key)));
-                    break;
-                case "select-one":
-                    {
-                        TextSelect select = new TextSelect(field);  // probably want to refactor this into a select-wrapper and call it 'selectByLabel'
-                        List<WebElement> els = select.getOptions(); // now find the index of options with attribute 'label' matching data.get(key)
-                        for (int i=0; i < els.size(); i++)
-                        {
-                            String elementLabel = els.get(i).getAttribute("label");
-                            if (elementLabel != null && elementLabel.equalsIgnoreCase(data.get(key)))
-                            {
-                                select.selectByIndex(i);
-                                break;
-                            }
-                        }
-                    }
                     break;
                 default:
                     _driver.setFormElement(field, data.get(key));
