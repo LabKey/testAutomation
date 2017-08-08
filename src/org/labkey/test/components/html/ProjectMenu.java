ac83b6bf0a1c72d976e3924a0b161b8e352ae31a
Nick Kerr <nicka@labkey.com>
2017-08-08 08:18:07 +0000

    UX Refresh: update projectMenu test locator
    SVN r52847 |2017-08-08 08:18:07 +0000

--- src/org/labkey/test/components/html/ProjectMenu.java
+++ src/org/labkey/test/components/html/ProjectMenu.java
@@ -185,8 +185,8 @@ public class ProjectMenu extends WebDriverComponent<ProjectMenu.ElementCache>
 
     public static class Locators
     {
-        public static final Locator lableyPageNavbar = Locator.xpath("//nav[@class='labkey-page-nav']")
-                .withChild(Locator.tagWithClass("div", "container").childTag("div").withClass("navbar-header"));
+        public static final Locator lableyPageNavbar = Locator.tagWithClass("nav", "labkey-page-nav")
+                .withDescendant(Locator.tagWithClass("div", "navbar-header"));
         public static final Locator menuProjectNav = Locator.tagWithClassContaining("li", "dropdown")
                 .withAttribute("data-name", "BetaNav");
         public static final Locator containerMobile = Locator.tagWithId("li", "project-mobile");
