76f4b387213a711ad0e1b16face31d1068515e32
Dan Duffek <danield@labkey.com>
2017-08-16 00:41:58 +0000

    #30892 Test Automation for LabKey Premium Feature Work - Force authentication as part of signing process.
    SVN r53108 |2017-08-16 00:41:58 +0000

--- src/org/labkey/test/util/DataRegionElecSignHelper.java
+++ src/org/labkey/test/util/DataRegionElecSignHelper.java
@@ -6,31 +6,44 @@ import org.labkey.test.Locator;
 
 public class DataRegionElecSignHelper extends AbstractDataRegionExportOrSignHelper
 {
+    private String _userName, _userPassword;
+
     public DataRegionElecSignHelper(DataRegionTable drt)
     {
         super(drt);
+        _userName = PasswordUtil.getUsername();
+        _userPassword = PasswordUtil.getPassword();
+    }
+
+    public DataRegionElecSignHelper(String userName, String userPassword, DataRegionTable drt)
+    {
+        super(drt);
+        _userName = userName;
+        _userPassword = userPassword;
     }
 
     public void signExcel(ColumnHeaderType headerType, ExcelFileType type, @Nullable Boolean selected, String reason)
     {
         startExcelExport(headerType, type, selected);
-        Locator submit = Locator.linkWithText("Submit");
-        getWrapper().waitForElement(submit);
-        getWrapper().setFormElement(Locator.input("reason"), reason);
-        getWrapper().setFormElement(Locator.input("email"), PasswordUtil.getUsername());
-        getWrapper().setFormElement(Locator.input("password"), PasswordUtil.getPassword());
-        getWrapper().click(submit);
+        signDocument(reason);
     }
 
     public void signText(ColumnHeaderType headerType, TextSeparator delim, TextQuote quote, @Nullable Boolean exportSelected, String reason)
     {
         startTextExport(headerType, delim, quote, exportSelected);
+        signDocument(reason);
+    }
+
+    public void signDocument(String reason){
         Locator submit = Locator.linkWithText("Submit");
         getWrapper().waitForElement(submit);
         getWrapper().setFormElement(Locator.input("reason"), reason);
-        getWrapper().setFormElement(Locator.input("email"), PasswordUtil.getUsername());
-        getWrapper().setFormElement(Locator.input("password"), PasswordUtil.getPassword());
+        getWrapper().setFormElement(Locator.input("email"), _userName);
+        getWrapper().setFormElement(Locator.input("password"), _userPassword);
         getWrapper().click(submit);
+        getWrapper().waitForElementToDisappear(submit);
+        getWrapper().waitForText("Signed Snapshot");
+
     }
 
     @Override
