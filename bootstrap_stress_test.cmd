1bc0172bdd4c97ff868acac9f4f8f5e65dbf629a
Trey Chadick <tchad@labkey.com>
2017-08-18 23:08:41 +0000

    Remove obsolete test scripts
    SVN r53195 |2017-08-18 23:08:41 +0000

--- bootstrap_stress_test.cmd
+++
@@ -1,9 +0,0 @@
-@echo Endlessly bootstraps your bootstrap server and runs a couple DRT tests; use for stress testing the bootstrap process.
-
-:start
-call ant -f ..\build.xml stop_tomcat
-call ant -f ..\build.xml bootstrap
-call ant -f ..\build.xml start_tomcat
-call ant drt -Dtest=basic
-call ant drt -Dtest=xtandem
-goto :start
--- upgrade_stress_test.cmd
+++
@@ -1,6 +0,0 @@
-@echo Endlessly upgrades your bootstrap server; use for stress testing the upgrade process.
-
-:start
-call ant -f ..\build.xml force_upgrade
-call ant drt -Dtest=basic
-goto :start
