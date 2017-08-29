158998bed98ef378ede8ea709e8515c5c41ab806
Sanchya Mahajan <sanchyam@labkey.com>
2017-08-29 23:54:23 +0000

    -added test to check for script injection using lists
    SVN r53346 |2017-08-29 23:54:23 +0000

--- src/org/labkey/test/tests/ProjectSettingsTest.java
+++ src/org/labkey/test/tests/ProjectSettingsTest.java
@@ -15,6 +15,7 @@
  */
 package org.labkey.test.tests;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.labkey.test.BaseWebDriverTest;
@@ -25,10 +26,14 @@ import org.labkey.test.TestTimeoutException;
 import org.labkey.test.categories.DailyB;
 import org.labkey.test.components.html.BootstrapMenu;
 import org.labkey.test.components.html.SiteNavBar;
+import org.labkey.test.util.DataRegionTable;
+import org.labkey.test.util.ListHelper;
 import org.openqa.selenium.WebElement;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -38,6 +43,8 @@ public class ProjectSettingsTest extends BaseWebDriverTest
     {setIsBootstrapWhitelisted(true);}
     private static final Locator helpMenuLinkDev =  Locator.tagWithText("span", "Help (default)");
     private static final Locator helpMenuLinkProduction =  Locator.tagWithText("span", "Help");
+    private static final String INJECT_CHARS = "<script>alert(\"8(\");</script>";
+    private static final String DATE_TIME_FORMAT_INJECTION = "yyyy-MM-dd HH:mm'" + INJECT_CHARS + "'";
     private Locator helpMenuLink = TestProperties.isDevModeEnabled() ? helpMenuLinkDev : helpMenuLinkProduction;
 
     @Override
@@ -53,7 +60,7 @@ public class ProjectSettingsTest extends BaseWebDriverTest
     }
 
     //this project's properties will be altered and so should not copy site properties
-    protected String getProjectAltertedName()
+    protected String getProjectAlteredName()
     {
         return "Independent Project";
     }
@@ -70,28 +77,32 @@ public class ProjectSettingsTest extends BaseWebDriverTest
         assertEquals("Help link state unexpected.", helpLinkPresent, visibleLinks.stream().anyMatch((a)-> a.getText().equals("LabKey Documentation")));
     }
 
+    @BeforeClass
+    public static void setupProject()
+    {
+        ProjectSettingsTest init = (ProjectSettingsTest)getCurrentTest();
+        init.setUpTest();
+    }
+
     protected void setUpTest()
     {
-
         _containerHelper.createProject(getProjectName(), null);
         checkHelpLinks(null, true, true);
-        _containerHelper.createProject(getProjectAltertedName(), null);
+        _containerHelper.createProject(getProjectAlteredName(), null);
         checkHelpLinks(null, true, true);
 
-        goToProjectSettings(getProjectAltertedName());
+        goToProjectSettings(getProjectAlteredName());
         setFormElement(Locator.name("reportAProblemPath"), "");
         clickButton("Save");
         assertElementNotPresent(Locators.labkeyError);
 
-        checkHelpLinks(getProjectAltertedName(), false, true);
+        checkHelpLinks(getProjectAlteredName(), false, true);
 //        assertElementNotPresent("Support link still present after removing link from settings", supportLink);
     }
 
     @Test
     public void testSteps()
     {
-        setUpTest();
-
         //assert both locators are present in clone project
         goToProjectHome();
         checkHelpLinks(null, true, true);
@@ -104,15 +115,35 @@ public class ProjectSettingsTest extends BaseWebDriverTest
 
         //assert help link missing in proj 1, present in proj 2
         checkHelpLinks(getProjectName(), true, false);
-        checkHelpLinks(getProjectAltertedName(), false, true);
+        checkHelpLinks(getProjectAlteredName(), false, true);
 
         //change proj 2 to exclude both help and support
-        goToProjectSettings(getProjectAltertedName());
+        goToProjectSettings(getProjectAlteredName());
         uncheckCheckbox(Locator.checkboxByName("enableHelpMenu"));
         clickButtonContainingText("Save");
 
         //assert help link itself gone
         assertElementNotPresent(helpMenuLink);
+    }
+
+    @Test
+    public void testInjection()
+    {
+        goToProjectHome();
+        goToProjectSettings();
+        setFormElement(Locator.name("defaultDateTimeFormat"), DATE_TIME_FORMAT_INJECTION);
+        clickButtonContainingText("Save");
+        _listHelper.createList(getProjectName(), "IceCream", ListHelper.ListColumnType.AutoInteger, "IceCreamID",
+                new ListHelper.ListColumn("IceCreamDate", "", ListHelper.ListColumnType.DateTime, ""));
+        goToProjectHome();
+        clickAndWait(Locator.linkWithText("IceCream"));
+        Map<String, String> testRow = new HashMap<>();
+        String testDate = "1800-05-10 10:32";
+        testRow.put("IceCreamDate", testDate);
+        _listHelper.insertNewRow(testRow);
+        DataRegionTable list = new DataRegionTable("query", getDriver());
+        String attemptedInjection = list.getDataAsText(0, 0);
+        assertEquals("Wrong list data from injection attempt", testDate + INJECT_CHARS, attemptedInjection);
     }
 
     @Override
@@ -123,7 +154,7 @@ public class ProjectSettingsTest extends BaseWebDriverTest
         clickButtonContainingText("Save");
 
         deleteProject(getProjectName(), afterTest);
-        deleteProject(getProjectAltertedName(), afterTest);
+        deleteProject(getProjectAlteredName(), afterTest);
     }
 
     @Override
