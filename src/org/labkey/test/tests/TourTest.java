347d0884c30818bb6d29f3d4ad0cd7db675bf4a7
Nick Kerr <nicka@labkey.com>
2017-08-30 20:34:04 +0000

    UX Refresh: whitelist TourTest
    SVN r53364 |2017-08-30 20:34:04 +0000

--- src/org/labkey/test/tests/TourTest.java
+++ src/org/labkey/test/tests/TourTest.java
@@ -23,6 +23,7 @@ import org.junit.experimental.categories.Category;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.Locator;
 import org.labkey.test.categories.DailyA;
+import org.labkey.test.components.html.ProjectMenu;
 import org.labkey.test.pages.TourEditor;
 import org.labkey.test.util.DataRegionTable;
 import org.labkey.test.util.LogMethod;
@@ -32,7 +33,7 @@ import java.util.List;
 @Category({DailyA.class})
 public class TourTest extends BaseWebDriverTest
 {
-    public static final String PROJECT_NAME = "Tour Test Project";
+    {setIsBootstrapWhitelisted(true);}
     public static final String SUBFOLDER1 = "test project 1";
     public static final String SUBFOLDER2 = "test project 2";
     public static final String TOUR_NAME = "Test Tour";
@@ -43,7 +44,7 @@ public class TourTest extends BaseWebDriverTest
     @Override
     protected String getProjectName()
     {
-        return PROJECT_NAME;
+        return "Tour Test Project";
     }
 
     @Override
@@ -72,39 +73,34 @@ public class TourTest extends BaseWebDriverTest
     @LogMethod
     public void setupBasicTour()
     {
-        beginAt("/tours/" + getProjectName() + "/" + SUBFOLDER1 + "/begin.view");
-        waitForText("Tours");
-        DataRegionTable table = new DataRegionTable("query", getDriver());
-        table.clickInsertNewRow();
-        waitForText("Tour Builder");
-        TourEditor tourEditor = new TourEditor(getDriver());
-        tourEditor.setTitle("Test Tour");
+        TourEditor tourEditor = navigateToInsertTour(SUBFOLDER1);
+        tourEditor.setTitle(TOUR_NAME);
         tourEditor.setMode(TourEditor.TourMode.RUNONCE);
         tourEditor.setDescription(TOUR_DESC);
-        tourEditor.setSelector(1, "select");
+        tourEditor.setSelector(1, ".navbar-nav-lk .fa-search");
         tourEditor.setStep(1, "{\n" +
                 "        placement: \"right\",\n" +
-                "        title: \"This is a webpart.\",\n" +
-                "        content: \"What can't you do in a webpart!\",\n" +
+                "        title: \"This is global search.\",\n" +
+                "        content: \"Search for it all!\",\n" +
                 "}");
-        tourEditor.setSelector(2, "span#adminMenuPopupText");
+        tourEditor.setSelector(2, ".navbar-nav-lk .fa-cog");
         tourEditor.setStep(2, "{\n" +
                 "        placement: \"left\",\n" +
                 "        title: \"This is the admin menu.\",\n" +
                 "        content: \"Click here to perform administrative tasks.\",\n" +
                 "}");
-        tourEditor.setSelector(3, "span#helpMenuPopupText");
+        tourEditor.setSelector(3, ".navbar-nav-lk .fa-user");
         tourEditor.setStep(3, "{\n" +
                 "        placement: \"left\",\n" +
                 "        title: \"This is the help menu.\",\n" +
                 "        content: \"Click here for tutorials and various help tasks.\",\n" +
                 "}");
         tourEditor.addStep();
-        tourEditor.setSelector(4, "li#folderBar.menu-folders");
+        tourEditor.setSelector(4, ".lk-body-title h3");
         tourEditor.setStep(4, "{\n" +
                 "        placement: \"bottom\",\n" +
-                "        title: \"This is the folder menu.\",\n" +
-                "        content: \"Use the links here to navigate to different folders.\",\n" +
+                "        title: \"This is the page title.\",\n" +
+                "        content: \"Displays the title of the page you are on.\",\n" +
                 "}");
         sleep(WAIT);
         tourEditor.save();
@@ -123,83 +119,55 @@ public class TourTest extends BaseWebDriverTest
     public void testBasicTour()
     {
         TourNavigator tourNavigator = new TourNavigator();
-        goToProjectHome();
-        sleep(WAIT);
-        clickFolder(SUBFOLDER1);
+        navigateToFolder(SUBFOLDER1);
         assertBasicTour();
         //tour should only run once
-        goToProjectHome();
-        sleep(WAIT);
-        clickFolder(SUBFOLDER1);
+        navigateToFolder(SUBFOLDER1);
         tourNavigator.assertNoTourBubble();
     }
 
     public void testTourRoundTrip()
     {
         TourNavigator tourNavigator = new TourNavigator();
-        beginAt("/tours/" + getProjectName() + "/" + SUBFOLDER1 + "/begin.view");
-        waitForText("Tours");
-        clickAndWait(Locator.linkWithText("Edit"));
-        TourEditor tourEditor = new TourEditor(getDriver());
-        String tourJSON = tourEditor.export();
-        beginAt("/tours/" + getProjectName() + "/" + SUBFOLDER2 + "/begin.view");
-        waitForText("Tours");
-        DataRegionTable table = new DataRegionTable("query", getDriver());
-        table.clickInsertNewRow();
-        waitForText("Tour Builder");
-        tourEditor = new TourEditor(getDriver());
+        TourEditor tourEditor = navigateToEditTour(SUBFOLDER1, TOUR_NAME);
+        final String tourJSON = tourEditor.export();
+        tourEditor = navigateToInsertTour(SUBFOLDER2);
         tourEditor.importTour(tourJSON);
         tourEditor.setTitle(TOUR_NAME);
         tourEditor.save();
-        goToProjectHome();
-        sleep(WAIT);
-        clickFolder(SUBFOLDER2);
+        navigateToFolder(SUBFOLDER2);
         assertBasicTour();
         //tour should only run once
-        goToProjectHome();
-        sleep(WAIT);
-        clickFolder(SUBFOLDER1);
+        navigateToFolder(SUBFOLDER1);
         tourNavigator.assertNoTourBubble();
     }
 
     public void testRunAlways()
     {
-        beginAt("/tours/" + getProjectName() + "/" + SUBFOLDER1 + "/begin.view");
-        waitForText("Tours");
-        clickAndWait(Locator.linkWithText("Edit"));
-        TourEditor tourEditor = new TourEditor(getDriver());
+        TourEditor tourEditor = navigateToEditTour(SUBFOLDER1, TOUR_NAME);
         tourEditor.setMode(TourEditor.TourMode.RUNALWAYS);
         tourEditor.save();
-        goToProjectHome();
-        sleep(WAIT);
-        clickFolder(SUBFOLDER1);
+        navigateToFolder(SUBFOLDER1);
         assertBasicTour();
-        goToProjectHome();
-        sleep(WAIT);
-        clickFolder(SUBFOLDER1);
+        navigateToFolder(SUBFOLDER1);
         assertBasicTour();
     }
 
     public void testOffMode()
     {
         TourNavigator tourNavigator = new TourNavigator();
-        beginAt("/tours/" + getProjectName() + "/" + SUBFOLDER1 + "/begin.view");
-        waitForText("Tours");
-        clickAndWait(Locator.linkWithText("Edit"));
-        TourEditor tourEditor = new TourEditor(getDriver());
+        TourEditor tourEditor = navigateToEditTour(SUBFOLDER1, TOUR_NAME);
         tourEditor.setMode(TourEditor.TourMode.OFF);
         tourEditor.save();
-        waitForText("Saved");
-        goToProjectHome();
-        sleep(WAIT);
-        clickFolder(SUBFOLDER1);
+        navigateToFolder(SUBFOLDER1);
         tourNavigator.assertNoTourBubble();
     }
 
     private void assertBasicTour()
     {
         TourNavigator tourNavigator = new TourNavigator();
-        tourNavigator.assertTourBubble("1", "This is a webpart.", "What can't you do in a webpart!");
+        tourNavigator.waitForTourBubble("1");
+        tourNavigator.assertTourBubble("1", "This is global search.", "Search for it all!");
         tourNavigator.nextTourBubble();
         tourNavigator.waitForTourBubble("2");
         tourNavigator.assertTourBubble("2", "This is the admin menu.", "Click here to perform administrative tasks.");
@@ -208,10 +176,38 @@ public class TourTest extends BaseWebDriverTest
         tourNavigator.assertTourBubble("3", "This is the help menu.", "Click here for tutorials and various help tasks.");
         tourNavigator.nextTourBubble();
         tourNavigator.waitForTourBubble("4");
-        tourNavigator.assertTourBubble("4", "This is the folder menu.", "Use the links here to navigate to different folders.");
+        tourNavigator.assertTourBubble("4", "This is the page title.", "Displays the title of the page you are on.");
         tourNavigator.doneTourBubble();
         sleep(WAIT);
         tourNavigator.assertNoTourBubble();
+    }
+
+    private void navigateToFolder(String folder)
+    {
+        ProjectMenu menu = new ProjectMenu(this.getDriver());
+        menu.navigateToFolder(getProjectName(), folder);
+    }
+
+    private TourEditor navigateToEditTour(String folder, String tourTitle)
+    {
+        DataRegionTable table = navigateToTours(folder);
+        table.clickEditRow(table.getRowIndex("Title", tourTitle));
+        waitForText("Tour Builder");
+        return new TourEditor(getDriver());
+    }
+
+    private TourEditor navigateToInsertTour(String folder)
+    {
+        DataRegionTable table = navigateToTours(folder);
+        table.clickInsertNewRow();
+        waitForText("Tour Builder");
+        return new TourEditor(getDriver());
+    }
+
+    private DataRegionTable navigateToTours(String folder)
+    {
+        beginAt("/tours/" + getProjectName() + "/" + folder + "/begin.view");
+        return new DataRegionTable("query", getDriver());
     }
 
     private class TourNavigator
@@ -228,7 +224,8 @@ public class TourTest extends BaseWebDriverTest
 
         public void assertNoTourBubble()
         {
-            if(isElementPresent(Locator.tag("div").withAttributeContaining("class", "hopscotch-bubble animated hide"))) return;
+            if (isElementPresent(Locator.tag("div").withAttributeContaining("class", "hopscotch-bubble animated hide")))
+                return;
             assertElementNotPresent(Locator.tag("div").withAttributeContaining("class", "hopscotch"));
         }
 
@@ -243,6 +240,7 @@ public class TourTest extends BaseWebDriverTest
         private void waitForTourBubble(String number)
         {
             waitForElement(Locator.tagWithClass("span", "hopscotch-bubble-number").withText(number));
+            sleep(750); // time for animation to complete
         }
     }
 }
