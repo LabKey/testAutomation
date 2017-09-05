9a7dc12618662af9e430b6e8e98aa7f0a4f5d0dc
Adam Rauch <adam@labkey.com>
2017-09-05 19:12:45 +0000

    Remove reference to FolderManagement action; check permissions on individual actions
    SVN r53512 |2017-09-05 19:12:45 +0000

--- src/org/labkey/test/tests/SecurityTest.java
+++ src/org/labkey/test/tests/SecurityTest.java
@@ -138,20 +138,21 @@ public class SecurityTest extends BaseWebDriverTest
 
     protected static final String[] unreachableUrlsExtendedList = {"/security/group.view?group=Developers",
         "user/showUsers.view?", "security/project.view?returnUrl=%2Fuser%2FshowUsers.view%3F", "admin/createFolder.view?",
-        "/analytics/begin.view?", "/login/configure.view?", "/admin/customizeEmail.view?",
-        "/admin/filesSiteSettings.view?", "/admin/projectSettings.view?",
-        "/flow/flowAdmin.view?", "/admin/folderManagement.view?",
-        "/admin/reorderFolders.view?", "/admin/customizeSite.view?",
-        "/reports/configureReportsAndScripts.view?", "/audit/showAuditLog.view?",
-        "/search/admin.view?", "/ms1/showAdmin.view?", "/ms2/showMS2Admin.view?",
-        "/experiment-types/begin.view?", "/pipeline-status/showList.view?", "/pipeline/setup.view?",
-        "/ms2/showProteinAdmin.view?", "/admin/actions.view?", "/admin/caches.view?",
-        "/admin/dbChecker.view?",
-        "/query/dataSourceAdmin.view?",
-        "/admin/dumpHeap.view?", "/admin/environmentVariables.view?", "/admin/memTracker.view?",
-        "/admin/queries.view?", "/admin/resetErrorMark.view?", "/admin/showThreads.view?",
-        "/admin-sql/scripts.view?", "/admin/systemProperties.view?", "/admin/emailTest.view?",
-        "/admin/showAllErrors.view?", "/admin/showErrorsSinceMark.view?",  "/admin/showPrimaryLog.view?"};
+        "/analytics/begin.view?", "/login/configure.view?", "/admin/customizeEmail.view?", "/admin/filesSiteSettings.view?",
+        "/admin/projectSettings.view?", "/flow/flowAdmin.view?", "/admin/reorderFolders.view?", "/admin/customizeSite.view?",
+        "/reports/configureReportsAndScripts.view?", "/audit/showAuditLog.view?", "/search/admin.view?",
+        "/ms1/showAdmin.view?", "/ms2/showMS2Admin.view?", "/experiment-types/begin.view?", "/pipeline-status/showList.view?",
+        "/pipeline/setup.view?", "/ms2/showProteinAdmin.view?", "/admin/actions.view?", "/admin/caches.view?",
+        "/admin/dbChecker.view?", "/query/dataSourceAdmin.view?", "/admin/dumpHeap.view?", "/admin/environmentVariables.view?",
+        "/admin/memTracker.view?", "/admin/queries.view?", "/admin/resetErrorMark.view?", "/admin/showThreads.view?",
+        "/admin-sql/scripts.view?", "/admin/systemProperties.view?", "/admin/emailTest.view?", "/admin/showAllErrors.view?",
+        "/admin/showErrorsSinceMark.view?", "/admin/showPrimaryLog.view?",
+
+        /* Management actions shouldn't be reachable by non-admins */
+        "/admin-missingValues.view", "/admin-manageFolders.view", "/admin-moduleProperties.view", "/admin-concepts.view",
+        "/admin-search.view", "/admin-notifications.view", "/admin-exportFolder.view", "/admin-importFolder.view",
+        "/admin-fileRoots.view", "/admin-folderInformation.view"
+    };
 
     /**
      * verify that a normal user does not get a link to the admin console or see their own history, nor
