a0ded6e895708210d69bfae5657cbba0185ce738
Nick Kerr <nicka@labkey.com>
2017-09-19 16:29:15 +0000

    31656: Ensure query parameters are passed by Ext3 store - regression test
    SVN r53845 |2017-09-19 16:29:15 +0000

--- data/api/maxRows.js
+++ data/api/maxRows.js
@@ -4,11 +4,16 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 window.gridView = new LABKEY.ext.EditorGridPanel({
+    renderTo: 'testDiv',
     store: new LABKEY.ext.Store({
-        schemaName : 'lists',
-        sql: 'SELECT People.FirstName, People.LastName, People.Age FROM People',
-        maxRows: 2}),
-    renderTo : 'testDiv',
+        schemaName: 'lists',
+        sql: 'PARAMETERS(MinAge INTEGER) SELECT People.FirstName, People.LastName, People.Age FROM People WHERE People.Age > MinAge',
+        // 31656: Ensure query parameters are passed along
+        parameters: {
+            MinAge: 20
+        },
+        maxRows: 2
+    }),
     editable : true,
     enableFilters : true,
     title :'maxRows Test',
