f9786cc49bc867afaa9cfa394e61ee843724ce13
Trey Chadick <tchad@labkey.com>
2017-09-22 05:20:32 +0000

    Admin menu should wait for page load
    SVN r53947 |2017-09-22 05:20:32 +0000

--- src/org/labkey/test/WebDriverWrapper.java
+++ src/org/labkey/test/WebDriverWrapper.java
@@ -544,14 +544,9 @@ public abstract class WebDriverWrapper implements WrapsDriver
 
     public void clickAdminMenuItem(String... items)
     {
-        clickAdminMenuItem(false, items);
-    }
-
-    public void clickAdminMenuItem(boolean wait, String... items)
-    {
         if (LabKeySiteWrapper.IS_BOOTSTRAP_LAYOUT)
         {
-            new SiteNavBar(getDriver()).clickAdminMenuItem(wait, items);
+            new SiteNavBar(getDriver()).clickAdminMenuItem(true, items);
         }
         else
         {
