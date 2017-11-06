1b69bf43dae93a2ba00ae7dbfcbb94ef923f0369
Trey Chadick <tchad@labkey.com>
2017-11-06 18:56:19 +0000

    Make test trigger scripts work well on PG and MSSQL
    SVN r54925 |2017-11-06 18:56:19 +0000

--- modules/triggerTestModule/resources/queries/exp.data/DataClassTest.js
+++ modules/triggerTestModule/resources/queries/exp.data/DataClassTest.js
@@ -5,78 +5,85 @@
  */
 var shared = require("TriggerTestModule/EmployeeLib");
 var console = require("console");
-   
+
 function init(event, errors) {
-	console.log("init got triggered with event: " + event);
+    console.log("init got triggered with event: " + event);
     console.log(shared.sampleFunc("this is from the shared function"));
 }
 
 function beforeInsert(row, errors) {
-	console.log("beforeInsert got triggered");
-	console.log("row is: " + row);
-	if(row.Comments == "Individual Test") {
-		row.Country = "Inserting Single";
-		row.Comments = "BeforeDelete"; //set this for next test step
-	}
-	else if(row.Comments == "Import Test")
-		row.Country = "Importing TSV";
-	else if(row.Comments == "API Test")
-		row.Country = "API BeforeInsert";
-	console.log("edited row is: " + row);
-	console.log(shared.sampleFunc("this is from the shared function"));
+    console.log("exp.data: beforeInsert: row is: " + row);
+    if(row.Comments) {
+        if (row.Comments == "Individual Test") {
+            row.Country = "Inserting Single";
+            row.Comments = "BeforeDelete"; //set this for next test step
+        }
+        else if (row.Comments == "Import Test")
+            row.Country = "Importing TSV";
+        else if (row.Comments == "API Test")
+            row.Country = "API BeforeInsert";
+    } else {
+        if (row.comments == "Individual Test") {
+            row.country = "Inserting Single";
+            row.comments = "BeforeDelete"; //set this for next test step
+        }
+        else if (row.comments == "Import Test")
+            row.country = "Importing TSV";
+        else if (row.comments == "API Test")
+            row.country = "API BeforeInsert";
+    }
+
+    console.log("exp.data: edited row is: " + row);
+    console.log(shared.sampleFunc("exp.data: this is from the shared function"));
 }
 
 function beforeUpdate(row, oldRow, errors) {
-	console.log("beforeUpdate got triggered");
-	console.log("row is: " + row);
-	if(row.Comments == "BeforeUpdate")
-		row.Country = "Before Update changed me";
-	console.log("old row is: " + oldRow);
-	console.log(shared.sampleFunc("this is from the shared function"));
+    console.log("exp.data: beforeUpdate: row is: " + row);
+    if(row.Comments == "BeforeUpdate")
+        row.Country = "Before Update changed me";
+    else if(row.comments == "BeforeUpdate")
+        row.country = "Before Update changed me";
+    console.log("exp.data: old row is: " + oldRow);
+    console.log(shared.sampleFunc("exp.data: this is from the shared function"));
 }
 
 function beforeDelete(row, errors) {
-	console.log("beforeDelete got triggered");
-	console.log("row is: " + row);
-	if(row.Comments == "BeforeDelete")
-		errors[null] = "This is the Before Delete Error";
+    console.log("exp.data: beforeDelete: row is: " + row);
+    if(row.Comments == "BeforeDelete" || row.comments == "BeforeDelete")
+        errors[null] = "This is the Before Delete Error";
 
-	console.log(shared.sampleFunc("this is from the shared function"));
+    console.log(shared.sampleFunc("exp.data: this is from the shared function"));
 }
 
 function afterInsert(row, errors) {
-	console.log("afterInsert got triggered");
-	console.log("row is: " + row);
+    console.log("exp.data: afterInsert: row is: " + row);
 
-	if(row.Comments == "AfterInsert")
-		errors[null] = "This is the After Insert Error";
+    if(row.Comments == "AfterInsert" || row.comments == "AfterInsert")
+        errors[null] = "This is the After Insert Error";
 
-	console.log(shared.sampleFunc("this is from the shared function"));
+    console.log(shared.sampleFunc("exp.data: this is from the shared function"));
 }
 
 function afterUpdate(row, oldRow, errors) {
-	console.log("afterUpdate got triggered");
-	console.log("row is: " + row);
+    console.log("exp.data: afterUpdate: row is: " + row);
 
-	if(row.Comments == "AfterUpdate")
-		errors[null] = "This is the After Update Error";
+    if(row.Comments == "AfterUpdate" || row.comments == "AfterUpdate")
+        errors[null] = "This is the After Update Error";
 
-	console.log("old row is: " +oldRow);
-	console.log(shared.sampleFunc("this is from the shared function"));
+    console.log("exp.data: old row is: " +oldRow);
+    console.log(shared.sampleFunc("exp.data: this is from the shared function"));
 }
 
 function afterDelete(row, errors) {
-	console.log("afterDelete got triggered");
-	console.log("row is: " + row);
+    console.log("exp.data: afterDelete: row is: " + row);
 
-	if(row.Country == "Before Update changed me")
-		errors[null] = "This is the After Delete Error";
+    if(row.Country == "Before Update changed me" || row.country == "Before Update changed me")
+        errors[null] = "This is the After Delete Error";
 
-	console.log(shared.sampleFunc("this is from the shared function"));
+    console.log(shared.sampleFunc("exp.data: this is from the shared function"));
 }
 
-
 function complete(event, errors) {
-	console.log("complete got triggered with event: " + event);
-    console.log(shared.sampleFunc("this is from the shared function"));
+    console.log("exp.data: complete got triggered with event: " + event);
+    console.log(shared.sampleFunc("exp.data: this is from the shared function"));
 }
--- modules/triggerTestModule/resources/queries/lists/Employees.js
+++ modules/triggerTestModule/resources/queries/lists/Employees.js
@@ -12,71 +12,63 @@ function init(event, errors) {
 }
 
 function beforeInsert(row, errors) {
-	console.log("beforeInsert got triggered");
-	console.log("row is: " + row);
-    if(row.Name == "Emp 2")
-    	row.Company = "Inserting Single";
-    else if(row.Name == "Emp 5")
-        row.Company = "Importing TSV";
-    else if(row.Name == "Emp 6")
-        row.Company = "API BeforeInsert";
-    console.log("edited row is: " + row);
-    console.log(shared.sampleFunc("this is from the shared function"));
+	console.log("list: beforeInsert: row is: " + row);
+    if(row.name == "Emp 2")
+    	row.company = "Inserting Single";
+    else if(row.name == "Emp 5")
+        row.company = "Importing TSV";
+    else if(row.name == "Emp 6")
+        row.company = "API BeforeInsert";
+    console.log("list: edited row is: " + row);
+    console.log(shared.sampleFunc("list: this is from the shared function"));
 }
 
 function beforeUpdate(row, oldRow, errors) {
-	console.log("beforeUpdate got triggered");
-	console.log("row is: " + row);
-	if(row.Name == "Emp 3" || row.name == "Emp 8" )
-        row.Company = "Before Update changed me";
-	console.log("old row is: " + oldRow);
-    console.log(shared.sampleFunc("this is from the shared function"));
+	console.log("list: beforeUpdate: row is: " + row);
+	if(row.name == "Emp 3" || row.name == "Emp 8" )
+        row.company = "Before Update changed me";
+	console.log("list: old row is: " + oldRow);
+    console.log(shared.sampleFunc("list: this is from the shared function"));
 }
 
 function beforeDelete(row, errors) {
-	console.log("beforeDelete got triggered");
-	console.log("row is: " + row);
-    if(row.Company == "Inserting Single" || row.Company == "DeleteMe")
+	console.log("list: beforeDelete: row is: " + row);
+    if(row.company == "Inserting Single" || row.company == "DeleteMe")
         errors[null] = "This is the Before Delete Error";
 
-    console.log(shared.sampleFunc("this is from the shared function"));
+    console.log(shared.sampleFunc("list: this is from the shared function"));
 }
 
 function afterInsert(row, errors) {
-	console.log("afterInsert got triggered");
-	console.log("row is: " + row);
+	console.log("list: afterInsert: row is: " + row);
 
     if(row.name == "Emp 1")
         errors[null] = "This is the After Insert Error";
 
-    console.log(shared.sampleFunc("this is from the shared function"));
+    console.log(shared.sampleFunc("list: this is from the shared function"));
 }
 
 function afterUpdate(row, oldRow, errors) {
-	console.log("afterUpdate got triggered");
-	console.log("row is: " + row);
+	console.log("list: afterUpdate: row is: " + row);
 
     if(row.name == "Emp 2" || row.name == "Emp 6" )
         errors[null] = "This is the After Update Error";
     //throw new Error("This is the After Update Error");
 
-	console.log("old row is: " +oldRow);
-    console.log(shared.sampleFunc("this is from the shared function"));
+	console.log("list: old row is: " +oldRow);
+    console.log(shared.sampleFunc("list: this is from the shared function"));
 }
 
 function afterDelete(row, errors) {
-	console.log("afterDelete got triggered");
-	console.log("row is: " + row);
+	console.log("list: afterDelete: row is: " + row);
 
-    if(row.Company == "Before Update changed me")
+    if(row.company == "Before Update changed me")
         errors[null] = "This is the After Delete Error";
-    //throw new Error("This is the After Delete Error");
 
-    console.log(shared.sampleFunc("this is from the shared function"));
+    console.log(shared.sampleFunc("list: this is from the shared function"));
 }
 
-
 function complete(event, errors) {
-	console.log("complete got triggered with event: " + event);
-    console.log(shared.sampleFunc("this is from the shared function"));
+	console.log("list: complete got triggered with event: " + event);
+    console.log(shared.sampleFunc("list: this is from the shared function"));
 }
