fb3f5f17f41e5a16c27e6549748ef61bfe878d48
Sweta Jewargikar <swetaj@labkey.com>
2017-12-01 20:55:10 +0000

    Code Review changes
    SVN r55315 |2017-12-01 20:55:10 +0000

--- src/org/labkey/test/tests/MultiplePKUploadAssayTest.java
+++ src/org/labkey/test/tests/MultiplePKUploadAssayTest.java
@@ -24,20 +24,20 @@ public class MultiplePKUploadAssayTest extends BaseWebDriverTest
     @Override
     protected void doCleanup(boolean afterTest) throws TestTimeoutException
     {
-            super.doCleanup(afterTest);
-            _containerHelper.deleteProject(getProjectName(), afterTest);
+        super.doCleanup(afterTest);
+        _containerHelper.deleteProject(getProjectName(), afterTest);
     }
 
     @BeforeClass
     public static void setupProject()
     {
-                MultiplePKUploadAssayTest init = (MultiplePKUploadAssayTest)getCurrentTest();
-                init.doSetup();
+        MultiplePKUploadAssayTest init = (MultiplePKUploadAssayTest) getCurrentTest();
+        init.doSetup();
     }
 
     private void doSetup()
     {
-            _containerHelper.createProject(getProjectName(), "Assay");
+        _containerHelper.createProject(getProjectName(), "Assay");
     }
 
     @Test
@@ -51,15 +51,15 @@ public class MultiplePKUploadAssayTest extends BaseWebDriverTest
         log("Creating individual lists");
         File List1 = TestFileUtils.getSampleData("ThawLists/List1.xlsx");
         File List2 = TestFileUtils.getSampleData("ThawLists/List2.xlsx");
-        _listHelper.createListFromFile(getProjectName(),"ThawList1",List1);
-        _listHelper.createListFromFile(getProjectName(),"ThawList2",List2);
+        _listHelper.createListFromFile(getProjectName(), "ThawList1", List1);
+        _listHelper.createListFromFile(getProjectName(), "ThawList2", List2);
 
         log("Joining the query");
         String joinedList = "JoinedThawList";
         String sql = "SELECT ThawList1.participantID,ThawList1.specimenID,ThawList2.visitId,ThawList2.Date\n" +
-                    "FROM    ThawList1,ThawList2\n" +
-                    "Where   ThawList1.participantID=ThawList2.participantID\n" +
-                    "ORDER BY ThawList1.participantID";
+                "FROM    ThawList1,ThawList2\n" +
+                "Where   ThawList1.participantID=ThawList2.participantID\n" +
+                "ORDER BY ThawList1.participantID";
         goToSchemaBrowser();
         createNewQuery("lists");
         setFormElement(Locator.name("ff_newQueryName"), joinedList);
@@ -67,35 +67,38 @@ public class MultiplePKUploadAssayTest extends BaseWebDriverTest
         setCodeEditorValue("queryText", sql);
         clickButton("Save & Finish");
 
-        log("Importing the data into assay");
-        goToProjectHome();
-        PortalHelper portalHelper = new PortalHelper(this);
         log("Now import the data from the list into the assay.");
-        portalHelper.clickAndWait(Locator.linkWithText(assayName));
-        portalHelper.clickButton(("Import Data"), "Batch Properties");
-        portalHelper.checkRadioButton(Locator.radioButtonById("RadioBtn-Lookup"));
-        portalHelper.waitForText("Use an existing sample list");
-        portalHelper.checkRadioButton(Locator.radioButtonById("RadioBtn-ThawListType-List"));
-        portalHelper.waitForText("Schema:");
+        goToProjectHome();
+        clickAndWait(Locator.linkWithText(assayName));
+        clickButton(("Import Data"), "Batch Properties");
+        checkRadioButton(Locator.radioButtonById("RadioBtn-Lookup"));
+        waitForText("Use an existing sample list");
+        checkRadioButton(Locator.radioButtonById("RadioBtn-ThawListType-List"));
+        waitForText("Schema:");
 
         log("Selecting the joined list");
         _ext4Helper.selectComboBoxItem(Locator.id("thawListSchemaName"), "lists");
         waitForElement(Locator.css(".query-loaded-marker"));
         _ext4Helper.selectComboBoxItem(Locator.id("thawListQueryName"), joinedList);
 
-        portalHelper.clickButton("Next", "Run Properties");
+        clickButton("Next", "Run Properties");
 
         log("Specifying the specimenID");
         assertTitleContains("Data Import: Run Properties and Data File");
         setFormElement(Locator.name("name"), "First run");
+        setFormElement(Locator.id("TextAreaDataCollector.textArea"), "SpecimenID\nS17\nS22");
+        clickButton("Save and Finish");
+        assertElementPresent(Locator.css(".labkey-error").withText("Can not resolve thaw list entry for specimenId: S22"));
+
         setFormElement(Locator.id("TextAreaDataCollector.textArea"), "SpecimenID\nS17");
         clickButton("Save and Finish");
-
         log("Verifying the visit ID");
+
         clickAndWait(Locator.linkWithText("First run"));
-        DataRegionTable table = new DataRegionTable("Data",getDriver());
+        DataRegionTable table = new DataRegionTable("Data", getDriver());
         List<String> visitID = table.getColumnDataAsText("VisitID");
-        assertEquals("Missing visitID","17.0",visitID.get(0));
+        assertEquals("Missing visitID", "17.0", visitID.get(0));
+        assertEquals("Mismatch in row count",1,table.getDataRowCount());
 
     }
 
