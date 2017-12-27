fc28e85c93d6c05181a781178de7be615ca3975b
Dan Duffek <danield@labkey.com>
2017-12-27 16:18:51 +0000

    Fixing AbstractAssayTest check for pipeline being set.
    SVN r55628 |2017-12-27 16:18:51 +0000

--- src/org/labkey/test/tests/AbstractAssayTest.java
+++ src/org/labkey/test/tests/AbstractAssayTest.java
@@ -74,7 +74,7 @@ public abstract class AbstractAssayTest extends BaseWebDriverTest
         setPipelineRoot(dir.getAbsolutePath());
 
         //make sure it was set
-        assertTextPresent("The pipeline root was set to '" + dir.getAbsolutePath() + "'");
+        assertTextPresent("The pipeline root was set to '" + dir.getAbsolutePath());
     }
 
     /**
