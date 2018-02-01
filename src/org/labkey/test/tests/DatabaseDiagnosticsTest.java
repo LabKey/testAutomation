762c5b0b8ad804d2119f60c9e6826351ab3819d0
Trey Chadick <tchad@labkey.com>
2018-02-01 07:07:46 +0000

    Collect exception information after tests
    SVN r56264 |2018-02-01 07:07:46 +0000

--- src/org/labkey/test/tests/DatabaseDiagnosticsTest.java
+++ src/org/labkey/test/tests/DatabaseDiagnosticsTest.java
@@ -20,14 +20,17 @@ import org.junit.experimental.categories.Category;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.Locator;
 import org.labkey.test.TestProperties;
+import org.labkey.test.WebTestHelper;
 import org.labkey.test.categories.CustomModules;
 import org.labkey.test.categories.DailyA;
 import org.labkey.test.categories.DailyB;
 import org.labkey.test.categories.DailyC;
 import org.labkey.test.categories.Git;
 import org.labkey.test.io.Grep;
+import org.labkey.test.util.Maps;
 import org.labkey.test.util.PasswordUtil;
 import org.labkey.test.util.PipelineStatusTable;
+import org.openqa.selenium.WebElement;
 
 import java.io.File;
 import java.util.Arrays;
@@ -83,12 +86,21 @@ public class DatabaseDiagnosticsTest extends BaseWebDriverTest
         File[] logs = logDir.listFiles();
         Map<File, Integer> contaminatedLogs = Grep.grep(PasswordUtil.getPassword(), logs);
         Map<String, String> failureFiles = new TreeMap<>();
-        contaminatedLogs.keySet().stream().forEach(
+        contaminatedLogs.keySet().forEach(
                 file -> failureFiles.put(file.getName(), "line " + contaminatedLogs.get(file)));
 
         assertTrue(String.format("These tomcat logs (in %s) contained unwanted text [%s]:\n%s",
                 tomcatHome.getAbsolutePath(), PasswordUtil.getPassword(), failureFiles.toString()),
                 failureFiles.isEmpty());
+    }
+
+    @Test
+    public void collectActionExceptions()
+    {
+        beginAt(WebTestHelper.buildURL("admin", "actions", Maps.of("tabId", "exceptions")));
+        WebElement exceptionCount = waitForElement(Locator.id("exceptionCount"));
+        if (!"no exceptions".equals(exceptionCount.getText()))
+            getArtifactCollector().dumpPageSnapshot("exceptions", null);
     }
 
     @Override
