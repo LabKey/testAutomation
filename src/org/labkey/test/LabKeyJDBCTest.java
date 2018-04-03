7bb9ceda7617764cf5d7470970381162e530e807
Tony Galuhn <tgaluhn@labkey.com>
2018-04-03 23:27:05 +0000

    Spec 32151 Automated tests
    SVN r57411 |2018-04-03 23:27:05 +0000

--- src/org/labkey/test/LabKeyJDBCTest.java
+++ src/org/labkey/test/LabKeyJDBCTest.java
@@ -19,17 +19,19 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.labkey.remoteapi.query.jdbc.LabKeyConnection;
 import org.labkey.test.categories.BVT;
 import org.labkey.test.util.PasswordUtil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -137,6 +139,9 @@ public class LabKeyJDBCTest
         boolean foundEntityId = false;
         boolean foundRowId = false;
         boolean foundName = false;
+        boolean foundCreated = false;
+        boolean foundSearchable = false;
+        boolean foundInstalledVersion = false;
 
         while (rs.next())
         {
@@ -165,10 +170,101 @@ public class LabKeyJDBCTest
                 assertEquals("Wrong auto-increment for Name", "NO", rs.getString("IS_AUTOINCREMENT"));
                 assertEquals("Wrong auto-generated for Name", "NO", rs.getString("IS_GENERATEDCOLUMN"));
             }
+            if ("created".equalsIgnoreCase(column))
+            {
+                foundCreated = true;
+                assertEquals("Wrong type for Created", Types.TIMESTAMP, rs.getInt("DATA_TYPE"));
+            }
+            if ("searchable".equalsIgnoreCase(column))
+            {
+                foundSearchable = true;
+                assertEquals("Wrong type for Searchable", Types.BOOLEAN, rs.getInt("DATA_TYPE"));
+            }
+        }
+
+        rs = _connection.getMetaData().getColumns("/home", "core", "modules", null);
+        while (rs.next())
+        {
+            if ("installedVersion".equalsIgnoreCase(rs.getString("COLUMN_NAME")))
+            {
+                foundInstalledVersion = true;
+                assertEquals("Wrong type for InstalledVersion", Types.DOUBLE, rs.getInt("DATA_TYPE"));
+            }
         }
 
         assertTrue("Didn't find EntityId column", foundEntityId);
         assertTrue("Didn't find Name column", foundName);
         assertTrue("Didn't find RowId column", foundRowId);
+        assertTrue("Didn't find Created column", foundCreated);
+        assertTrue("Didn't find Searchable column", foundSearchable);
+        assertTrue("Didn't find InstalledVersion column", foundInstalledVersion);
+    }
+
+    @Test
+    public void testRootIsCatalog() throws SQLException
+    {
+        // With the default rootIsCatalog == false, schemas/tables are visible at the top level
+        validateTopLevelQueries(true);
+        ((LabKeyConnection)_connection).setRootIsCatalog(true);
+        validateTopLevelQueries(false);
+    }
+
+    private void validateTopLevelQueries(boolean expectQueries) throws SQLException
+    {
+        ResultSet rs = _connection.getMetaData().getTables("", null, null, null);
+        boolean foundContainers = false;
+        while (!foundContainers && rs.next())
+        {
+            String table = rs.getString("TABLE_NAME");
+            foundContainers = "containers".equalsIgnoreCase(table);
+        }
+        if (expectQueries)
+        {
+            assertTrue("Didn't find top level containers query", foundContainers);
+        }
+        else
+        {
+            assertFalse("Found top level containers query", foundContainers);
+        }
+    }
+
+    @Test
+    public void testConnectionNoOps() throws Exception
+    {
+        // these are no-ops required by supported client applications, verify that none of them throw
+        _connection.setAutoCommit(false);
+        _connection.setReadOnly(true);
+        _connection.clearWarnings();
+        _connection.commit();
+    }
+
+    @Test
+    public void testStatementResults() throws SQLException
+    {
+        PreparedStatement statement = _connection.prepareStatement("SELECT * FROM core.modules");
+        ResultSet rs = statement.executeQuery();
+
+        rs.next();
+        // test all implemented typed value getters. Some of these are allowed casts from the underlying database types
+        // If a value is being presented as an incorrect type, the call to a typed getter will fail.
+        rs.getString("name");
+        rs.getBigDecimal("installedVersion");
+        rs.getFloat("installedVersion");
+        rs.getDouble("installedVersion");
+
+        // this is an implemented no-op
+        statement.cancel();
+
+        statement = _connection.prepareStatement("SELECT * FROM core.containers");
+        rs = statement.executeQuery();
+        rs.next();
+        rs.getBoolean("searchable");
+        rs.getTimestamp("created");
+        rs.getDate("created");
+        rs.getTime("created");
+        rs.getString("entityId");
+        rs.getInt("rowId");
+        rs.getLong("rowId");
+        rs.getShort("rowId");
     }
 }
