d0299f8d1c2c8b600b54557713eac9441d76bc69
Cory Nathe <cnathe@labkey.com>
2018-05-08 01:58:56 +0000

    Spec #34009: test fixes for updates to use new LABKEY.ext4.BootstrapTabPanel - to replace ext4 tab panel on ehr-serviceRequests, ehr-enterData, ehr-colonyOverview
    SVN r58063 |2018-05-08 01:58:56 +0000

--- src/org/labkey/test/Locators.java
+++ src/org/labkey/test/Locators.java
@@ -22,6 +22,7 @@ public abstract class Locators
     public static final Locator.XPathLocator alertWarning = Locator.byClass("alert").withClass("alert-warning");
     public static final Locator signInLink = Locator.tagWithAttributeContaining("a", "href", "login.view");
     public static final Locator.XPathLocator folderTab = Locator.tagWithClass("div", "lk-nav-tabs-ct").append(Locator.tagWithClass("ul", "lk-nav-tabs")).childTag("li");
+    public static final Locator.XPathLocator webpartTitle = Locator.tagWithClass("div", "labkey-wp-title-text");
 
     public static Locator.XPathLocator headerContainer()
     {
