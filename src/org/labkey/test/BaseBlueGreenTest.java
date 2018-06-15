c576bed1d8c7e57acb7ca02391fc4d29cc639620
Dan Duffek <danield@labkey.com>
2018-06-15 00:28:01 +0000

    Updating with feedback from code review.
    SVN r58682 |2018-06-15 00:28:01 +0000

--- src/org/labkey/test/BaseBlueGreenTest.java
+++ src/org/labkey/test/BaseBlueGreenTest.java
@@ -5,14 +5,8 @@ public abstract class BaseBlueGreenTest extends BaseWebDriverTest
 {
     protected boolean runValidationOnly()
     {
-        if(null == System.getProperty("testValidationOnly"))
-            log("System property testValidationOnly is null");
-        else
-            log("Value of system property testValidationOnly is '" + System.getProperty("testValidationOnly").toLowerCase().trim() + "'");
-
         if ((null == System.getProperty("testValidationOnly")) || (System.getProperty("testValidationOnly").toLowerCase().trim().equals("false")))
         {
-            log("Going to do a full test run.");
             return false;
         }
         else
@@ -28,7 +22,6 @@ public abstract class BaseBlueGreenTest extends BaseWebDriverTest
         // Only run cleanup before the running a full test.
         if(!runValidationOnly() && !afterTest)
         {
-            log("Doing cleanup.");
             super.doCleanup(afterTest);
         }
     }
