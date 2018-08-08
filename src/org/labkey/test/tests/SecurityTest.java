223e505c3171f65c99914678ea5603f9f1acb032
Karl Lum <klum@labkey.com>
2018-08-08 20:36:11 +0000

    test fix
    SVN r59670 |2018-08-08 20:36:11 +0000

--- src/org/labkey/test/tests/SecurityTest.java
+++ src/org/labkey/test/tests/SecurityTest.java
@@ -160,7 +160,7 @@ public class SecurityTest extends BaseWebDriverTest
                 "/user/showUsers.view?", "/security/project.view?returnUrl=%2Fuser%2FshowUsers.view%3F", "/admin/createFolder.view?",
                 "/analytics/begin.view?", "/login/configure.view?", "/admin/customizeEmail.view?", "/admin/filesSiteSettings.view?",
                 "/admin/projectSettings.view?", "/flow/flowAdmin.view?", "/admin/reorderFolders.view?", "/admin/customizeSite.view?",
-                "/reports/configureReportsAndScripts.view?", "/audit/showAuditLog.view?", "/search/admin.view?",
+                "/core/configureReportsAndScripts.view?", "/audit/showAuditLog.view?", "/search/admin.view?",
                 "/ms1/showAdmin.view?", "/ms2/showMS2Admin.view?", "/experiment-types/begin.view?", "/pipeline-status/showList.view?",
                 "/pipeline/setup.view?", "/ms2/showProteinAdmin.view?", "/admin/actions.view?", "/admin/caches.view?",
                 "/admin/dbChecker.view?", "/query/dataSourceAdmin.view?", "/admin/dumpHeap.view?", "/admin/environmentVariables.view?",
