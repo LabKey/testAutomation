913b13a3893339bd01a2ac956103fdf520357544
Cory Nathe <cnathe@labkey.com>
2018-09-03 21:03:32 +0000

    Rlabkey package update: labkey.transform helper functions - updated package version and changes to existing R transform scripts checked in to svn to use these helpers
    SVN r59987 |2018-09-03 21:03:32 +0000

--- data/qc/assayTransformWarning.R
+++ data/qc/assayTransformWarning.R
@@ -1,54 +1,4 @@
-
-
-readRunPropertiesFile <- function()
-{
-    # set up a data frame to store the run properties
-    properties = data.frame(NA, NA, NA, NA);
-    colnames(properties) = c("name", "val1", "val2", "val3");
-
-    #read in the run properties from the TSV
-    lines = readLines("${runInfo}");
-
-    # each line has a run property with the name, val1, val2, etc.
-    for (i in 1:length(lines))
-    {
-        # split the line into the various parts (tab separated)
-        parts = strsplit(lines[i], split="\t")[[1]];
-
-        # if the line does not have 4 parts, add NA's as needed
-        if (length(parts) < 4)
-        {
-            for (j in 1:4)
-            {
-                if (is.na(parts[j]))
-                {
-                    parts[j] = NA;
-                }
-            }
-        }
-
-        # add the parts for the given run property to the properties data frame
-        properties[i,] = parts;
-    }
-
-    properties
-}
-
-getRunPropertyValue <- function(colName)
-{
-    value = NA;
-    if (any(run.props$name == colName))
-    {
-        value = run.props$val1[run.props$name == colName];
-
-        # return NA for an empty string
-        if (nchar(value) == 0)
-        {
-            value = NA;
-        }
-    }
-    value;
-}
+suppressMessages(library(Rlabkey));
 
 setMaxSeverity <- function(level)
 {
@@ -60,7 +10,7 @@ setMaxSeverity <- function(level)
 	{
 		value = 2;
 	}
-	else if(getRunPropertyValue("severityLevel") != "ERROR" && level > run.error.level)
+	else if(labkey.transform.getRunPropertyValue(run.props, "severityLevel") != "ERROR" && level > run.error.level)
 	{
 		value = level;
 	}
@@ -102,12 +52,12 @@ handleErrors <- function()
 }
 
 run.error.level = 0; 
-run.props = readRunPropertiesFile();
+run.props = labkey.transform.readRunPropertiesFile("${runInfo}");
 
 # save the important run.props as separate variables
-run.data.file = getRunPropertyValue("runDataFile");
+run.data.file = labkey.transform.getRunPropertyValue(run.props, "runDataFile");
 run.output.file = run.props$val3[run.props$name == "runDataFile"];
-runprop.output.file = getRunPropertyValue("transformedRunPropertiesFile");
+runprop.output.file = labkey.transform.getRunPropertyValue(run.props, "transformedRunPropertiesFile");
 
 # Logic here that might generate errors or warnings
 # Set this to test different error levels
