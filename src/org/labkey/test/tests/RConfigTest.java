9f2af4586fd6ed1bde6ab51adc19c55e12290f7f
Emily Zhai <emilyz@labkey.com>
2018-09-14 21:58:26 +0000

    Nestle R report R engine association - RConfigTest review changes
    SVN r60202 |2018-09-14 21:58:26 +0000

--- src/org/labkey/test/tests/RConfigTest.java
+++ src/org/labkey/test/tests/RConfigTest.java
@@ -2,13 +2,13 @@ package org.labkey.test.tests;
 
 import org.jetbrains.annotations.Nullable;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.Locator;
 import org.labkey.test.Locator.XPathLocator;
-import org.labkey.test.TestProperties;
 import org.labkey.test.TestTimeoutException;
 import org.labkey.test.categories.DailyB;
 import org.labkey.test.pages.ConfigureReportsAndScriptsPage;
@@ -16,7 +16,6 @@ import org.labkey.test.pages.ConfigureReportsAndScriptsPage.EngineConfig;
 import org.labkey.test.pages.ConfigureReportsAndScriptsPage.EngineType;
 import org.labkey.test.pages.core.admin.ShowAdminPage;
 import org.labkey.test.util.Ext4Helper;
-import org.labkey.test.util.LogMethod;
 import org.labkey.test.util.RReportHelper;
 
 import java.util.ArrayList;
@@ -36,9 +35,6 @@ public class RConfigTest extends BaseWebDriverTest
 
     private String FOLDER_NAME = "subfolder";
 
-    private XPathLocator DEFAULT_CHECKBOX = XPathLocator.id("editEngine_default-inputEl");
-    private XPathLocator ENABLED_CHECKBOX = XPathLocator.id("editEngine_enabled-inputEl");
-
     private RReportHelper _RReportHelper = new RReportHelper(this);
     private Ext4Helper _Ext4Helper = new Ext4Helper(this);
 
@@ -49,7 +45,7 @@ public class RConfigTest extends BaseWebDriverTest
         ConfigureReportsAndScriptsPage scripts = ShowAdminPage.beginAt(getCurrentTest()).clickViewsAndScripting();
 
         log("Clean up scripting engines");
-        scripts.deleteEnginesFromList(R_ENGINES);
+         scripts.deleteEnginesFromList(R_ENGINES);
 
         log("Delete project");
         _containerHelper.deleteProject(getProjectName(), afterTest);
@@ -64,40 +60,49 @@ public class RConfigTest extends BaseWebDriverTest
 
     protected void doSetup() throws Exception
     {
-        _RReportHelper.ensureRConfig(false);
-
         _containerHelper.createProject(getProjectName(), "Custom");
         _containerHelper.createSubfolder(getProjectName(), FOLDER_NAME);
     }
 
-    @Test
-    public void testSteps() throws Exception
+    @Before
+    public void configureEngines()
     {
-        testAddMultipleEngines();
-        testEngineIsInherited();
-        testSwitchDefaultEngine();
-        testNoEngines();
+                log("Navigate to 'Views and Scripting' admin console page");
+        goToAdminConsole().goToAdminConsoleLinksSection();
+        ConfigureReportsAndScriptsPage scripts = ShowAdminPage.beginAt(getCurrentTest()).clickViewsAndScripting();
+
+        EngineConfig config = new EngineConfig(_RReportHelper.getRExecutable());
+
+        if (!scripts.isEnginePresent(DEFAULT_ENGINE_NAME))
+        {
+            _RReportHelper.ensureRConfig(false);
+        }
+
+        if (!scripts.isEnginePresent(SECONDARY_ENGINE_NAME))
+        {
+            log("Configure secondary R engine");
+            config.setName(SECONDARY_ENGINE_NAME);
+            scripts.addEngine(EngineType.R, config);
+        }
+
+        if (!scripts.isEnginePresent(DISABLED_ENGINE_NAME))
+        {
+            log("Configure and disable R engine");
+            config.setName(DISABLED_ENGINE_NAME);
+            scripts.addEngine(EngineType.R, config);
+            scripts.editEngine(DISABLED_ENGINE_NAME);
+            Assert.assertTrue(_Ext4Helper.isChecked(XPathLocator.id("editEngine_enabled-inputEl")));
+            _Ext4Helper.uncheckCheckbox(XPathLocator.id("editEngine_enabled-inputEl"));
+            clickButton("Submit", -1);
+        }
     }
 
-    @LogMethod
-    private void testAddMultipleEngines()
+    @Test
+    public void testAddMultipleEngines()
     {
         log("Navigate to 'Views and Scripting' admin console page");
         goToAdminConsole().goToAdminConsoleLinksSection();
         ConfigureReportsAndScriptsPage scripts = ShowAdminPage.beginAt(getCurrentTest()).clickViewsAndScripting();
-
-        log("Configure secondary R engine");
-        EngineConfig config = new EngineConfig(_RReportHelper.getRExecutable());
-        config.setName(SECONDARY_ENGINE_NAME);
-        scripts.addEngine(EngineType.R, config);
-
-        log("Configure and disable R engine");
-        config.setName(DISABLED_ENGINE_NAME);
-        scripts.addEngine(EngineType.R, config);
-        scripts.editEngine(DISABLED_ENGINE_NAME);
-        Assert.assertTrue(_Ext4Helper.isChecked(ENABLED_CHECKBOX));
-        _Ext4Helper.uncheckCheckbox(ENABLED_CHECKBOX);
-        clickButton("Submit", -1);
 
         log("Verify R engines exist");
         Assert.assertTrue("Default R engine does not exist", scripts.isEnginePresent(DEFAULT_ENGINE_NAME));
@@ -105,8 +110,8 @@ public class RConfigTest extends BaseWebDriverTest
         Assert.assertTrue("Disabled R engine was not created properly", scripts.isEnginePresent(DISABLED_ENGINE_NAME));
     }
 
-    @LogMethod
-    private void testEngineIsInherited()
+    @Test
+    public void testEngineIsInherited()
     {
         log("Navigate to 'R Config' tab in folder management page");
         goToProjectHome();
@@ -136,8 +141,8 @@ public class RConfigTest extends BaseWebDriverTest
         Assert.assertEquals("Subfolder is not inheriting the correct parent R configuration", SECONDARY_ENGINE_NAME, getText(Locator.id("parentConfig")));
     }
 
-    @LogMethod
-    private void testSwitchDefaultEngine()
+    @Test
+    public void testSwitchDefaultEngine()
     {
         log("Navigate to 'Views and Scripting' admin console page");
         goToAdminConsole().goToAdminConsoleLinksSection();
@@ -145,30 +150,31 @@ public class RConfigTest extends BaseWebDriverTest
 
         log("Verify site default checkbox is checked and disabled");
         scripts.editEngine(DEFAULT_ENGINE_NAME);
-        Assert.assertTrue(_Ext4Helper.isChecked(DEFAULT_CHECKBOX));
-        assertAttributeEquals(DEFAULT_CHECKBOX, "disabled", "true");
+        Assert.assertTrue(_Ext4Helper.isChecked(XPathLocator.id("editEngine_default-inputEl")));
+        assertAttributeEquals(XPathLocator.id("editEngine_default-inputEl"), "disabled", "true");
 
         log("Disabling site default should be prevented");
-        Assert.assertTrue(_Ext4Helper.isChecked(ENABLED_CHECKBOX));
-        _Ext4Helper.uncheckCheckbox(ENABLED_CHECKBOX);
+        Assert.assertTrue(_Ext4Helper.isChecked(XPathLocator.id("editEngine_enabled-inputEl")));
+        _Ext4Helper.uncheckCheckbox(XPathLocator.id("editEngine_enabled-inputEl"));
         clickButton("Submit", -1);
         assertExt4MsgBox("Site default engine must be enabled.", "OK");
         clickButton("Cancel", -1);
 
         log("Switch site default");
         scripts.editEngine(SECONDARY_ENGINE_NAME);
-        Assert.assertFalse(_Ext4Helper.isChecked(DEFAULT_CHECKBOX));
-        _Ext4Helper.checkCheckbox(DEFAULT_CHECKBOX);
+        XPathLocator loc = XPathLocator.id("editEngine_default-inputEl");
+        Assert.assertFalse(_Ext4Helper.isChecked(XPathLocator.id("editEngine_default-inputEl")));
+        _Ext4Helper.checkCheckbox(XPathLocator.id("editEngine_default-inputEl"));
         click(Locator.linkWithText("Submit"));
         acceptAlert();
         _Ext4Helper.waitForMaskToDisappear();
 
         log("Verify site default has switched in admin console page");
         scripts.editEngine(DEFAULT_ENGINE_NAME);
-        Assert.assertFalse(_Ext4Helper.isChecked(DEFAULT_CHECKBOX));
+        Assert.assertFalse(_Ext4Helper.isChecked(XPathLocator.id("editEngine_default-inputEl")));
         clickButton("Cancel", -1);
         scripts.editEngine(SECONDARY_ENGINE_NAME);
-        Assert.assertTrue(_Ext4Helper.isChecked(DEFAULT_CHECKBOX));
+        Assert.assertTrue(_Ext4Helper.isChecked(XPathLocator.id("editEngine_default-inputEl")));
         clickButton("Cancel", -1);
 
         log("Verify site default has switched in folder management page");
@@ -178,48 +184,20 @@ public class RConfigTest extends BaseWebDriverTest
         assertRadioButtonSelected(Locator.radioButtonByNameAndValue("overrideDefault", "parent"));
     }
 
-    @LogMethod
-    private void testNoEngines()
+    @Test
+    public void testNoEngines()
     {
-        // Only run test in TeamCity to prevent deleting local R instances
-        if (TestProperties.isTestRunningOnTeamCity())
-        {
-            log("Navigate to 'Views and Scripting' admin console page");
-            goToAdminConsole().goToAdminConsoleLinksSection();
-            ConfigureReportsAndScriptsPage scripts = ShowAdminPage.beginAt(getCurrentTest()).clickViewsAndScripting();
+        log("Navigate to 'Views and Scripting' admin console page");
+        goToAdminConsole().goToAdminConsoleLinksSection();
+        ConfigureReportsAndScriptsPage scripts = ShowAdminPage.beginAt(getCurrentTest()).clickViewsAndScripting();
 
-            log("Delete all R engine definitions");
-            scripts.deleteEnginesForLanguage("R");
+        log("Delete all R engine definitions");
+        scripts.deleteEnginesForLanguage("R");
 
-            log("Verify user is notified of lack of engines in folder management page");
-            goToProjectHome();
-            goToFolderManagement().goToRConfigTab();
-            assertElementPresent(Locator.tagWithText("h4", "No Available R Configurations"));
-        }
-    }
-
-    private void resetDefaultConfiguration(ConfigureReportsAndScriptsPage scripts)
-    {
-        resetDefaultConfiguration(scripts, DEFAULT_ENGINE_NAME);
-    }
-
-    private void resetDefaultConfiguration(ConfigureReportsAndScriptsPage scripts, String engineName)
-    {
-        if(scripts.isEnginePresent(engineName))
-        {
-            scripts.editEngine(engineName);
-            if(!_Ext4Helper.isChecked(DEFAULT_CHECKBOX))
-            {
-                _Ext4Helper.checkCheckbox(DEFAULT_CHECKBOX);
-                click(Locator.linkWithText("Submit"));
-                acceptAlert();
-                _Ext4Helper.waitForMaskToDisappear();
-            }
-            else
-            {
-                clickButton("Cancel", -1);
-            }
-        }
+        log("Verify user is notified of lack of engines in folder management page");
+        goToProjectHome();
+        goToFolderManagement().goToRConfigTab();
+        assertElementPresent(Locator.tagWithText("h4", "No Available R Configurations"));
     }
 
     @Override
