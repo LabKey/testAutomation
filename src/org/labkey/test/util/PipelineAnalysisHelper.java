0a372542918e1d872f4ceb7d5718ad471d146a69
Chris Joosse <chrisj@labkey.com>
2018-09-26 15:55:48 +0000

    don't regard file status text to be part of file name
    SVN r60382 |2018-09-26 15:55:48 +0000

--- src/org/labkey/test/util/PipelineAnalysisHelper.java
+++ src/org/labkey/test/util/PipelineAnalysisHelper.java
@@ -93,7 +93,8 @@ public class PipelineAnalysisHelper
         _test._fileBrowserHelper.selectImportDataAction(importAction);
 
         _test.waitForElement(Locator.id("fileStatus").containing(files[0]), BaseWebDriverTest.WAIT_FOR_JAVASCRIPT, false);
-        assertEquals("Wrong file(s)", fileNames, new HashSet<>(Arrays.asList(_test.getText(Locator.id("fileStatus")).trim().split("\n"))));
+        assertEquals("Wrong file(s)", fileNames,
+                new HashSet<>(Arrays.asList(_test.getText(Locator.id("fileStatus")).replace("(Refreshing status)", "").trim().split("\n"))));
         for (Map.Entry<String, String> property : protocolProperties.entrySet())
         {
             if ("xmlParameters".equals(property.getKey()))
