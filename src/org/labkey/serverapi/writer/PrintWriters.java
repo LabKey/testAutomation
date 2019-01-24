b5859aa86e79ad9b90c14137bc76b447d822e67c
Chris Joosse <chrisj@labkey.com>
2019-01-24 23:15:28 +0000

    Issue 36118: refactor test usages of PrintWriters
    SVN r62015 |2019-01-24 23:15:28 +0000

---
+++ src/org/labkey/serverapi/writer/PrintWriters.java
@@ -0,0 +1,51 @@
+package org.labkey.serverapi.writer;
+
+import org.labkey.serverapi.reader.StringUtilsLabKey;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class PrintWriters
+{
+    /**
+     * Create a standard PrintWriter, with standard character set and buffering, for output to an OutputStream
+     *
+     * @param out OutputStream destination for the new PrintWriter
+     * @return A standard, buffered PrintWriter targeting the OutputStream
+     */
+    public static PrintWriter getPrintWriter(OutputStream out)
+    {
+        return new StandardPrintWriter(out);
+    }
+
+    /**
+     * Create a standard PrintWriter, with standard character set and buffering, for output to a File
+     *
+     * @param file File destination for the new PrintWriter
+     * @return A standard, buffered PrintWriter targeting the File
+     */
+    public static PrintWriter getPrintWriter(File file) throws FileNotFoundException
+    {
+        return new StandardPrintWriter(file);
+    }
+
+
+    // Use factory methods above, unless you really need to subclass
+    public static class StandardPrintWriter extends PrintWriter
+    {
+        public StandardPrintWriter(OutputStream out)
+        {
+            super(new BufferedWriter(new OutputStreamWriter(out, StringUtilsLabKey.DEFAULT_CHARSET)));
+        }
+
+        public StandardPrintWriter(File file) throws FileNotFoundException
+        {
+            super(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StringUtilsLabKey.DEFAULT_CHARSET)));
+        }
+    }
+}
