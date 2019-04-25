331c16eb99f899aee8bf98226d2917842c77513a
Trey Chadick <tchad@labkey.com>
2019-04-25 15:16:01 +0000

    Remove injection exclusions for fixed issues
    Don't attempt injection for `.lastFilter` from action that triggers POST
    
    SVN r63478 |2019-04-25 15:16:01 +0000

--- src/org/labkey/test/util/Crawler.java
+++ src/org/labkey/test/util/Crawler.java
@@ -336,14 +336,11 @@ public class Crawler
     protected Map<ControllerActionId, List<String>> getExcludedParametersFromInjection()
     {
         Map<ControllerActionId, List<String>> map = new HashMap<>();
-        map.put(new ControllerActionId("assay", "assayResults"), Collections.singletonList("Data.Run/RowId")); // TODO: 23321: Bad rowId input for assay details triggers server errors
-        map.put(new ControllerActionId("assay", "assayRuns"), Collections.singletonList("Data.Batch/RowId")); // TODO: 23321: Bad rowId input for assay details triggers server errors
         map.put(new ControllerActionId("study-designer", "designer"), Collections.singletonList("panel")); // TODO: 16768: study-designer.DesignerAction: IllegalArgumentException on bad 'panel'
-        map.put(new ControllerActionId("study-samples", "samples"), Collections.singletonList("AtRepository")); // TODO: 21337: study-samples.SamplesAction: SQLGenerationException from un-parseable URL parameters
-        map.put(new ControllerActionId("assay", "uploadWizard"), Arrays.asList("participantVisitResolver", "targetStudy")); // TODO: 37037: Assay upload wizard chokes on malformed parameters
 
         // Permanent exclusions
         map.put(new ControllerActionId("plate", "designer"), Arrays.asList("colCount", "rowCount")); // 37208: Plate designer dumps stack trace from bad URL parameters
+        map.put(new ControllerActionId("reports", "runReport"), Arrays.asList(".lastFilter")); // Action triggers a POST, which logs an error. See `ViewServlet.requestActionURL`
 
         return map;
     }
