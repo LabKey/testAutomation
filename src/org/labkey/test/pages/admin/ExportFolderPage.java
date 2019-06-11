8ae9e1811cd1335a906fe19fbedc9e8b168930cc
Chris Joosse <chrisj@labkey.com>
2019-06-11 11:10:23 -0700

    Refactor tests to use ExportFolderPage class

--- src/org/labkey/test/pages/admin/ExportFolderPage.java
+++ src/org/labkey/test/pages/admin/ExportFolderPage.java
@@ -4,9 +4,12 @@ import org.labkey.test.Locator;
 import org.labkey.test.WebDriverWrapper;
 import org.labkey.test.WebTestHelper;
 import org.labkey.test.components.ext4.Checkbox;
+import org.labkey.test.components.ext4.RadioButton;
 import org.labkey.test.pages.LabKeyPage;
 import org.labkey.test.util.Maps;
 import org.openqa.selenium.WebDriver;
+
+import java.io.File;
 
 public class ExportFolderPage extends LabKeyPage<ExportFolderPage.ElementCache>
 {
@@ -29,22 +32,43 @@ public class ExportFolderPage extends LabKeyPage<ExportFolderPage.ElementCache>
     public ExportFolderPage selectExperimentsAndRuns(boolean checked)
     {
         elementCache().experimentsAndRunsCheckbox.set(checked);
-
         return this;
     }
 
-    public ExportFolderPage selectRoleAssignments(boolean checked)
+    public ExportFolderPage exportRoleAssignments(boolean checked)
     {
-        elementCache().selectRoleAssighmentsCheckbox.set(checked);
-
+        elementCache().roleAssighmentsCheckbox.set(checked);
         return this;
     }
 
-    public ExportFolderPage selectFiles(boolean checked)
+    public ExportFolderPage includeFiles(boolean checked)
     {
         elementCache().selectFilesCheckbox.set(checked);
-
         return this;
+    }
+
+    public ExportFolderPage selectETLDefintions(boolean checked)
+    {
+        elementCache().etlDefinitionsCheckbox.set(checked);
+        return this;
+    }
+
+    public ExportFolderPage exportSecurityGroups(boolean checked)
+    {
+        elementCache().exportSecurityGroupsCheckbox.set(checked);
+        return this;
+    }
+
+    public ExportFolderPage includeSubfolders(boolean checked)
+    {
+        elementCache().includeSubfoldersCheckBox.set(checked);
+        return this;
+    }
+
+    public File exportToBrowserAsZipFile()
+    {
+        elementCache().browserAsZipFileToggle.check();
+        return clickAndWaitForDownload(Locator.linkWithSpan("Export"));
     }
 
     protected ElementCache newElementCache()
@@ -57,11 +81,21 @@ public class ExportFolderPage extends LabKeyPage<ExportFolderPage.ElementCache>
         public Checkbox experimentsAndRunsCheckbox = new Checkbox(Locator.tagWithText("label", "Experiments and runs")
                 .precedingSibling("input").findWhenNeeded(getDriver()));
 
-        public Checkbox selectRoleAssighmentsCheckbox = new Checkbox(Locator.tagWithText("label", "Role assignments for users and groups")
+        public Checkbox roleAssighmentsCheckbox = new Checkbox(Locator.tagWithText("label", "Role assignments for users and groups")
                 .precedingSibling("input").findWhenNeeded(getDriver()));
 
         public Checkbox selectFilesCheckbox = new Checkbox(Locator.tagWithText("label", "Files")
                 .precedingSibling("input").findWhenNeeded(getDriver()));
+
+        public Checkbox etlDefinitionsCheckbox = new Checkbox(Locator.tagWithText("label", "ETL Definitions")
+                .precedingSibling("input").findWhenNeeded(getDriver()));
+        public Checkbox exportSecurityGroupsCheckbox = new Checkbox(Locator.tagWithText("label", "Project-level groups and members")
+                .precedingSibling("input").findWhenNeeded(getDriver()));
+        public Checkbox includeSubfoldersCheckBox = new Checkbox(Locator.tagContainingText("label", "Include Subfolders")
+                .precedingSibling("input").findWhenNeeded(getDriver()));
+
+        RadioButton browserAsZipFileToggle = new RadioButton(Locator.tagContainingText("label", "Browser as zip file")
+            .precedingSibling("input").findWhenNeeded(getDriver()));
 
     }
 }
