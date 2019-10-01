a905b88cd0bee8f98d44b999e7ceca025b99e075
Israel Hilerio <israelh@labkey.com>
2019-10-01 15:03:45 -0700

    Remove commented code

--- modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
+++ modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
@@ -3,33 +3,3 @@ ALTER TABLE vehicle.Colors ADD CONSTRAINT FK_Manufacturers_CycleCol FOREIGN KEY 
 
 ALTER TABLE vehicle.manufacturers ADD COLUMN CycleCol INT UNIQUE;
 ALTER TABLE vehicle.manufacturers ADD CONSTRAINT FK_Colors_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.Colors (CycleCol);
-
--- CREATE TABLE vehicle.SelfReferencingPrimary
--- (
---     RowId INT NOT NULL,
---     refCol INT NOT NULL UNIQUE,
---
---     CONSTRAINT PK_selfReferencingPrimary PRIMARY KEY (RowId)
--- );
---
---
--- CREATE TABLE vehicle.SelfReferencingSecondary
--- (
---     RowId INT NOT NULL,
---     refCol INT NOT NULL UNIQUE,
---
---     CONSTRAINT PK_selfReferencingSecondary PRIMARY KEY (RowId),
---     CONSTRAINT FK_SelfReferencingPrimary_refCol FOREIGN KEY (refCol) REFERENCES vehicle.SelfReferencingPrimary (refCol)
--- );
---
--- ALTER TABLE vehicle.SelfReferencingPrimary ADD CONSTRAINT FK_SelfReferencingSecondary_refCol FOREIGN KEY (refCol) REFERENCES vehicle.SelfReferencingSecondary (refCol)
-
--- CREATE TABLE vehicle.SelfReferencing
--- (
---     RowId INT NOT NULL,
---     rowId2 INT NOT NULL UNIQUE,
---     refCol INT NOT NULL,
---
---     CONSTRAINT PK_selfReferencing PRIMARY KEY (RowId),
---     CONSTRAINT FK_SelfReferencing_refCol FOREIGN KEY (refCol) REFERENCES vehicle.SelfReferencing (RowId2)
--- );
