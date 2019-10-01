2a7a278c60222d35e38bd0848c3138b389112ec1
Israel Hilerio <israelh@labkey.com>
2019-10-01 14:21:25 -0700

    Can reproduce: java.lang.IllegalStateException: Loop detected: null.VehicleId -> null null.ModelId -> null null.ManufacturerId -> null null.CycleCol -> null null.CycleCol -> null

--- modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
+++ modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
@@ -1,3 +1,9 @@
+ALTER TABLE vehicle.Colors ADD COLUMN CycleCol INT UNIQUE;
+ALTER TABLE vehicle.Colors ADD CONSTRAINT FK_Manufacturers_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.manufacturers (CycleCol);
+
+ALTER TABLE vehicle.manufacturers ADD COLUMN CycleCol INT UNIQUE;
+ALTER TABLE vehicle.manufacturers ADD CONSTRAINT FK_Colors_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.Colors (CycleCol);
+
 -- CREATE TABLE vehicle.SelfReferencingPrimary
 -- (
 --     RowId INT NOT NULL,
@@ -5,7 +11,7 @@
 --
 --     CONSTRAINT PK_selfReferencingPrimary PRIMARY KEY (RowId)
 -- );
-
+--
 --
 -- CREATE TABLE vehicle.SelfReferencingSecondary
 -- (
@@ -18,12 +24,12 @@
 --
 -- ALTER TABLE vehicle.SelfReferencingPrimary ADD CONSTRAINT FK_SelfReferencingSecondary_refCol FOREIGN KEY (refCol) REFERENCES vehicle.SelfReferencingSecondary (refCol)
 
-CREATE TABLE vehicle.SelfReferencing
-(
-    RowId INT NOT NULL,
-    rowId2 INT NOT NULL UNIQUE,
-    refCol INT NOT NULL,
-
-    CONSTRAINT PK_selfReferencing PRIMARY KEY (RowId),
-    CONSTRAINT FK_SelfReferencing_refCol FOREIGN KEY (refCol) REFERENCES vehicle.SelfReferencing (RowId2)
-);
+-- CREATE TABLE vehicle.SelfReferencing
+-- (
+--     RowId INT NOT NULL,
+--     rowId2 INT NOT NULL UNIQUE,
+--     refCol INT NOT NULL,
+--
+--     CONSTRAINT PK_selfReferencing PRIMARY KEY (RowId),
+--     CONSTRAINT FK_SelfReferencing_refCol FOREIGN KEY (refCol) REFERENCES vehicle.SelfReferencing (RowId2)
+-- );
