8198e97b7ee37123ac7e40499d6e2a91414a2932
Israel Hilerio <israelh@labkey.com>
2019-10-01 15:16:20 -0700

    Code clean up

--- modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
+++ modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
@@ -1,5 +1,5 @@
 ALTER TABLE vehicle.Colors ADD COLUMN CycleCol INT UNIQUE;
-ALTER TABLE vehicle.Colors ADD CONSTRAINT FK_Manufacturers_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.manufacturers (CycleCol);
+ALTER TABLE vehicle.Manufacturers ADD COLUMN CycleCol INT UNIQUE;
 
-ALTER TABLE vehicle.manufacturers ADD COLUMN CycleCol INT UNIQUE;
-ALTER TABLE vehicle.manufacturers ADD CONSTRAINT FK_Colors_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.Colors (CycleCol);
+ALTER TABLE vehicle.Colors ADD CONSTRAINT FK_Manufacturers_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.Manufacturers (CycleCol);
+ALTER TABLE vehicle.Manufacturers ADD CONSTRAINT FK_Colors_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.Colors (CycleCol);
