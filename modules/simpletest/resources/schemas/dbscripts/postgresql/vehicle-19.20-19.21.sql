7d34080e68a6195792349fd43f600c27f1a6a1f1
Israel Hilerio <israelh@labkey.com>
2019-10-02 10:17:07 -0700

    Repped without containers

--- modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
+++ modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
@@ -1,5 +1,28 @@
-ALTER TABLE vehicle.Colors ADD COLUMN CycleCol INT UNIQUE;
-ALTER TABLE vehicle.Manufacturers ADD COLUMN CycleCol INT UNIQUE;
+CREATE TABLE vehicle.FirstFKTable
+(
+    RowId INT NOT NULL,
+    StartCycleCol INT NOT NULL UNIQUE,
+    CONSTRAINT PK_FirstFKTable PRIMARY KEY (RowId)
+);
 
-ALTER TABLE vehicle.Colors ADD CONSTRAINT FK_Manufacturers_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.Manufacturers (CycleCol);
-ALTER TABLE vehicle.Manufacturers ADD CONSTRAINT FK_Colors_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.Colors (CycleCol);
+CREATE TABLE vehicle.SecondFKTable
+(
+    RowId INT NOT NULL,
+    StartCycleCol INT NOT NULL UNIQUE,
+    CycleCol INT NOT NULL UNIQUE,
+
+    CONSTRAINT PK_SecondFKTable PRIMARY KEY (RowId)
+);
+
+CREATE TABLE vehicle.ThirdFKTable
+(
+    RowId INT NOT NULL,
+    CycleCol INT NOT NULL UNIQUE,
+
+    CONSTRAINT PK_ThirdFKTable PRIMARY KEY (RowId)
+);
+
+--ALTER TABLE vehicle.SecondFKTable ADD CONSTRAINT FK_FirstFKTable_StartCycleCol FOREIGN KEY (StartCycleCol) REFERENCES vehicle.FirstFKTable (StartCycleCol);
+ALTER TABLE vehicle.FirstFKTable ADD CONSTRAINT FK_ThirdFKTable_StartCycleCol FOREIGN KEY (StartCycleCol) REFERENCES vehicle.SecondFKTable (StartCycleCol);
+ALTER TABLE vehicle.SecondFKTable ADD CONSTRAINT FK_ThirdFKTable_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.ThirdFKTable (CycleCol);
+ALTER TABLE vehicle.ThirdFKTable ADD CONSTRAINT FK_SecondFKTable_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.SecondFKTable (CycleCol);
