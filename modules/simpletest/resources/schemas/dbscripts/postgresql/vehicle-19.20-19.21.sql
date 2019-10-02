e19cc8a18874780f31515df1665ff8c610bb9c21
Israel Hilerio <israelh@labkey.com>
2019-10-02 11:52:38 -0700

    Manufacturers table added back

--- modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
+++ modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
@@ -22,7 +22,6 @@ CREATE TABLE vehicle.ThirdFKTable
     CONSTRAINT PK_ThirdFKTable PRIMARY KEY (RowId)
 );
 
---ALTER TABLE vehicle.SecondFKTable ADD CONSTRAINT FK_FirstFKTable_StartCycleCol FOREIGN KEY (StartCycleCol) REFERENCES vehicle.FirstFKTable (StartCycleCol);
 ALTER TABLE vehicle.FirstFKTable ADD CONSTRAINT FK_ThirdFKTable_StartCycleCol FOREIGN KEY (StartCycleCol) REFERENCES vehicle.SecondFKTable (StartCycleCol);
 ALTER TABLE vehicle.SecondFKTable ADD CONSTRAINT FK_ThirdFKTable_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.ThirdFKTable (CycleCol);
 ALTER TABLE vehicle.ThirdFKTable ADD CONSTRAINT FK_SecondFKTable_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.SecondFKTable (CycleCol);
---
+++ modules/simpletest/resources/schemas/dbscripts/sqlserver/vehicle-19.20-19.21.sql
@@ -0,0 +1,27 @@
+CREATE TABLE vehicle.FirstFKTable
+(
+    RowId INT IDENTITY(1,1),
+    StartCycleCol INT NOT NULL UNIQUE,
+    CONSTRAINT PK_FirstFKTable PRIMARY KEY (RowId)
+);
+
+CREATE TABLE vehicle.SecondFKTable
+(
+    RowId INT IDENTITY(1,1),
+    StartCycleCol INT NOT NULL UNIQUE,
+    CycleCol INT NOT NULL UNIQUE,
+
+    CONSTRAINT PK_SecondFKTable PRIMARY KEY (RowId)
+);
+
+CREATE TABLE vehicle.ThirdFKTable
+(
+    RowId INT IDENTITY(1,1),
+    CycleCol INT NOT NULL UNIQUE,
+
+    CONSTRAINT PK_ThirdFKTable PRIMARY KEY (RowId)
+);
+
+ALTER TABLE vehicle.FirstFKTable ADD CONSTRAINT FK_ThirdFKTable_StartCycleCol FOREIGN KEY (StartCycleCol) REFERENCES vehicle.SecondFKTable (StartCycleCol);
+ALTER TABLE vehicle.SecondFKTable ADD CONSTRAINT FK_ThirdFKTable_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.ThirdFKTable (CycleCol);
+ALTER TABLE vehicle.ThirdFKTable ADD CONSTRAINT FK_SecondFKTable_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.SecondFKTable (CycleCol);
