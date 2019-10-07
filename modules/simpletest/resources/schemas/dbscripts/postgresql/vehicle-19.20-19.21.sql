5c785ed561d52b868d1fbfb3ac20133b31d7776f
Israel Hilerio <israelh@labkey.com>
2019-10-07 10:57:13 -0700

    Updated names in sql script upgrade and xml updated

--- modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
+++ modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-19.20-19.21.sql
@@ -22,6 +22,6 @@ CREATE TABLE vehicle.ThirdFKTable
     CONSTRAINT PK_ThirdFKTable PRIMARY KEY (RowId)
 );
 
-ALTER TABLE vehicle.FirstFKTable ADD CONSTRAINT FK_ThirdFKTable_StartCycleCol FOREIGN KEY (StartCycleCol) REFERENCES vehicle.SecondFKTable (StartCycleCol);
+ALTER TABLE vehicle.FirstFKTable ADD CONSTRAINT FK_SecondFKTable_StartCycleCol FOREIGN KEY (StartCycleCol) REFERENCES vehicle.SecondFKTable (StartCycleCol);
 ALTER TABLE vehicle.SecondFKTable ADD CONSTRAINT FK_ThirdFKTable_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.ThirdFKTable (CycleCol);
 ALTER TABLE vehicle.ThirdFKTable ADD CONSTRAINT FK_SecondFKTable_CycleCol FOREIGN KEY (CycleCol) REFERENCES vehicle.SecondFKTable (CycleCol);
