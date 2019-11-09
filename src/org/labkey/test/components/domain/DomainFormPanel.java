fa30e5adf0c1bdf6ef6ec9d14137011d8a6f9c3b
labkey-martyp <martyp@labkey.com>
2019-11-08 17:02:22 -0800

    Update src/org/labkey/test/components/domain/DomainFormPanel.java
    Co-Authored-By: Trey Chadick <tchad@labkey.com>

--- src/org/labkey/test/components/domain/DomainFormPanel.java
+++ src/org/labkey/test/components/domain/DomainFormPanel.java
@@ -172,7 +172,7 @@ public class DomainFormPanel extends WebDriverComponent<DomainFormPanel.ElementC
         if (isExpanded())
         {
             elementCache().collapseIcon.click();
-            getWrapper().sleep(250); // wait for transition to happen
+            getWrapper().shortWait().until(LabKeyExpectedConditions.animationIsDone(getComponentElement())); // wait for transition to happen
         }
         return this;
     }
