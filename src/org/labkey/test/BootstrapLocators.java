e03831e7ef01992a13bcb19df1794280b759a523
labkey-tchad <tchad@labkey.com>
2019-12-18 11:19:57 -0800

    Add NotificationBanner component

--- src/org/labkey/test/BootstrapLocators.java
+++ src/org/labkey/test/BootstrapLocators.java
@@ -2,8 +2,28 @@ package org.labkey.test;
 
 public abstract class BootstrapLocators
 {
-    static public Locator infoBanner = Locator.tagWithClass("div", "alert-info");
-    static public Locator successBanner = Locator.tagWithClass("div", "alert-success");
-    static public Locator dangerBanner = Locator.tagWithClass("div", "alert-danger");
-    static public Locator warningBanner = Locator.tagWithClass("div", "alert-warning");
+    static public Locator infoBanner = Locator.tagWithClass("div", BannerType.INFO.getCss());
+    static public Locator successBanner = Locator.tagWithClass("div", BannerType.SUCCESS.getCss());
+    static public Locator dangerBanner = Locator.tagWithClass("div", BannerType.ERROR.getCss());
+    static public Locator warningBanner = Locator.tagWithClass("div", BannerType.WARNING.getCss());
+
+    public enum BannerType
+    {
+        SUCCESS("success-alert"),
+        INFO("info-alert"),
+        WARNING("warning-alert"),
+        ERROR("danger-alert");
+
+        private final String _css;
+
+        BannerType(String css)
+        {
+            _css = css;
+        }
+
+        public String getCss()
+        {
+            return _css;
+        }
+    }
 }
