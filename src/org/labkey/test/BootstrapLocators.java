9c22a8482cd5225eb84f257404b2b915fdb4783c
Dan Duffek <danield@labkey.com>
2019-12-30 22:06:07 -0800

    Changing name from alert to banner. (#182)
    * Changed to user "banner" and not "alert" to describe the message at the top of the page. Alert was too easily confused with another dialog type.
    
    * Add NotificationBanner component
    
    * Fixing tests that are expecting errors.
    
    Co-authored-by: Trey Chadick <tchad@labkey.com>

--- src/org/labkey/test/BootstrapLocators.java
+++ src/org/labkey/test/BootstrapLocators.java
@@ -2,11 +2,31 @@ package org.labkey.test;
 
 public abstract class BootstrapLocators
 {
-    public static final Locator infoAlert = Locator.tagWithClass("div", "alert-info");
-    public static final Locator successAlert = Locator.tagWithClass("div", "alert-success");
-    public static final Locator dangerAlert = Locator.tagWithClass("div", "alert-danger");
-    public static final Locator warningAlert = Locator.tagWithClass("div", "alert-warning");
-    
+    public static final Locator infoBanner = Locator.tagWithClass("div", BannerType.INFO.getCss());
+    public static final Locator successBanner = Locator.tagWithClass("div", BannerType.SUCCESS.getCss());
+    public static final Locator errorBanner = Locator.tagWithClass("div", BannerType.ERROR.getCss());
+    public static final Locator warningBanner = Locator.tagWithClass("div", BannerType.WARNING.getCss());
+
+    public enum BannerType
+    {
+        SUCCESS("alert-success"),
+        INFO("alert-info"),
+        WARNING("alert-warning"),
+        ERROR("alert-danger");
+
+        private final String _css;
+
+        BannerType(String css)
+        {
+            _css = css;
+        }
+
+        public String getCss()
+        {
+            return _css;
+        }
+    }
+
     public static Locator panel(String panelHeading)
     {
         return Locator.byClass("panel").withChild(Locator.byClass("panel-heading").withText(panelHeading));
