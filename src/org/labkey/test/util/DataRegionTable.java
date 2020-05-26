2e9f693bd43be277d5d3bf381eef1c91a59c61f1
labkey-sweta <swetaj@labkey.com>
2020-05-26 01:39:48 -0700

    Automation for setting default dataset view

--- src/org/labkey/test/util/DataRegionTable.java
+++ src/org/labkey/test/util/DataRegionTable.java
@@ -26,6 +26,7 @@ import org.labkey.test.WebDriverWrapperImpl;
 import org.labkey.test.components.ColumnChartRegion;
 import org.labkey.test.components.CustomizeView;
 import org.labkey.test.components.PagingWidget;
+import org.labkey.test.components.SetDefaultPageClass;
 import org.labkey.test.components.SummaryStatisticsDialog;
 import org.labkey.test.components.ext4.Window;
 import org.labkey.test.components.html.BootstrapMenu;
@@ -135,6 +136,15 @@ public class DataRegionTable extends DataRegion
             _pagingWidget = new PagingWidget(this);
         }
         return _pagingWidget;
+    }
+
+    public SetDefaultPageClass clicksetDefault()
+    {
+        if (!getCustomizeView().isPanelExpanded())
+        {
+            getViewsMenu().clickSubMenu(false, "Set Default");
+        }
+        return new SetDefaultPageClass(getDriver());
     }
 
     public CustomizeView openCustomizeGrid()
