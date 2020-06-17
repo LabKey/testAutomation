6217369879f1426e9158fc3dd32ac07ee5bc4d9f
Rosaline Pyktel <rosalinep@labkey.com>
2020-06-17 09:59:07 -0700

    Attempt 2 at matching. Also fix wording on 'yes' button

--- src/org/labkey/test/pages/core/login/LoginConfigRow.java
+++ src/org/labkey/test/pages/core/login/LoginConfigRow.java
@@ -38,8 +38,8 @@ public class LoginConfigRow extends WebDriverComponent<LoginConfigRow.ElementCac
     {
         getWrapper().shortWait().until(ExpectedConditions.elementToBeClickable(elementCache().deleteButton));
         elementCache().deleteButton.click();
-        new ModalDialog.ModalDialogFinder(getDriver()).withTitle("Permanently delete")
-                .waitFor().dismiss("Yes");
+        new ModalDialog.ModalDialogFinder(getDriver()).withBodyTextContaining("Deletion cannot be undone.")
+                .waitFor().dismiss("Yes, Delete");
         getWrapper().shortWait().until(ExpectedConditions.stalenessOf(getComponentElement()));
         return new LoginConfigurePage(getDriver());
     }
