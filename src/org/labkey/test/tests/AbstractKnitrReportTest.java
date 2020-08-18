747031303a6d3936ce783359c3b47d920155b6ac
labkey-tchad <tchad@labkey.com>
2020-08-18 11:28:00 -0700

    Include date in data frame for test

--- src/org/labkey/test/tests/AbstractKnitrReportTest.java
+++ src/org/labkey/test/tests/AbstractKnitrReportTest.java
@@ -142,7 +142,7 @@ public abstract class AbstractKnitrReportTest extends BaseWebDriverTest
     {
         Locator[] reportContains = {Locator.tag("p").withText("This is a minimal example which shows knitr working with HTML pages in LabKey."),
                                     Locator.tag("img").withAttribute("title", "plot of chunk blood-pressure-scatter"),
-                                    Locator.tag("pre").containing("## \"1\",249318596,\"2008-05-17\",86,36,129,76,64,17,0,\"false\",\"English\",\"urn:lsid:labkey.com:Study.Data-2156:5004.249318596.20080517.0000\""),
+                                    Locator.tag("pre").containing("## \"1\",249318596,\"2008-05-17\",86,36,129,76,64"),
                                     Locator.tag("pre").withText("## knitr says hello to HTML!"),
                                     Locator.tag("pre").startsWith("## Error").containing(": non-numeric argument to binary operator"),
                                     Locator.tag("p").startsWith("Well, everything seems to be working. Let's ask R what is the value of \u03C0? Of course it is 3.141")};
