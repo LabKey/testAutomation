916ecac59ce872b3d0c3de12ea0747394227c1d1
labkey-tchad <tchad@labkey.com>
2020-09-29 15:27:49 -0700

    New test module for testing module editor

---
+++ modules/editableModule/resources/reports/schemas/editableModuleReport.r
@@ -0,0 +1,9 @@
+##
+#  Copyright (c) 2013-2019 LabKey Corporation
+#
+#  Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+##
+cat('Hello, ', labkey.url.params$greeter, '!\n', sep='')
+
+cat("{ \"a\": [1,2,3], \"", labkey.url.params$greeter, "\": \"Hello\" }\n", sep="", file="${jsonout:hello.json}")
+
