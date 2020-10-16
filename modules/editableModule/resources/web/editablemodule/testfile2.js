f4eb5db5fa963bd5361e130f86f137e8fa6ec2a4
Trey Chadick <tchad@labkey.com>
2020-10-16 10:04:42 -0700

    New test module for module editor (#486)

---
+++ modules/editableModule/resources/web/editablemodule/testfile2.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2013-2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+function testfile2(div, content) {
+    div.innerHTML = content + ' Loaded dependent scripts.';
+}
