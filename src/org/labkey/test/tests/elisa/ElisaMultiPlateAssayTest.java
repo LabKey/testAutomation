ace07f75153f9c0ec5e2432490f622ace150f8a9
Chris Joosse <chrisj@labkey.com>
2020-10-29 18:37:02 -0700

    add validation to main scenarios and add resiliency test

--- src/org/labkey/test/tests/elisa/ElisaMultiPlateAssayTest.java
+++ src/org/labkey/test/tests/elisa/ElisaMultiPlateAssayTest.java
@@ -4,11 +4,13 @@ import org.junit.BeforeClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.labkey.api.exp.PropertyDescriptor;
 import org.labkey.remoteapi.Connection;
 import org.labkey.remoteapi.assay.GetProtocolCommand;
 import org.labkey.remoteapi.assay.Protocol;
 import org.labkey.remoteapi.assay.ProtocolResponse;
 import org.labkey.remoteapi.assay.SaveProtocolCommand;
+import org.labkey.remoteapi.domain.Domain;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.Locator;
 import org.labkey.test.TestFileUtils;
@@ -16,17 +18,20 @@ import org.labkey.test.categories.DailyB;
 import org.labkey.test.pages.ReactAssayDesignerPage;
 import org.labkey.test.pages.assay.AssayDataPage;
 import org.labkey.test.pages.assay.AssayRunsPage;
+import org.labkey.test.pages.assay.elisa.ElisaRunDetailsPage;
 import org.labkey.test.pages.assay.plate.PlateDesignerPage;
 import org.labkey.test.pages.assay.plate.PlateTemplateListPage;
-import org.labkey.test.util.DataRegionTable;
+import org.labkey.test.params.FieldDefinition;
 import org.labkey.test.util.ExperimentalFeaturesHelper;
 import org.labkey.test.util.PortalHelper;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
@@ -36,9 +41,6 @@ public class ElisaMultiPlateAssayTest extends BaseWebDriverTest
 {
     public final String EXP_FEATURE = "elisaMultiPlateSupport";
     static final File TEST_ASSAY_ELISA_FILE1 = TestFileUtils.getSampleData("Elisa/biotek_01.xlsx");
-    static final File TEST_ASSAY_ELISA_FILE2 = TestFileUtils.getSampleData("Elisa/biotek_02.xls");
-    static final File TEST_ASSAY_ELISA_FILE3 = TestFileUtils.getSampleData("Elisa/biotek_03.xls");
-    static final File TEST_ASSAY_ELISA_FILE4 = TestFileUtils.getSampleData("Elisa/biotek_04.xls");
     static final File THREE_PLATE_MSD = TestFileUtils.getSampleData("Elisa/3plateMSD.csv");
 
     @BeforeClass
@@ -108,11 +110,55 @@ public class ElisaMultiPlateAssayTest extends BaseWebDriverTest
     }
 
     @Test
+    public void testDetailsViewResiliencyWithRenamedFields() throws Exception
+    {
+        // arrange
+        String assayName = "test_AssayWithDifferentFieldNames";
+        String plateTemplateName = "high_speed_tangent";
+        String runId = "has different spot and plate";
+        ExperimentalFeaturesHelper.enableExperimentalFeature(createDefaultConnection(), EXP_FEATURE);
+
+        createPlateTemplate(16, 24, "high-throughput (multi plate)", plateTemplateName);
+        Protocol newAssayProtocol = getProtocolWithPlateTemplate(assayName, "ELISA", plateTemplateName);
+
+        // rename PlateName and Spot fields and ensure it still works
+        Domain resultsDomain = newAssayProtocol.getDomains().stream()
+                .filter(a -> a.getName().equals("Data Fields")).findFirst().orElse(null);
+        assertNotNull("expect data fields to be on the assay", resultsDomain);
+        List<org.labkey.remoteapi.domain.PropertyDescriptor> fields = resultsDomain.getFields();
+        for(org.labkey.remoteapi.domain.PropertyDescriptor field : fields)
+        {
+            if(field.getName().equals("PlateName"))
+                field.setName("PlateMoniker");
+            if(field.getName().equals("Spot"))
+                field.setName("Puff");
+        }
+
+        newAssayProtocol.setSelectedMetadataInputFormat("COMBINED");
+        saveProtocol(createDefaultConnection(), newAssayProtocol);
+
+        // act
+        uploadHighSpeedFile(assayName, runId, THREE_PLATE_MSD, "4 Parameter");
+
+        goToAssayRunsPage(assayName)
+                .getTable().clickRowDetails(0);
+        ElisaRunDetailsPage detailsPage = new ElisaRunDetailsPage(getDriver());
+
+        assertFalse("don't expect plate filter to be present", detailsPage.isPlateSelectPresent());
+        assertFalse("don't expect spot filter to be present", detailsPage.isSpotSelectPresent());
+
+        assertTrue(detailsPage.getAlertWarning().isPresent());
+        assertThat(detailsPage.getAlertWarning().get().getText(),
+                is("Warning: the assay design is missing the following fields which may affect the various plotting features and display of this page: PlateName, Spot."));
+    }
+
+    @Test
     public void testMultiPlateHighThroughputAssayWith4ParamCurveFit() throws Exception
     {
         // arrange
         String assayName = "test_high_throughput_assay_with_4_Param";
         String plateTemplateName = "high_speed_fancy";
+        String runId = "high-speed multiplate 4Param";
         ExperimentalFeaturesHelper.enableExperimentalFeature(createDefaultConnection(), EXP_FEATURE);
 
         createPlateTemplate(16, 24, "high-throughput (multi plate)", plateTemplateName);
@@ -121,22 +167,32 @@ public class ElisaMultiPlateAssayTest extends BaseWebDriverTest
         saveProtocol(createDefaultConnection(), newAssayProtocol);
 
         // act
-        uploadHighSpeedFile(assayName, "high speed multiplate first run", THREE_PLATE_MSD, "4 Parameter");
+        uploadHighSpeedFile(assayName, runId, THREE_PLATE_MSD, "4 Parameter");
 
         // assert
+        goToAssayRunsPage(assayName).clickViewResults().getDataTable()
+                .assertPaginationText(1, 100, 4608);
 
-        // sanity-check to see that we got data in the runs table at least
-        goToAssayRunsPage(assayName);
-        clickAndWait(Locator.linkWithText("view results"));
-        DataRegionTable dataTable = DataRegionTable.DataRegion(getDriver()).withName("Data").waitFor();
-        dataTable.assertPaginationText(1, 100, 4608);
+        goToAssayRunsPage(assayName)
+                .getTable().clickRowDetails(0);
+        ElisaRunDetailsPage detailsPage = new ElisaRunDetailsPage(getDriver());
 
-        // confirm the new react-based run details view
-        DataRegionTable runsTable = goToAssayRunsPage(assayName).getTable();
-        runsTable.clickRowDetails(runsTable.getRowIndex("Assay Id", "high speed multiplate first run")); // should be row 0
+        // ensure that expected options are there, at least
+        assertThat(detailsPage.getAvailableSamples(), hasItems("Sample 1", "Sample 2",
+                "Sample 3", "Sample 4","Sample 5","Sample 6","Sample 7","Sample 8", "Sample 9",
+                "Sample 10", "Sample 11", "Sample 12", "Sample 13", "Sample 14", "Sample 15",
+                "Sample 16", "Sample 17", "Sample 18", "Sample 19", "Sample 20", "SARS-COV2-POS-3"));
 
-        // todo: verify relevant/expected on assayRunDetails view
-        
+        // verify some selected option values
+        detailsPage.selectPlate("Plate_1LK05A1071")
+                .selectSpot("1")
+                .setSelectedSamples(List.of("Sample 11", "Sample 13"))
+                .setSelectedControls(List.of("Blank (Diluent)"));
+
+        // now go to the results view and expect the filters above to be in effect
+        detailsPage.clickViewResultsGrid().getDataTable()
+                .assertPaginationText(1, 35, 35);
+        closeExtraWindows();
     }
 
     @Test
@@ -144,6 +200,7 @@ public class ElisaMultiPlateAssayTest extends BaseWebDriverTest
     {
         String assayName = "test_high_throughput_assay_with_linear_curve";
         String plateTemplateName = "high_speed_regular";
+        String runId = "high-speed multiplate Linear";
         ExperimentalFeaturesHelper.enableExperimentalFeature(createDefaultConnection(), EXP_FEATURE);
 
         createPlateTemplate(16, 24, "high-throughput (multi plate)", plateTemplateName);
@@ -152,11 +209,31 @@ public class ElisaMultiPlateAssayTest extends BaseWebDriverTest
         saveProtocol(createDefaultConnection(), newAssayProtocol);
 
         // act
-        uploadHighSpeedFile(assayName, "high speed multiplate linear run", THREE_PLATE_MSD, "Linear");
+        uploadHighSpeedFile(assayName, runId, THREE_PLATE_MSD, "Linear");
 
         // assert
+        goToAssayRunsPage(assayName)
+                .getTable().clickRowDetails(0);
 
-        // todo: verify
+        ElisaRunDetailsPage detailsPage = new ElisaRunDetailsPage(getDriver());
+        detailsPage.selectPlate("Plate_1LK05AG072")
+                .selectSpot("3")
+                .setShowCurveFitLineCheckboxSelected(true);
+
+        // confirm that selected spot/plate combinations show expected curve fit params
+        List<Map<String, Object>> curveFitRows = executeSelectRowCommand(
+                "assay.ELISA.test_high_throughput_assay_with_linear_curve", "CurveFit" ).getRows();
+        Map<String, Object> fitParamsRow = curveFitRows.stream()
+                .filter(a-> "Plate_1LK05AG072".equals(a.get("PlateName")) && a.get("Spot").equals(3))
+                .findFirst().get();
+        assertNotNull(fitParamsRow);
+        String expectedFitParams = fitParamsRow.get("FitParameters").toString().replace(" ", "");
+
+        assertTrue("Expect curve fit details to be selected by default", detailsPage.getShowCurveFitLineCheckboxSelected());
+        assertTrue("Expect rSquared element to be visible", detailsPage.getRSquaredShown());
+        assertTrue("Expect fitParameters to be visible", detailsPage.getCurveFitParamsShown());
+        assertThat("Expect selection to reflect expected curve fit params",
+                detailsPage.getCurveFitParams(), is(expectedFitParams));
     }
 
     @Test
@@ -176,13 +253,26 @@ public class ElisaMultiPlateAssayTest extends BaseWebDriverTest
         AssayRunsPage runsPage = uploadFile(assayName, "manual linear run", TEST_ASSAY_ELISA_FILE1, "Linear",
                 "A", 1, 5);
 
-
         AssayDataPage dataPage = runsPage.clickViewResults();
         dataPage.getDataTable().assertPaginationText(1, 92, 92);
 
-        // todo: verify view info
+        goToAssayRunsPage(assayName)
+                .getTable().clickRowDetails(0);
+        ElisaRunDetailsPage detailsPage = new ElisaRunDetailsPage(getDriver());
 
-        log("foo");
+        assertFalse("Plate select should not be shown for single-plate assay", detailsPage.isPlateSelectPresent());
+        assertFalse("Spot select should not be shown for single-plate assay", detailsPage.isSpotSelectPresent());
+
+        assertThat("Expect these specimens to appear in the sample filter box",
+                detailsPage.getAvailableSamples(),
+                hasItems("specimen 1 A", "specimen 2 A", "specimen 3 A", "specimen 4 A", "specimen 5 A"));
+        assertThat("expect just 1 control", detailsPage.getAvailableControls(), hasItems("Standard"));
+
+        detailsPage.clickViewResultsGrid()
+                .getDataTable()
+                .assertPaginationText(1, 12, 12);
+
+        closeExtraWindows();
     }
 
     private AssayRunsPage uploadHighSpeedFile(String assayName, String runName, File file, String curveFitMethod)
