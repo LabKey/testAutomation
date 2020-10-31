793bfca3d2c7f5c40cd14afe88641b437485f875
Kevin Krouse <kevink@labkey.com>
2020-10-30 17:23:30 -0700

    Issue 40442: LinkedSchema should throw handled exception (#515)
    - update expected error
    - update and use list API to create list more better faster

--- src/org/labkey/test/params/list/IntListDefinition.java
+++ src/org/labkey/test/params/list/IntListDefinition.java
@@ -1,10 +1,32 @@
 package org.labkey.test.params.list;
 
+import org.jetbrains.annotations.NotNull;
+import org.labkey.test.params.FieldDefinition;
+
+import java.util.List;
+
 public class IntListDefinition extends ListDefinition
 {
+    private static final String AUTO_INCREMENT_DOMAIN_KIND = "IntList";
+
     public IntListDefinition(String name, String autoIncrementKeyName)
     {
         super(name);
-        setAutoIncrementKeyName(autoIncrementKeyName);
+        setKeyName(autoIncrementKeyName);
+    }
+
+    @Override
+    public List<FieldDefinition> getFields()
+    {
+        List<FieldDefinition> fields = super.getFields();
+        fields.add(0, new FieldDefinition(getKeyName(), FieldDefinition.ColumnType.Integer).setPrimaryKey(true));
+        return fields;
+    }
+
+    @NotNull
+    @Override
+    protected String getKind()
+    {
+        return AUTO_INCREMENT_DOMAIN_KIND;
     }
 }
