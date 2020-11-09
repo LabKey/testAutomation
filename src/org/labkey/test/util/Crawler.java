c7195c0b955d370cf6a5e77e28ec65b2135a0b57
Dan Duffek <danield@labkey.com>
2020-11-09 11:54:55 -0800

    Add datafinder-begin to exclusion list. (#524)

--- src/org/labkey/test/util/Crawler.java
+++ src/org/labkey/test/util/Crawler.java
@@ -257,6 +257,7 @@ public class Crawler
                 new ControllerActionId("reagent", "begin"),
                 new ControllerActionId("su2c", "begin"),
                 new ControllerActionId("trialshare", "begin"),
+                new ControllerActionId("datafinder", "begin"),
                 new ControllerActionId("ehr_compliancedb", "requirementDetails"),
                 new ControllerActionId("onprc_billingpublic", "begin"),
                 new ControllerActionId("hdrl", "begin")
