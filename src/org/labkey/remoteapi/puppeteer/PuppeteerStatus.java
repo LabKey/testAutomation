d4d368104e19902b66b59fc7774863bc4c264b05
Nick Kerr <nicka@labkey.com>
2020-12-24 10:33:41 -0800

    Move PuppeteerHelper to testAutomation (#569)

---
+++ src/org/labkey/remoteapi/puppeteer/PuppeteerStatus.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.labkey.remoteapi.puppeteer;
+
+import org.json.JSONObject;
+import org.labkey.remoteapi.CommandResponse;
+
+public class PuppeteerStatus
+{
+    final private boolean _isAvailable;
+    final private boolean _pingSuccess;
+    final private JSONObject _service;
+
+    public PuppeteerStatus(JSONObject payload)
+    {
+        _isAvailable = payload.getBoolean("isAvailable");
+        _pingSuccess = payload.getBoolean("pingSuccess");
+        _service = new JSONObject(payload.get("service"));
+    }
+
+    public PuppeteerStatus(CommandResponse response)
+    {
+        this(new JSONObject(response.getParsedData().get("data")));
+    }
+
+    public boolean isAvailable()
+    {
+        return _isAvailable;
+    }
+
+    public boolean isPingSuccess()
+    {
+        return _pingSuccess;
+    }
+
+    public JSONObject getService()
+    {
+        return _service;
+    }
+}
