7363b4b3eeff0cab8c3ebd462cb359c9cff7866e
Cory Nathe <cnathe@labkey.com>
2021-08-13 13:20:07 -0500

    Item 8561: Selenium test cases for Unique ID fields in sample type (#836)
    * DomainFormPanel try/catch to allow for asking if the panel alert/info is displayed
    * SampleTypeDesigner updates for UniqueId alert related behaviors (has alert, get alert msg, click alert button)
    * EntityInsertPanel helpers for clicking Bulk Update button and getting EntityBulkUpdateDialog
    * Refactor helper out of SampleTypeTest and into DataRegionExportHelper for exporting XLS file and verifying row count and column headers

--- src/org/labkey/test/pages/experiment/UpdateSampleTypePage.java
+++ src/org/labkey/test/pages/experiment/UpdateSampleTypePage.java
@@ -2,6 +2,7 @@ package org.labkey.test.pages.experiment;
 
 import org.labkey.test.WebDriverWrapper;
 import org.labkey.test.WebTestHelper;
+import org.labkey.test.components.bootstrap.ModalDialog;
 import org.labkey.test.util.Maps;
 import org.openqa.selenium.WebDriver;
 
@@ -22,5 +23,11 @@ public class UpdateSampleTypePage extends CreateSampleTypePage
     {
         driver.beginAt(WebTestHelper.buildURL("experiment", containerPath, "editSampleType", Maps.of("RowId", String.valueOf(sampleTypeId))));
         return new UpdateSampleTypePage(driver.getDriver());
+    }
+
+    public ModalDialog clickSaveExpectingAlert()
+    {
+        elementCache().saveButton.click();
+        return new ModalDialog.ModalDialogFinder(getDriver()).timeout(1000).waitFor();
     }
 }
