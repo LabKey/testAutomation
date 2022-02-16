c6a1e4b26398754348c720651a19ad26ab848c7a
Chris Joosse <chrisj@labkey.com>
2022-02-16 14:08:44 -0800

    Lots of code review feedback

--- src/org/labkey/test/util/TestUser.java
+++ src/org/labkey/test/util/TestUser.java
@@ -1,6 +1,7 @@
 package org.labkey.test.util;
 
 import org.labkey.api.collections.CaseInsensitiveHashMap;
+import org.labkey.remoteapi.Connection;
 import org.labkey.remoteapi.security.CreateUserResponse;
 import org.labkey.test.Locator;
 import org.labkey.test.WebDriverWrapper;
@@ -10,6 +11,7 @@ import org.openqa.selenium.WebElement;
 import java.util.Map;
 
 import static org.labkey.test.WebDriverWrapper.WAIT_FOR_PAGE;
+import static org.labkey.test.util.TestLogger.log;
 
 public class TestUser
 {
@@ -17,80 +19,70 @@ public class TestUser
     private final String _email;
     private CreateUserResponse _createUserResponse;
     private String _password;
-    private Map<String, String> _permissions;
+    private final APIUserHelper _apiUserHelper;
+    private Connection _impersionationConnection;
 
     public TestUser(WebDriverWrapper test, String email)
     {
         _test = test;
         _email = email;
+        _apiUserHelper = new APIUserHelper(_test);
+        create();
     }
 
     /**
-     * If this instance has not been used to create a user on the server, will no-op
+     * Creates the user immediately via the API, and stores the createUserResponse to hang on to data like
+     *
      * @return
      */
-    public TestUser createIfNeeded()
+    private TestUser create()
     {
-        var apiUserHelper = new APIUserHelper(_test);
-        if (apiUserHelper.getUserId(_email) == null)    // or alternately, could check _createUserResponse for null
-        {
-            _createUserResponse = apiUserHelper.createUser(_email);
-            if (_password != null)
-            {
-                // reset the initial password here, somehow
-                // note that the _createUserResponse has a link to the password reset URL containing a hash
-                // that the login-setPassword.view action might accept...
-
-                //... or we could borrow this from LKSW's setInitialPassword
-                _test.beginAt(WebTestHelper.buildURL("security", "showRegistrationEmail", Map.of("email", _email)));
-                // Get setPassword URL from notification email.
-                WebElement resetLink = Locator.linkWithHref("setPassword.view").findElement(_test.getDriver());
-
-                _test.clickAndWait(resetLink, WAIT_FOR_PAGE);
-
-                _test.setFormElement(Locator.id("password"), _password);
-                _test.setFormElement(Locator.id("password2"), _password);
-
-                _test.clickButton("Set Password");
-            }
-            if (_permissions != null)
-            {
-                for (String key : _permissions.keySet())
-                {
-                    new ApiPermissionsHelper(_test).addMemberToRole(getEmail(), key, PermissionsHelper.MemberType.user, _permissions.get(key));
-                }
-            }
-        }
+        _createUserResponse = _apiUserHelper.createUser(_email);
         return this;
-    }
-
-    public boolean isCreated()
-    {
-        return _createUserResponse != null;
     }
 
     public void deleteUser()
     {
-        if (_createUserResponse != null)
-        {
-            new APIUserHelper(_test).deleteUser(_email);
-        }
+        _apiUserHelper.deleteUsers(false, _email);
     }
 
     public Long getUserId()
     {
-        if (_createUserResponse == null)
-            createIfNeeded();
         return (Long) _createUserResponse.getUserId();
     }
 
     public String getEmail()
     {
-        return _email;
+        return _createUserResponse.getEmail();
     }
 
+    /**
+     * Uses the UI to reset the randomly-generated password a user gets when created, by following the reset link they'll
+     * receive in mail. Also stores the provided password in the bean for later use.
+     *  Note: this can only be done once for a given user account
+     * @param password  The password
+     * @return  The current instance
+     */
     public TestUser setPassword(String password)
     {
+        if (_password == null)  // if null, this is the initial password-
+        {
+            //... borrowed from LKSW's setInitialPassword - in the future, do via API
+            _test.beginAt(WebTestHelper.buildURL("security", "showRegistrationEmail", Map.of("email", _email)));
+            // Get setPassword URL from notification email.
+            WebElement resetLink = Locator.linkWithHref("setPassword.view").findElement(_test.getDriver());
+
+            _test.clickAndWait(resetLink, WAIT_FOR_PAGE);
+
+            _test.setFormElement(Locator.id("password"), password);
+            _test.setFormElement(Locator.id("password2"), password);
+
+            _test.clickButton("Set Password");
+        }
+        else
+        {
+            log("Warning: user " +_email+ "has already selected a password.");
+        }
         _password = password;
         return this;
     }
@@ -102,9 +94,26 @@ public class TestUser
 
     public TestUser addPermission(String role, String containerContext)
     {
-        if (_permissions == null)
-            _permissions = new CaseInsensitiveHashMap();
-        _permissions.put(role, containerContext);
+        // apply permission setting right away
+        new ApiPermissionsHelper(_test).addMemberToRole(getEmail(), role, PermissionsHelper.MemberType.user, containerContext);
+
         return this;
+    }
+
+    public void impersonate() throws Exception
+    {
+        if (_impersionationConnection != null)
+            log("Already impersonating.");
+
+        log("Begin impersonating as user: " + getEmail());
+        _impersionationConnection = _test.createDefaultConnection();
+        _impersionationConnection.impersonate(getEmail());
+    }
+
+    public void stopImpersonating() throws Exception
+    {
+        log("Stop impersonating uer " + getEmail());
+        _impersionationConnection.stopImpersonate();
+        _impersionationConnection = null;
     }
 }
