d100d964f9564bb3e9a04430a2c856389a4ca1a8
Ian Sigmon <labkey-ians@users.noreply.github.com>
2022-05-17 10:50:11 -0700

    Add check for requested export tabs that aren't found  (#1109)
    * Followup feedback to throw an error if there are unfound tabs specified on export

--- src/org/labkey/test/components/ui/grids/TabSelectionExportDialog.java
+++ src/org/labkey/test/components/ui/grids/TabSelectionExportDialog.java
@@ -4,11 +4,14 @@ import org.jetbrains.annotations.Nullable;
 import org.labkey.test.Locator;
 import org.labkey.test.components.bootstrap.ModalDialog;
 import org.labkey.test.components.html.Checkbox;
+import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TabSelectionExportDialog extends ModalDialog
 {
@@ -28,13 +31,22 @@ public class TabSelectionExportDialog extends ModalDialog
         //If tabs is null use default tab selections
         if (tabs != null)
         {
+            Set<String> missingTabs = new HashSet<>(tabs);
             for (WebElement checkbox : elementCache().checkboxes)
             {
                 String text = checkbox.getText();
                 if (!tabs.contains(text))
+                {
                     elementCache().getCheckBox(checkbox).check();
+                    missingTabs.remove(text);
+                }
                 else
                     elementCache().getCheckBox(checkbox).uncheck();
+            }
+
+            if (!missingTabs.isEmpty())
+            {
+                throw new NotFoundException("Tab(s) not found: " + missingTabs);
             }
         }
         return getWrapper().doAndWaitForDownload(elementCache().exportButton::click);
