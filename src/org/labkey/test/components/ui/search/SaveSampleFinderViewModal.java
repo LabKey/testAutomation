0c4ae7745ffc1a83dabee2c651991dba2ab135c1
XingY <xyang@labkey.com>
2022-07-14 21:22:49 -0700

    test coverage for saved sample finder views

---
+++ src/org/labkey/test/components/ui/search/SaveSampleFinderViewModal.java
@@ -0,0 +1,87 @@
+package org.labkey.test.components.ui.search;
+
+import org.labkey.test.Locator;
+import org.labkey.test.components.bootstrap.ModalDialog;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SaveSampleFinderViewModal extends ModalDialog
+{
+    public SaveSampleFinderViewModal(WebDriver driver)
+    {
+        this("Save Custom Search", driver);
+    }
+
+    protected SaveSampleFinderViewModal(String title, WebDriver driver)
+    {
+        super(new ModalDialog.ModalDialogFinder(driver).withTitle(title));
+    }
+
+    public String getName()
+    {
+        return Locator.tag("input").findElement(getComponentElement()).getAttribute("value");
+    }
+
+    public SaveSampleFinderViewModal setName(String name)
+    {
+        WebElement input = elementCache().nameInput;
+        input.clear();
+        if (name != null)
+            input.sendKeys(name);
+        return this;
+    }
+
+    public void clickSave()
+    {
+        clickSave(false);
+    }
+
+    public String clickSave(boolean hasError)
+    {
+        elementCache().saveBtn.click();
+        if (!hasError)
+        {
+            waitForClose();
+            return null;
+        }
+        return getErrorMsg();
+    }
+
+    public void clickCancel()
+    {
+        elementCache().cancelButton.click();
+        waitForClose();
+    }
+
+    public String getErrorMsg()
+    {
+        return elementCache().errorMsg.getText();
+    }
+
+    @Override
+    protected ElementCache newElementCache()
+    {
+        return new ElementCache();
+    }
+
+    @Override
+    protected ElementCache elementCache()
+    {
+        return (ElementCache) super.elementCache();
+    }
+
+    protected class ElementCache extends ModalDialog.ElementCache
+    {
+        WebElement errorMsg = Locator.tagWithClassContaining("div", "alert-danger").findWhenNeeded(getComponentElement());
+
+        WebElement nameInput = Locator.tag("input").findWhenNeeded(getComponentElement());
+
+        WebElement saveBtn = Locator.tagWithClassContaining("button", "btn-success")
+                .withText("Save")
+                .findWhenNeeded(getComponentElement());
+        WebElement cancelButton = Locator.tagWithClassContaining("button", "btn-default")
+                .withText("Cancel")
+                .findWhenNeeded(getComponentElement());
+    }
+
+}
