c3d02cf64568d08a97e58893442e5153138bd89b
labkey-tchad <tchad@labkey.com>
2022-07-18 11:47:13 -0700

    More distinct page class name

--- src/org/labkey/test/pages/flow/reports/FlowReportsBeginPage.java
+++ src/org/labkey/test/pages/flow/reports/FlowReportsBeginPage.java
@@ -21,22 +21,22 @@ import org.labkey.test.components.flow.FlowReportsWebpart;
 import org.labkey.test.pages.LabKeyPage;
 import org.openqa.selenium.WebDriver;
 
-public class BeginPage extends LabKeyPage<BeginPage.ElementCache>
+public class FlowReportsBeginPage extends LabKeyPage<FlowReportsBeginPage.ElementCache>
 {
-    public BeginPage(WebDriver driver)
+    public FlowReportsBeginPage(WebDriver driver)
     {
         super(driver);
     }
 
-    public static BeginPage beginAt(WebDriverWrapper driver)
+    public static FlowReportsBeginPage beginAt(WebDriverWrapper driver)
     {
         return beginAt(driver, driver.getCurrentContainerPath());
     }
 
-    public static BeginPage beginAt(WebDriverWrapper driver, String containerPath)
+    public static FlowReportsBeginPage beginAt(WebDriverWrapper driver, String containerPath)
     {
         driver.beginAt(WebTestHelper.buildURL("flow-reports", containerPath, "begin"));
-        return new BeginPage(driver.getDriver());
+        return new FlowReportsBeginPage(driver.getDriver());
     }
 
     public FlowReportsWebpart reportsPanel()
