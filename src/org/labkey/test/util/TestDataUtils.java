e8d91dd9eff2ba9e63a75dabbcb92c66b387be38
labkey-tchad <tchad@labkey.com>
2022-09-15 13:26:59 -0700

    Add utility method for merging collections into a single list

--- src/org/labkey/test/util/TestDataUtils.java
+++ src/org/labkey/test/util/TestDataUtils.java
@@ -6,16 +6,40 @@ import org.labkey.serverapi.reader.TabLoader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class TestDataUtils
 {
+    private TestDataUtils()
+    {
+        // Utility class. Do not instantiate.
+    }
+
     public static List<Map<String, Object>> rowMapsFromTsv(String tsvString) throws IOException
     {
         try (InputStream dataStream = IOUtils.toInputStream(tsvString, StandardCharsets.UTF_8))
         {
             return new TabLoader.TsvFactory().createLoader(dataStream, true).load();
         }
+    }
+
+    /**
+     * Create a new list containing the contents of all the provided lists.
+     * @param lists lists to be combined
+     * @return combined list
+     * @param <T> type of list contents
+     */
+    @SafeVarargs
+    public static <T> List<T> merge(Collection<T>... lists)
+    {
+        List<T> combined = new ArrayList<>();
+        for (Collection<T> list : lists)
+        {
+            combined.addAll(list);
+        }
+        return combined;
     }
 }
