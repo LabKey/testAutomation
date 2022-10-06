29ed211dcbfb60fc110655e920d68904b458b368
Chris Joosse <chrisj@labkey.com>
2022-10-06 13:16:03 -0700

    refactor changes for group management tests and components in apps (#1251)
    * refactor changes for group management tests and components in apps
    
    Co-authored-by: labkey-tchad <tchad@labkey.com>

---
+++ src/org/labkey/test/components/ui/permissions/GroupDetailsPanel.java
@@ -0,0 +1,69 @@
+package org.labkey.test.components.ui.permissions;
+
+import org.labkey.test.BootstrapLocators;
+import org.labkey.test.Locator;
+import org.labkey.test.components.Component;
+import org.labkey.test.components.WebDriverComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GroupDetailsPanel extends WebDriverComponent<GroupDetailsPanel.ElementCache>
+{
+    protected static final Locator LOC = BootstrapLocators.panel("Group Details");
+    private final WebElement _el;
+    private final WebDriver _driver;
+
+    protected GroupDetailsPanel(WebElement element, WebDriver driver)
+    {
+        _el = element;
+        _driver = driver;
+    }
+
+    @Override
+    public WebElement getComponentElement()
+    {
+        return _el;
+    }
+
+    @Override
+    public WebDriver getDriver()
+    {
+        return _driver;
+    }
+
+    public String getGroupName()
+    {
+        return elementCache().title.getText();
+    }
+
+    public String getUserCount()
+    {
+        return elementCache().detailValueEl("User Count").getText();
+    }
+
+    public String getGroupCount()
+    {
+        return elementCache().detailValueEl("Group Count").getText();
+    }
+
+
+    @Override
+    protected ElementCache newElementCache()
+    {
+        return new ElementCache();
+    }
+
+
+    protected class ElementCache extends Component<?>.ElementCache
+    {
+        final WebElement title = Locator.tagWithClass("p", "principal-title-primary").findWhenNeeded(this);
+
+        WebElement detailValueEl(String label)
+        {
+            return Locator.tagWithClass("div", "principal-detail-label").startsWith(label)
+                    .followingSibling("div").findElement(this);
+        }
+    }
+
+
+}
