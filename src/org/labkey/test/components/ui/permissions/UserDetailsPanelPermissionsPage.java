4e0c5fcafa5bddb4a5eea357d1cd620f29adcdd2
labkey-susanh <susanh@labkey.com>
2022-10-30 11:58:02 -0700

    new alpha package versions and test updates

--- src/org/labkey/test/components/ui/permissions/UserDetailsPanelPermissionsPage.java
+++ src/org/labkey/test/components/ui/permissions/UserDetailsPanelPermissionsPage.java
@@ -21,7 +21,7 @@ public class UserDetailsPanelPermissionsPage extends UserDetailsPanel
 
     public List<String> getEffectiveRoles()
     {
-        var listContainer=  Locator.tagWithClass("div", "principal-detail-label").withText("Effective Roles:")
+        var listContainer=  Locator.tagWithClass("div", "principal-detail-label").withText("Effective Roles")
                 .followingSibling("ul").waitForElement(this, 2000);
         return Locator.tag("li")
                 .findElements(listContainer)
