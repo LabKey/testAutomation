21a4a05edf49a3fcd885aeb6ca40caff7f42ca27
bbimber <bbimber@gmail.com>
2022-12-28 11:52:45 -0800

    Correct syntax

--- modules/simpletest/resources/schemas/dbscripts/sqlserver/vehicle-22.001-22.002.sql
+++ modules/simpletest/resources/schemas/dbscripts/sqlserver/vehicle-22.001-22.002.sql
@@ -1,4 +1,4 @@
 IF COL_LENGTH('vehicle.Vehicles', 'TriggerScriptContainer') IS NULL
 BEGIN
-ALTER TABLE vehicle.Vehicles ADD COLUMN IF NOT EXISTS TriggerScriptContainer ENTITYID;
+ALTER TABLE vehicle.Vehicles ADD TriggerScriptContainer ENTITYID;
 END;
