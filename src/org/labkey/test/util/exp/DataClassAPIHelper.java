9f8ce5552ebf869d87632c29ff3c64bcb31d33d4
labkey-danield <danield@labkey.com>
2023-02-01 19:13:02 -0800

    Add getters for average and calls to CPUTimer. Added DataClassAPIHelper.getRowIdsForDataEntity to get row ids for sources. Added Pager.getDropDownMenu to get the dropdown menu so the menu item click can be isolated inside the timers. Created a base class for the UI perf test (renamed an existing UI perf test file) Broke out the samples perf test to their own class. Added source perf tests.

--- src/org/labkey/test/util/exp/DataClassAPIHelper.java
+++ src/org/labkey/test/util/exp/DataClassAPIHelper.java
@@ -1,6 +1,11 @@
 package org.labkey.test.util.exp;
 
+import org.junit.Assert;
 import org.labkey.remoteapi.CommandException;
+import org.labkey.remoteapi.Connection;
+import org.labkey.remoteapi.query.Filter;
+import org.labkey.remoteapi.query.SelectRowsCommand;
+import org.labkey.remoteapi.query.SelectRowsResponse;
 import org.labkey.test.WebTestHelper;
 import org.labkey.test.params.FieldDefinition;
 import org.labkey.test.params.experiment.DataClassDefinition;
@@ -9,7 +14,11 @@ import org.labkey.test.util.TestDataGenerator;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class DataClassAPIHelper
 {
@@ -49,6 +58,33 @@ public class DataClassAPIHelper
                 new FieldDefinition("sampleDate", FieldDefinition.ColumnType.DateAndTime),
                 new FieldDefinition("boolColumn", FieldDefinition.ColumnType.Boolean),
                 new FieldDefinition("attachmentColumn", FieldDefinition.ColumnType.Attachment));
+    }
+
+    public static Map<String, Integer> getRowIdsForDataEntity(String containerPath, String dataTypeName, List<String> entityNames) throws IOException, CommandException
+    {
+
+        Connection connection = WebTestHelper.getRemoteApiConnection();
+        SelectRowsCommand cmd = new SelectRowsCommand("exp.data", dataTypeName);
+        cmd.setColumns(Arrays.asList("RowId", "Name"));
+        cmd.addFilter("Name", String.join(";", entityNames), Filter.Operator.IN);
+
+        SelectRowsResponse response = cmd.execute(connection, containerPath);
+
+        Map<String, Integer> rowIds = new HashMap<>();
+
+        for(Map<String, Object> row : response.getRows())
+        {
+            Object name = row.get("Name");
+            Object value = row.get("RowId");
+            rowIds.put(name.toString(), Integer.parseInt(value.toString()));
+        }
+
+        // Check that the names returned from the query match the names sent in.
+        Set<String> names = new HashSet<>(entityNames);
+        Assert.assertTrue("The names of the data entities returned from the query do not match the names sent in.",
+                names.containsAll(rowIds.keySet()));
+
+        return rowIds;
     }
 
 }
