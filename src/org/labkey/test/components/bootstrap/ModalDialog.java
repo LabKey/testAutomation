b71d94de99b1ae4040d84a828f9a08988ad957fa
labkey-tchad <tchad@labkey.com>
2023-03-22 13:19:50 -0700

    LabKeyAlert might not go stale.

--- src/org/labkey/test/components/bootstrap/ModalDialog.java
+++ src/org/labkey/test/components/bootstrap/ModalDialog.java
@@ -128,10 +128,8 @@ public class ModalDialog extends WebDriverComponent<ModalDialog.ElementCache>
     {
         if (waitSeconds > 0) // Zero to not expect dialog to close
         {
-            new WebDriverWait(getDriver(), Duration.ofSeconds(waitSeconds)).until(ExpectedConditions.and(
-                    ExpectedConditions.invisibilityOf(getComponentElement()),
-                    ExpectedConditions.invisibilityOfElementLocated(Locator.byClass("modal")),
-                    ExpectedConditions.invisibilityOfElementLocated(Locator.byClass("modal-backdrop"))));
+            new WebDriverWait(getDriver(), Duration.ofSeconds(waitSeconds))
+                    .until(ExpectedConditions.stalenessOf(getComponentElement()));
         }
     }
 
