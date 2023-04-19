9c8a06f2bd9e326ecc971f1d37ee586127044496
labkey-sweta <swetaj@labkey.com>
2023-04-18 18:07:48 -0700

    Automation test for site users

---
+++ src/org/labkey/test/pages/user/ClonePermissionsPage.java
@@ -0,0 +1,64 @@
+package org.labkey.test.pages.user;
+
+import org.labkey.test.Locator;
+import org.labkey.test.WebDriverWrapper;
+import org.labkey.test.WebTestHelper;
+import org.labkey.test.components.html.Input;
+import org.labkey.test.pages.LabKeyPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.Map;
+
+public class ClonePermissionsPage extends LabKeyPage<ClonePermissionsPage.ElementCache>
+{
+    public ClonePermissionsPage(WebDriver driver)
+    {
+        super(driver);
+    }
+
+    public static ClonePermissionsPage beginAt(WebDriverWrapper webDriverWrapper, Integer userId)
+    {
+        Map<String, String> params = Map.of("userId", userId.toString(), "schemaName", "core", "query.queryName", "Users", "targetUser", userId.toString());
+        webDriverWrapper.beginAt(WebTestHelper.buildURL("security", "clonePermissions", params));
+        return new ClonePermissionsPage(webDriverWrapper.getDriver());
+    }
+
+    public String getWarningMessage()
+    {
+        return elementCache().warningMsg.getText();
+    }
+
+    public ClonePermissionsPage setCloneUser(String value)
+    {
+        elementCache().cloneUser.set(value);
+        return this;
+    }
+
+    public ShowUsersPage clonePermission()
+    {
+        clickAndWait(elementCache().clonePermissionBtn);
+        return new ShowUsersPage(getDriver());
+    }
+
+    public ShowUsersPage cancelPermission()
+    {
+        clickAndWait(elementCache().cancelBtn);
+        return new ShowUsersPage(getDriver());
+    }
+
+    @Override
+    protected ClonePermissionsPage.ElementCache newElementCache()
+    {
+        return new ClonePermissionsPage.ElementCache();
+    }
+
+    protected class ElementCache extends LabKeyPage<?>.ElementCache
+    {
+        WebElement warningMsg = Locator.xpath("//table/tbody/tr[3]/td").findWhenNeeded(this);
+        Input cloneUser = new Input(Locator.name("cloneUser").findWhenNeeded(this), getDriver());
+        WebElement clonePermissionBtn = Locator.lkButton("Clone Permissions").findWhenNeeded(this);
+        WebElement cancelBtn = Locator.lkButton("Cancel").findElement(this);
+
+    }
+}
---
+++ src/org/labkey/test/tests/UserClonePermissionTest.java
@@ -0,0 +1,92 @@
+package org.labkey.test.tests;
+
+import org.jetbrains.annotations.Nullable;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.labkey.test.BaseWebDriverTest;
+import org.labkey.test.categories.Daily;
+import org.labkey.test.pages.user.ClonePermissionsPage;
+import org.labkey.test.util.ApiPermissionsHelper;
+import org.labkey.test.util.PermissionsHelper;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Category({Daily.class})
+@BaseWebDriverTest.ClassTimeout(minutes = 6)
+public class UserClonePermissionTest extends BaseWebDriverTest
+{
+    private static final String CLONED_SOURCE_SITE_USER = "sourcesiteuser@clonepermission.test";
+    private static final String CLONED_SOURCE_APP_USER = "sourceappuser@clonepermission.test";
+    private static final String CLONED_TARGET_SITE_USER = "targetsiteuser@clonepermission.test";
+    private static final String CLONED_TARGET_APP_USER = "targetappuser@clonepermission.test";
+    private static final String TEST_PASSWORD = "123456";
+    private static final String CLONE_GROUP = "Test clone group";
+
+    ApiPermissionsHelper _permissionsHelper = new ApiPermissionsHelper(this);
+
+    @Override
+    protected @Nullable String getProjectName()
+    {
+        return getClass().getSimpleName() + " Project";
+    }
+
+    @Override
+    public List<String> getAssociatedModules()
+    {
+        return Arrays.asList("core");
+    }
+
+    @BeforeClass
+    public static void setup()
+    {
+        UserClonePermissionTest initTest = (UserClonePermissionTest) getCurrentTest();
+        initTest.doSetup();
+    }
+
+    private void doSetup()
+    {
+        _containerHelper.createProject(getProjectName());
+
+        _userHelper.createUser(CLONED_SOURCE_SITE_USER);
+        _permissionsHelper.addUserToSiteGroup(CLONED_SOURCE_SITE_USER, "Site Administrators");
+
+        _userHelper.createUser(CLONED_SOURCE_APP_USER);
+        _permissionsHelper.addUserAsAppAdmin(CLONED_SOURCE_APP_USER);
+
+        goToProjectHome();
+        _permissionsHelper.createPermissionsGroup(CLONE_GROUP);
+        _permissionsHelper.setPermissions(CLONE_GROUP, "Reader");
+        _permissionsHelper.setPermissions(CLONE_GROUP, "Electronic Signer");
+        _permissionsHelper.addUserToProjGroup(CLONED_SOURCE_SITE_USER, getProjectName(), CLONE_GROUP);
+        _permissionsHelper.addMemberToRole(CLONED_SOURCE_SITE_USER, "Author", PermissionsHelper.MemberType.user);
+
+        _permissionsHelper.addUserToSiteGroup(CLONED_SOURCE_SITE_USER, "Developers");
+
+        _userHelper.createUser(CLONED_TARGET_SITE_USER);
+        _permissionsHelper.addUserToSiteGroup(CLONED_TARGET_SITE_USER, "Site Administrators");
+
+        _userHelper.createUser(CLONED_TARGET_APP_USER);
+    }
+
+    @Test
+    public void testSiteUserClonePermission()
+    {
+        impersonate(CLONED_TARGET_SITE_USER);
+        ClonePermissionsPage clonePermissionsPage = goToMyAccount().clickClonePermission();
+        clonePermissionsPage.setCloneUser(CLONED_SOURCE_SITE_USER);
+        clonePermissionsPage.clonePermission();
+        stopImpersonating();
+
+        _permissionsHelper.assertPermissionSetting(CLONED_SOURCE_SITE_USER, "Developers");
+        _permissionsHelper.assertPermissionSetting(CLONED_SOURCE_SITE_USER, "Reader");
+    }
+
+    @Override
+    protected void doCleanup(boolean afterTest)
+    {
+        super.doCleanup(afterTest);
+        _userHelper.deleteUsers(afterTest, CLONED_SOURCE_SITE_USER, CLONED_SOURCE_APP_USER, CLONED_TARGET_APP_USER, CLONED_TARGET_SITE_USER);
+    }
+}
