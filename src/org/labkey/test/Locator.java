07463704802fa283ca88c3b7720bd59d8b324d16
Dan Duffek <danield@labkey.com>
2023-06-29 14:28:29 -0700

    Fixing Random Failures in 23.3 Windows Test (#1545)
    Co-authored-by: Trey Chadick <tchad@labkey.com>

--- src/org/labkey/test/Locator.java
+++ src/org/labkey/test/Locator.java
@@ -36,6 +36,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WrapsDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Quotes;
 
@@ -434,6 +435,22 @@ public abstract class Locator extends By
     {
         WebElement element = findElementOrNull(context);
         return element != null && element.isDisplayed();
+    }
+
+    /**
+     * Will check if any elements that match this locator are visible.
+     *
+     * @param context Search context.
+     * @return True if there are any elements visible, false otherwise.
+     */
+    public boolean areAnyVisible(SearchContext context)
+    {
+        List<WebElement> elements = findElements(context);
+
+        // The method invisibilityOfAllElements returns true/false, but visibilityOfAllElements returns a list or null.
+        // Dealing with a true/false response, and taking the not of it, is easier than having to deal with a list that
+        // may or may not be null.
+        return !ExpectedConditions.invisibilityOfAllElements(elements).apply(getWebDriver(context));
     }
 
     protected final List<WebElement> decorateWebElements(List<WebElement> elements)
