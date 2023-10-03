893c7157f0208b2e8ce36eb8f70870ae705a7e88
labkey-tchad <tchad@labkey.com>
2023-10-02 17:20:16 -0700

    Fix subcomponent scoping

--- src/org/labkey/test/components/ui/domainproperties/samples/SampleTypeDesigner.java
+++ src/org/labkey/test/components/ui/domainproperties/samples/SampleTypeDesigner.java
@@ -178,14 +178,14 @@ public abstract class SampleTypeDesigner<T extends SampleTypeDesigner<T>> extend
 
     protected class ElementCache extends EntityTypeDesigner<T>.ElementCache
     {
-        protected final WebElement uniqueIdAlert = Locator.tagWithClassContaining("div","uniqueid-alert").refindWhenNeeded(this);
+        protected final WebElement uniqueIdAlert = Locator.tagWithClassContaining("div","uniqueid-alert").refindWhenNeeded(propertiesPanel);
         protected final WebElement uniqueIdAlertAddButton = Locator.tagWithClassContaining("div","uniqueid-alert")
-                .append(Locator.tag("button")).refindWhenNeeded(this);
-        protected final WebElement uniqueIdMsg = Locator.tagWithClass("div","uniqueid-msg").refindWhenNeeded(this);
+                .append(Locator.tag("button")).refindWhenNeeded(propertiesPanel);
+        protected final WebElement uniqueIdMsg = Locator.tagWithClass("div","uniqueid-msg").refindWhenNeeded(propertiesPanel);
         protected final WebElement uniqueIdMsgCheckIcon = Locator.tagWithClass("div","uniqueid-msg")
-                .append(Locator.tagWithClassContaining("i", "domain-panel-status-icon-green")).refindWhenNeeded(this);
+                .append(Locator.tagWithClassContaining("i", "domain-panel-status-icon-green")).refindWhenNeeded(propertiesPanel);
 
-        protected final WebElement addAliasButton = Locator.tagWithClass("i","container--addition-icon").findWhenNeeded(this);
+        protected final WebElement addAliasButton = Locator.tagWithClass("i","container--addition-icon").findWhenNeeded(propertiesPanel);
 
         public List<Input> parentAliases()
         {
