436fb4df9091e1defca47540aa3c6db5a222163a
labkey-tchad <tchad@labkey.com>
2023-10-12 15:57:43 -0700

    Add flag to dump browser console logs

--- src/org/labkey/test/WebDriverWrapper.java
+++ src/org/labkey/test/WebDriverWrapper.java
@@ -1897,32 +1897,35 @@ public abstract class WebDriverWrapper implements WrapsDriver
                 .withMessage("waiting for document to be ready")
                 .until(wd -> Objects.equals(executeScript("return document.readyState;"), "complete"));
         Locators.documentRoot.waitForElement(getDriver(), (int) timer.timeRemaining().toMillis());
-        executeScript("""
-                if (console.everything === undefined)
-                {
-                    console.everything = [];
+        if (TestProperties.isDumpBrowserConsole())
+        {
+            executeScript("""
+                    if (console.everything === undefined)
+                    {
+                        console.everything = [];
 
-                    console.defaultLog = console.log.bind(console);
-                    console.log = function(){
-                        console.everything.push({"type":"log", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
-                        console.defaultLog.apply(console, arguments);
-                    }
-                    console.defaultError = console.error.bind(console);
-                    console.error = function(){
-                        console.everything.push({"type":"error", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
-                        console.defaultError.apply(console, arguments);
-                    }
-                    console.defaultWarn = console.warn.bind(console);
-                    console.warn = function(){
-                        console.everything.push({"type":"warn", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
-                        console.defaultWarn.apply(console, arguments);
-                    }
-                    console.defaultDebug = console.debug.bind(console);
-                    console.debug = function(){
-                        console.everything.push({"type":"debug", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
-                        console.defaultDebug.apply(console, arguments);
-                    }
-                }""");
+                        console.defaultLog = console.log.bind(console);
+                        console.log = function(){
+                            console.everything.push({"type":"log", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+                            console.defaultLog.apply(console, arguments);
+                        }
+                        console.defaultError = console.error.bind(console);
+                        console.error = function(){
+                            console.everything.push({"type":"error", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+                            console.defaultError.apply(console, arguments);
+                        }
+                        console.defaultWarn = console.warn.bind(console);
+                        console.warn = function(){
+                            console.everything.push({"type":"warn", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+                            console.defaultWarn.apply(console, arguments);
+                        }
+                        console.defaultDebug = console.debug.bind(console);
+                        console.debug = function(){
+                            console.everything.push({"type":"debug", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+                            console.defaultDebug.apply(console, arguments);
+                        }
+                    }""");
+        }
         waitForOnReady("jQuery");
         waitForOnReady("Ext");
         waitForOnReady("Ext4");
