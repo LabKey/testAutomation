baa3266f36b74f0cce95b44a9e3482c365172b88
Chris Joosse <chrisj@labkey.com>
2023-11-10 15:06:48 -0800

    Await insert panel readiness before attempting to select grid (#1718)

--- src/org/labkey/test/components/ui/entities/EntityInsertPanel.java
+++ src/org/labkey/test/components/ui/entities/EntityInsertPanel.java
@@ -187,11 +187,6 @@ public class EntityInsertPanel extends WebDriverComponent<EntityInsertPanel.Elem
         return panel.fileUploadPanel();
     }
 
-    private Optional<EditableGrid> optionalGrid()
-    {
-        return new EditableGrid.EditableGridFinder(_driver).findOptional(this);
-    }
-
     public EntityInsertPanel setMergeData(boolean allowMerge)
     {
         var panel = showFileUpload();
@@ -213,7 +208,7 @@ public class EntityInsertPanel extends WebDriverComponent<EntityInsertPanel.Elem
 
     protected FileUploadPanel fileUploadPanel()
     {
-        return new FileUploadPanel.FileUploadPanelFinder(_driver).timeout(WAIT_FOR_JAVASCRIPT).waitFor(this);
+        return elementCache().fileUploadPanel();
     }
 
     private Optional<FileUploadPanel> optionalFileUploadPanel()
@@ -233,7 +228,7 @@ public class EntityInsertPanel extends WebDriverComponent<EntityInsertPanel.Elem
 
     public boolean isGridVisible()
     {
-        var optionalGrid = optionalGrid();
+        var optionalGrid = elementCache().optionalGrid();
         return optionalGrid.isPresent() && optionalGrid.get().isDisplayed();
     }
 
@@ -272,7 +267,7 @@ public class EntityInsertPanel extends WebDriverComponent<EntityInsertPanel.Elem
 
     public boolean hasTabs()
     {
-        return Locator.tagWithClassContaining("ul", "list-group").existsIn(this);
+        return elementCache().hasTabs();
     }
 
     public boolean isFileUploadVisible()
@@ -327,6 +322,11 @@ public class EntityInsertPanel extends WebDriverComponent<EntityInsertPanel.Elem
 
     public EntityInsertPanel showGrid()
     {
+        /* either this is a grid-only insert panel, or there will be a mode-select list-item to
+            allow the user to select the grid. Await one or the other to be present   */
+        WebDriverWrapper.waitFor(()-> isGridVisible() || hasTabs(),
+                "Neither the grid nor its selector appeared within the ready timeout", _readyTimeout);
+
         if (!isGridVisible())
         {
             modeSelectListItem("from Grid")
@@ -429,8 +429,28 @@ public class EntityInsertPanel extends WebDriverComponent<EntityInsertPanel.Elem
 
         EditableGrid grid = new EditableGrid.EditableGridFinder(_driver).timeout(WAIT_FOR_JAVASCRIPT).findWhenNeeded();
 
+        private Optional<EditableGrid> optionalGrid()
+        {
+            return new EditableGrid.EditableGridFinder(_driver).findOptional(this);
+        }
+
+        private Optional<FileUploadPanel> optionalFileUploadPanel()
+        {
+            return new FileUploadPanel.FileUploadPanelFinder(getDriver()).findOptional();
+        }
+
+        protected FileUploadPanel fileUploadPanel()
+        {
+            return new FileUploadPanel.FileUploadPanelFinder(_driver).timeout(WAIT_FOR_JAVASCRIPT).waitFor(this);
+        }
+
         WebElement formatString = Locator.tagWithClass("div","file-form-formats")
                 .refindWhenNeeded(this).withTimeout(WAIT_FOR_JAVASCRIPT);
+
+        public boolean hasTabs()
+        {
+            return Locator.tagWithClassContaining("ul", "list-group").existsIn(elementCache());
+        }
     }
 
     public static class EntityInsertPanelFinder extends WebDriverComponent.WebDriverComponentFinder<EntityInsertPanel, EntityInsertPanelFinder>
