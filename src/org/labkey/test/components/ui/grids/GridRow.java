935739c5feec10c492618e5a0b160ab5d93e2acb
Dan Duffek <danield@labkey.com>
2023-12-14 11:53:57 -0800

    Fixing some annoying bugs. (#1748)

--- src/org/labkey/test/components/ui/grids/GridRow.java
+++ src/org/labkey/test/components/ui/grids/GridRow.java
@@ -60,6 +60,10 @@ public class GridRow extends WebDriverComponent<GridRow.ElementCache>
     {
         assertTrue("The row does not have a select box", hasSelectColumn());
         getWrapper().shortWait().until(ExpectedConditions.elementToBeClickable(elementCache().selectCheckbox.getComponentElement()));
+
+        // Make sure the grid header is not hiding the checkbox.
+        getWrapper().scrollIntoView(getComponentElement());
+
         if (elementCache().selectCheckbox.get() != checked)
         {
             _grid.doAndWaitForUpdate(()-> elementCache().selectCheckbox.set(checked));
