02fbe3954a105d0c28b1e306a42beee7f8a1357c
labkey-danield <danield@labkey.com>
2024-02-16 11:56:10 -0800

    Change number of expected grid filters. Change check that a grid filter has been removed.

--- src/org/labkey/test/components/ui/FilterStatusValue.java
+++ src/org/labkey/test/components/ui/FilterStatusValue.java
@@ -4,9 +4,9 @@ import org.labkey.test.Locator;
 import org.labkey.test.WebDriverWrapper;
 import org.labkey.test.components.Component;
 import org.labkey.test.components.WebDriverComponent;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class FilterStatusValue extends WebDriverComponent<FilterStatusValue.ElementCache>
 {
@@ -52,16 +52,23 @@ public class FilterStatusValue extends WebDriverComponent<FilterStatusValue.Elem
         getWrapper().mouseOver(getComponentElement());
         getWrapper().mouseOver(elementCache().icon);
         WebDriverWrapper.waitFor(()-> isActive() && isClose(),
-                "the filter status item with text ["+getText()+"] did not become active", 500);
+                "The filter status item with text ["+getText()+"] did not become active.", 500);
         elementCache().icon.click();
 
-        // if the item you're dismissing is not the rightmost, it won't become stale; instead, its text will
+        // If the item you're dismissing is not the rightmost, it won't become stale; instead, its text will
         // be swapped out with the one to its right.  So, we check to see that either the text has changed or
         // the item became stale.
-        WebDriverWrapper.waitFor(()->  {
-                return ExpectedConditions.stalenessOf(getComponentElement()).apply(getDriver())
-                        || !getText().equals(originalText);
-        }, "the value item ["+originalText+"] did not disappear", 1000);
+        WebDriverWrapper.waitFor(()-> {
+                    try
+                    {
+                        return !getText().equals(originalText);
+                    }
+                    catch (StaleElementReferenceException s)
+                    {
+                        return true;
+                    }
+                }
+                , "The value item ["+originalText+"] did not disappear.", 1000);
     }
 
     /**
