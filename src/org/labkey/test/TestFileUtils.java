20e010aa87980acb0f19dee629137a45e14ca930
Adam Rauch <adam@labkey.com>
2024-10-27 11:24:27 -0700

    Adjust for PDFBox 3.0.x change

--- src/org/labkey/test/TestFileUtils.java
+++ src/org/labkey/test/TestFileUtils.java
@@ -24,6 +24,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.poi.xssf.streaming.SXSSFRow;
@@ -51,7 +52,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -480,7 +480,7 @@ public abstract class TestFileUtils
 
     public static String readPdfText(File pdf, String password)
     {
-        try (PDDocument document = PDDocument.load(pdf, password))
+        try (PDDocument document = Loader.loadPDF(pdf, password))
         {
             if (document.isEncrypted())
             {
