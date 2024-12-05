aa1f46df054e6efbccb8abcdfeb04fa51d514ed5
labkey-tchad <tchad@labkey.com>
2024-12-05 14:49:23 -0800

    Don't encode parameters passed to WebTestHelper.buildUrl

--- src/org/labkey/test/util/Crawler.java
+++ src/org/labkey/test/util/Crawler.java
@@ -1430,9 +1430,12 @@ public class Crawler
         {
             if (!sb.isEmpty())
                 sb.append('&');
-            sb.append(URLEncoder.encode(e.getKey(), StandardCharsets.UTF_8));
-            sb.append('=');
-            sb.append(null==e.getValue()?"":URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8));
+            sb.append(EscapeUtil.encode(e.getKey()));
+            if (e.getValue() != null)
+            {
+                sb.append('=');
+                sb.append(EscapeUtil.encode(e.getValue()));
+            }
         }
         );
         return sb.toString();
