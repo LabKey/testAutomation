7e3f506a9659df8fb5eaaa84200e4543f2cb1774
labkey-danield <danield@labkey.com>
2025-01-23 17:30:28 -0800

    Remove disabled annotation, leave category blank (CR feedback)

--- src/org/labkey/test/tests/ServerUpgradeTest.java
+++ src/org/labkey/test/tests/ServerUpgradeTest.java
@@ -6,14 +6,13 @@ import org.junit.experimental.categories.Category;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.Locator;
 import org.labkey.test.WebTestHelper;
-import org.labkey.test.categories.Disabled;
 import org.openqa.selenium.WebElement;
 
 import java.util.Collections;
 import java.util.List;
 
 // Don't run as a git test, explicitly add this test to a suite file.
-@Category({Disabled.class})
+@Category({})
 public class ServerUpgradeTest extends BaseWebDriverTest
 {
 
