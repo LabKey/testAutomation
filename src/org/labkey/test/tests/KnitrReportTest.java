350e84e3753c56c51e0e00f20c19783092190dcb
Nick Kerr <nickk@labkey.com>
2025-02-25 14:59:23 -0800

    Assert and checkExpectedErrors(1)
    Co-authored-by: Trey Chadick <tchad@labkey.com>

--- src/org/labkey/test/tests/KnitrReportTest.java
+++ src/org/labkey/test/tests/KnitrReportTest.java
@@ -199,11 +199,11 @@ public class KnitrReportTest extends AbstractKnitrReportTest
 
         createKnitrReport(rmdDependenciesReport, RReportHelper.ReportOption.knitrMarkdown);
 
-        int errorCountBefore = getServerErrorCount();
         _rReportHelper.clickReportTab();
         waitForElement(Locator.id("mtcars_table"));
         assertElementNotPresent(Locator.id("mtcars_table_wrapper")); // Created by jQuery
-        checkExpectedErrors(errorCountBefore + 1); // JavaScript error: "$(...).dataTable is not a function"
+        Assertions.assertThat(getServerErrors()).as("Server errors").contains("$(...).dataTable is not a function");
+        checkExpectedErrors(1); // JavaScript error: "$(...).dataTable is not a function"
 
         // now set the dependencies
         _rReportHelper.clickSourceTab();
