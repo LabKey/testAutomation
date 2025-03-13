944855a8a8593a3389f624f5358ffc8dff10c237
Sweta Jewargikar <swetaJ@labkey.com>
2025-03-13 12:51:33 -0700

    SND Query Provisioned snapshot automation test (#2315)

--- src/org/labkey/test/BaseWebDriverTest.java
+++ src/org/labkey/test/BaseWebDriverTest.java
@@ -2260,6 +2260,12 @@ public abstract class BaseWebDriverTest extends LabKeySiteWrapper implements Cle
         return new DataRegionTable("query", this);
     }
 
+    public SourceQueryPage editQuerySource(String schemaName, String queryName)
+    {
+        selectQuery(schemaName, queryName);
+        clickAndWait(Locator.linkContainingText("edit source"));
+        return new SourceQueryPage(getDriver());
+    }
     public void editQueryProperties(String schemaName, String queryName)
     {
         selectQuery(schemaName, queryName);
