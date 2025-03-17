41569a8e73febd4f6307f25775abac0f0c2d126b
labkey-danield <danield@labkey.com>
2025-03-17 10:39:31 -0700

    Missed a place.

--- src/org/labkey/test/util/TestDataGenerator.java
+++ src/org/labkey/test/util/TestDataGenerator.java
@@ -369,7 +369,7 @@ public class TestDataGenerator
         // Having double space is allowed in a field name but is a problem for automation.
         // We render double spaces as a single space as column headers in grids and the like, but we maintain the double
         // space in the name. This trips up helpers for various components. See Issue 52193 for details.
-        String randomFieldName = (randomString(numStartChars, null, chars).replace("  ", " ") +
+        String randomFieldName = (randomString(numStartChars, null, chars).replaceAll("\\s\\s+"," ") +
                 part +
                 randomString(numEndChars, null, chars).replaceAll("\\s\\s+"," ")).trim();
         TestLogger.log("Generated random field name: " + randomFieldName);
