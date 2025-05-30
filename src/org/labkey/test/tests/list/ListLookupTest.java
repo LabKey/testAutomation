43e05fa961721688f6f72fbaebebb88e55d33c86
labkey-tchad <tchad@labkey.com>
2025-05-30 12:44:49 -0700

    Fix TSV format Navigate to correct project before each test

--- src/org/labkey/test/tests/list/ListLookupTest.java
+++ src/org/labkey/test/tests/list/ListLookupTest.java
@@ -1,5 +1,6 @@
 package org.labkey.test.tests.list;
 
+import org.apache.commons.csv.CSVFormat;
 import org.jetbrains.annotations.Nullable;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -16,10 +17,12 @@ import org.labkey.test.params.FieldDefinition;
 import org.labkey.test.util.DataRegionTable;
 import org.labkey.test.util.EscapeUtil;
 import org.labkey.test.util.TestDataGenerator;
+import org.labkey.test.util.data.TestDataUtils;
 import org.labkey.test.util.query.QueryApiHelper;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -55,11 +58,12 @@ public class ListLookupTest extends BaseWebDriverTest
         log("Create a list to use as a lookup table with some number-like names.");
         _listHelper.createList(getProjectName(), lookToListName, lookToKeyFieldName,
                 new FieldDefinition(lookToFieldName, FieldDefinition.ColumnType.String));
-        String bulkData = lookToFieldName + "\n" +
-                "1E2\n" +
-                "102\n" +
-                "Lookup\n" +
-                ".123";
+        String bulkData = tsvFromColumn(List.of(
+            lookToFieldName,
+            "1E2",
+            "102",
+            "Lookup",
+            ".123"));
         _listHelper.bulkImportData(bulkData);
 
         DataRegionTable dataRegionTable = new DataRegionTable("query", getDriver());
@@ -85,11 +89,12 @@ public class ListLookupTest extends BaseWebDriverTest
     @Test
     public void testWithoutValidatorOrAlternateKeys() throws IOException, CommandException
     {
+        goToProjectHome();
         setLookupValidatorEnabled(false);
         resetList();
 
         log("Import data into the second list without alternate keys.");
-        String bulkData = lookFromLookupFieldName + "\n" + lookupKeyAsNameNumber;
+        String bulkData = tsvFromColumn(List.of(lookFromLookupFieldName, lookupKeyAsNameNumber));
         _listHelper.clickImportData()
                 .setText(bulkData)
                 .submit();
@@ -102,7 +107,7 @@ public class ListLookupTest extends BaseWebDriverTest
         log("Clean out list before next import.");
         resetList();
         log("Import data into second list without alternate keys supplying invalid primary key");
-        bulkData = lookFromLookupFieldName + "\n1000";
+        bulkData = tsvFromColumn(List.of(lookFromLookupFieldName, "1000"));
         _listHelper.clickImportData()
                 .setText(bulkData)
                 .submit();
@@ -113,7 +118,7 @@ public class ListLookupTest extends BaseWebDriverTest
         validateListValues(expectedData);
 
         log("Check for error if not using alternate key and type does not match");
-        bulkData = lookFromLookupFieldName + "\nnoneSuch";
+        bulkData = tsvFromColumn(List.of(lookFromLookupFieldName, "noneSuch"));
         ImportDataPage importDataPage = _listHelper.clickImportData();
         String error = importDataPage
                 .setText(bulkData)
@@ -124,20 +129,22 @@ public class ListLookupTest extends BaseWebDriverTest
     @Test
     public void testWithoutValidatorWithAlternateKeys() throws IOException, CommandException
     {
+        goToProjectHome();
         setLookupValidatorEnabled(false);
         log("Clean out list before next import.");
         resetList();
         log("Import data into the second list using number-like lookup values expecting alternate keys but also accepting primary keys.");
-        String bulkData = lookFromLookupFieldName + "\n" +
-                "1E2\n" + // valid alternate key looking like a number
-                lookupKeyAsNameNumber + "\n" + // valid alternate key same value as a primary key
-                ".123\n" + // valid alternate key looking like a float
-                "Lookup\n" + // valid alternate key that is a string
-                lookupKeyAsNameNumber + "\n" + // another copy
-                "102\n" + // valid number-like alternate key
-                lookToListValues.get(1).get(EscapeUtil.fieldKeyEncodePart(lookToKeyFieldName)) + "\n" + // primary key value not matching an alternate key
-                "1000" // primary key-type value that doesn't match
-                ;
+        String bulkData = tsvFromColumn(List.of(
+            lookFromLookupFieldName,
+            "1E2", // valid alternate key looking like a number
+            lookupKeyAsNameNumber, // valid alternate key same value as a primary key
+            ".123", // valid alternate key looking like a float
+            "Lookup", // valid alternate key that is a string
+            lookupKeyAsNameNumber, // another copy
+            "102", // valid number-like alternate key
+            lookToListValues.get(1).get(EscapeUtil.fieldKeyEncodePart(lookToKeyFieldName)), // primary key value not matching an alternate key
+            "1000" // primary key-type value that doesn't match
+        ));
         _listHelper.clickImportData()
                 .setText(bulkData)
                 .setImportLookupByAlternateKey(true)
@@ -156,7 +163,7 @@ public class ListLookupTest extends BaseWebDriverTest
         validateListValues(expectedData);
 
         log("Check for error if providing non-matching string value that is not a number");
-        bulkData = lookFromLookupFieldName + "\nNotAValue";
+        bulkData = tsvFromColumn(List.of(lookFromLookupFieldName, "NotAValue"));
         ImportDataPage importDataPage = _listHelper.clickImportData();
         String error = importDataPage
                 .setText(bulkData)
@@ -168,13 +175,14 @@ public class ListLookupTest extends BaseWebDriverTest
     @Test
     public void testWithLookupValidatorWithoutAlternateKeys() throws IOException, CommandException
     {
+        goToProjectHome();
         setLookupValidatorEnabled(true);
         log("Clean out list before next import.");
         resetList();
 
         //   without alternate keys
         log("With lookup validation on, import data into the second list without alternate keys.");
-        String bulkData = lookFromLookupFieldName + "\n" + lookupKeyAsNameNumber;
+        String bulkData = tsvFromColumn(List.of(lookFromLookupFieldName, lookupKeyAsNameNumber));
         _listHelper.clickImportData()
                 .setText(bulkData)
                 .submit();
@@ -187,13 +195,13 @@ public class ListLookupTest extends BaseWebDriverTest
         log("With lookup validation on, import data and provide an invalid primary key.");
         ImportDataPage importDataPage = _listHelper.clickImportData();
         String error = importDataPage
-                .setText(lookFromLookupFieldName + "\n1000")
+                .setText(tsvFromColumn(List.of(lookFromLookupFieldName, "1000")))
                 .submitExpectingError();
-        checker().withScreenshot().verifyEquals("Error message for invalid primary key value not as expected", "Value '1000' was not present in lookup target 'lists." + lookToListName + "' for field '" + lookFromLookupFieldName + "'", error);
+        checker().withScreenshot().verifyEquals("Error message for invalid primary key value not as expected", "Value '1000' was not present in lookup target 'lists." + lookToListName + "' for field '" + FieldDefinition.labelFromName(lookFromLookupFieldName) + "'", error);
 
         log("With lookup validation on, import data and provide an invalid primary key of type string.");
         error = importDataPage
-                .setText(lookFromLookupFieldName + "\nLook")
+                .setText(tsvFromColumn(List.of(lookFromLookupFieldName, "Look")))
                 .submitExpectingError();
         checker().withScreenshot().verifyEquals("Error message for invalid primary key type not as expected", "Could not convert value 'Look' (String) for Integer field '" + lookFromLookupFieldName + "'", error);
     }
@@ -201,20 +209,22 @@ public class ListLookupTest extends BaseWebDriverTest
     @Test
     public void testWithLookupValidatorAndAlternateKeys() throws IOException, CommandException
     {
+        goToProjectHome();
         setLookupValidatorEnabled(true);
         log("Clean out list before next import.");
         resetList();
 
         log("With lookup validation on, import data into the second list using number-like lookup values expecting alternate keys but also accepting primary keys.");
-        String bulkData = lookFromLookupFieldName + "\n" +
-                "1E2\n" + // valid alternate key looking like a number
-                lookupKeyAsNameNumber + "\n" + // valid alternate key same value as a primary key
-                ".123\n" + // valid alternate key looking like a float
-                "Lookup\n" + // valid alternate key that is a string
-                lookupKeyAsNameNumber + "\n" + // another copy
-                "102\n" + // valid number-like alternate key
-                lookToListValues.get(1).get(EscapeUtil.fieldKeyEncodePart(lookToKeyFieldName)) + "\n" // primary key value not matching an alternate key
-        ;
+        String bulkData = tsvFromColumn(List.of(
+            lookFromLookupFieldName,
+            "1E2", // valid alternate key looking like a number
+            lookupKeyAsNameNumber, // valid alternate key same value as a primary key
+            ".123", // valid alternate key looking like a float
+            "Lookup", // valid alternate key that is a string
+            lookupKeyAsNameNumber, // another copy
+            "102", // valid number-like alternate key
+            lookToListValues.get(1).get(EscapeUtil.fieldKeyEncodePart(lookToKeyFieldName)) // primary key value not matching an alternate key
+        ));
         _listHelper.clickImportData()
                 .setText(bulkData)
                 .setImportLookupByAlternateKey(true)
@@ -231,7 +241,7 @@ public class ListLookupTest extends BaseWebDriverTest
         );
         validateListValues(expectedData);
 
-        bulkData = lookFromLookupFieldName + "\nInvalid";
+        bulkData = tsvFromColumn(List.of(lookFromLookupFieldName, "Invalid"));
         ImportDataPage importDataPage = _listHelper.clickImportData();
         String error = importDataPage
                 .setText(bulkData)
@@ -239,12 +249,12 @@ public class ListLookupTest extends BaseWebDriverTest
                 .submitExpectingError();
         checker().withScreenshot().verifyEquals("Error message for invalid string alternate key not as expected", "Value 'Invalid' not found for field " + lookFromLookupFieldName + " in the current context.", error);
 
-        bulkData = lookFromLookupFieldName + "\n1234";
+        bulkData = tsvFromColumn(List.of(lookFromLookupFieldName, "1234"));
         error = importDataPage
                 .setText(bulkData)
                 .setImportLookupByAlternateKey(true)
                 .submitExpectingError();
-        checker().withScreenshot().verifyEquals("Error message for invalid number-like alternate key not as expected", "Value '1234' was not present in lookup target 'lists." + lookToListName + "' for field '" + lookFromLookupFieldName + "'", error);
+        checker().withScreenshot().verifyEquals("Error message for invalid number-like alternate key not as expected", "Value '1234' was not present in lookup target 'lists." + lookToListName + "' for field '" + FieldDefinition.labelFromName(lookFromLookupFieldName) + "'", error);
 
     }
 
@@ -271,6 +281,12 @@ public class ListLookupTest extends BaseWebDriverTest
 
         assertEquals("List data not as expected after action.",
                 expectedValue, actualValue);
+    }
+
+    private String tsvFromColumn(List<String> column)
+    {
+        List<List<String>> rows = column.stream().map(Collections::singletonList).toList();
+        return TestDataUtils.stringFromRows(rows, CSVFormat.TDF);
     }
 
     @Override
