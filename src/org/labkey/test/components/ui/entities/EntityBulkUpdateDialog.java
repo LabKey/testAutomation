5e67b0bfc8678c6c1e4e0ac96c54c6e72eadd07d
cnathe <cnathe@labkey.com>
2025-06-02 15:41:21 -0500

    Issue 52556: remove "-fileUpload" from file field input id

--- src/org/labkey/test/components/ui/entities/EntityBulkUpdateDialog.java
+++ src/org/labkey/test/components/ui/entities/EntityBulkUpdateDialog.java
@@ -244,7 +244,6 @@ public class EntityBulkUpdateDialog extends ModalDialog
      */
     public FileAttachmentContainer getFileField(CharSequence fieldIdentifier)
     {
-        fieldIdentifier = EntityBulkInsertDialog.fileUploadFieldKey(fieldIdentifier);
         return enableAndWait(fieldIdentifier, elementCache().fileUploadField(fieldIdentifier));
     }
 
