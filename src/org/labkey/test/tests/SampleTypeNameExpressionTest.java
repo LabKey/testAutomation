239dbbc25c14be41d886dbfcabed840eadf7aa04
labkey-tchad <tchad@labkey.com>
2025-06-26 13:55:51 -0700

    Reintroduce previous test coverage

--- src/org/labkey/test/tests/SampleTypeNameExpressionTest.java
+++ src/org/labkey/test/tests/SampleTypeNameExpressionTest.java
@@ -245,10 +245,26 @@ public class SampleTypeNameExpressionTest extends BaseWebDriverTest
         expectedNames.add("[" + PARENT_SAMPLE_01 + ", " + PARENT_SAMPLE_02 + ", " + PARENT_SAMPLE_03 + "]-child");
         assertEquals("Sample names are not as expected", expectedNames, names);
 
-        log("Verify import tsv should successfully create derivatives from parent starting with #, as long as values are quoted");
+        log("Verify import tsv should successfully create derivatives from line starting with #, as long as values are quoted");
         data = "MaterialInputs/" + EscapeUtil.fieldKeyEncodePart(PARENT_SAMPLE_TYPE) + "\n"; // fully encoded
         data += "\"" + PARENT_SAMPLE_03 + "\"\n";
-        data += "\"" + PARENT_SAMPLE_03 + "," + PARENT_SAMPLE_02 + "\"\n";
+
+        sampleHelper.bulkImport(data);
+
+        names = materialTable.getColumnDataAsText("Name");
+        Collections.reverse(names);
+
+        log("generated sample names:");
+        names.forEach(this::log);
+
+        assertEquals(3, names.size());
+
+        expectedNames.add(PARENT_SAMPLE_03 + "-child");
+        assertEquals("Sample names are not as expected", expectedNames, names);
+
+        log("Verify import tsv should successfully create derivatives from parent starting with #, as long as this is not the 1st field in the row");
+        data = "Description\tMaterialInputs/" + EscapeUtil.fieldKeyEncodePart(PARENT_SAMPLE_TYPE) + "\n"; // fully encoded
+        data += "Parents with leading # should work\t" + PARENT_SAMPLE_03 + "," + PARENT_SAMPLE_02 + "\n";
 
         sampleHelper.bulkImport(data);
 
@@ -260,7 +276,6 @@ public class SampleTypeNameExpressionTest extends BaseWebDriverTest
 
         assertEquals(4, names.size());
 
-        expectedNames.add(PARENT_SAMPLE_03 + "-child");
         expectedNames.add("[" + PARENT_SAMPLE_03 + ", " + PARENT_SAMPLE_02 + "]-child");
         assertEquals("Sample names are not as expected", expectedNames, names);
 
