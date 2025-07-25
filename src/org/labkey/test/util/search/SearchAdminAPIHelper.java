dd0053f2cd684950563f7137bec8893c7adf29ff
labkey-jeckels <jeckels@labkey.com>
2025-07-25 15:48:34 -0700

    Revamp priorities

--- src/org/labkey/test/util/search/SearchAdminAPIHelper.java
+++ src/org/labkey/test/util/search/SearchAdminAPIHelper.java
@@ -78,7 +78,7 @@ public abstract class SearchAdminAPIHelper
         // Invoke a special server action that waits until all previous indexer tasks are complete, even wait for background indexing tasks to complete (e.g. deleteContainer)
         var cmd = new SimplePostCommand("search", "waitForIndexer");
         cmd.setTimeout(timeout);
-        cmd.setParameters(Map.of("priority", "background"));
+        cmd.setParameters(Map.of("priority", "crawlLow"));
 
         executeCommand(cmd, null);
     }
