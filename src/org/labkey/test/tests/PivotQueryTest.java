cd13d8dc00e7c2a2ada2d85f340b72b9ef351306
ChrisJoosse <chrisj@labkey.com>
2025-07-29 11:24:30 -0700

    coverage for Issue 52739

--- src/org/labkey/test/tests/PivotQueryTest.java
+++ src/org/labkey/test/tests/PivotQueryTest.java
@@ -26,7 +26,12 @@ import org.labkey.test.categories.Daily;
 import org.labkey.test.categories.Data;
 import org.labkey.test.components.ChartTypeDialog;
 import org.labkey.test.pages.study.DatasetDesignerPage;
+import org.labkey.test.params.FieldDefinition;
 import org.labkey.test.util.DataRegionTable;
+import org.labkey.test.util.DomainUtils;
+import org.labkey.test.util.EscapeUtil;
+import org.labkey.test.util.TestDataGenerator;
+import org.labkey.test.util.data.TestDataUtils;
 
 import java.io.File;
 import java.util.Arrays;
@@ -180,6 +185,54 @@ public class PivotQueryTest extends ReportTest
 
         //Confirm warning message indicating a non-numeric value could not be used.
         assertTextPresent("The y-axis measure '" + MEASURE_COLUMN + "' had 1 value(s) that could not be converted to a number and are not included in the plot");
+    }
+
+    // coverage for Issue 52739
+    @Test
+    public void testBadPivotQuery()
+    {
+        String datasetName = TestDataGenerator.randomDomainName("D2", DomainUtils.DomainKind.StudyDatasetVisit);
+        String textFieldName = TestDataGenerator.randomFieldName("F1", ":,;'\"");
+        FieldDefinition textField = new FieldDefinition(textFieldName, FieldDefinition.ColumnType.String);
+        goToProjectHome();
+
+        var datasetDesigner = _studyHelper.defineDataset(datasetName, getProjectName());
+        datasetDesigner.getFieldsPanel().addFields(List.of(textField));
+        var viewDatasetPage = datasetDesigner.clickSave()
+                .clickViewData();
+        TestDataUtils.TsvQuoter _tsvQuoter = new TestDataUtils.TsvQuoter();
+        String bulkData = """
+                ParticipantId	date	[F1]
+                1	7/28/2025	this
+                2	7/29/2025	that
+                3	7/30/2025	the other
+                4	7/31/2025	and more
+                5	8/1/2025	but wait
+                6	8/2/2025	still more
+                """.replace("[F1]", _tsvQuoter.quoteValue(textFieldName));
+        var importPage = viewDatasetPage.getDataRegion().clickImportBulkData();
+        importPage.setText(bulkData);
+        importPage.submit();
+
+        // configure the query
+        String queryName = "Q1";
+        String queryText = """
+                SELECT ParticipantId, SequenceNum, MAX([F1]) AS I1Max, [F1] FROM study.[D2]
+                GROUP BY ParticipantId, SequenceNum, [F1]
+                PIVOT I1Max BY [F1]
+                """.replace("[F1]", EscapeUtil.getSqlQuotedValue(textFieldName))
+                .replace("[D2]", EscapeUtil.getSqlQuotedValue(datasetName));
+
+        goToModule("Query");
+        var createQueryPage = createNewQuery("study", datasetName);
+        createQueryPage.setName(queryName);
+        var sourceQueryPage = createQueryPage.clickCreate();
+        sourceQueryPage.setSource(queryText);
+        var executeQueryPage = sourceQueryPage.clickSaveAndFinish();
+
+        // ensure query results contain F1 contents
+        executeQueryPage.getDataRegion();
+        assertTextPresent("this", "that", "the other", "and more", "but wait", "still more");
     }
 
     @Override public BrowserType bestBrowser()
