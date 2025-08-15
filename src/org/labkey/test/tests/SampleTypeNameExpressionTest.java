38aceec2f103caa71f26ad3a405a4d02f2f6db9a
Dan Duffek <danield@labkey.com>
2025-08-15 13:35:22 -0700

    Test Fixes for LKS Sample Type Test (#2634)

--- src/org/labkey/test/tests/SampleTypeNameExpressionTest.java
+++ src/org/labkey/test/tests/SampleTypeNameExpressionTest.java
@@ -73,8 +73,9 @@ public class SampleTypeNameExpressionTest extends BaseWebDriverTest
 {
     private static final String PROJECT_NAME = "SampleType_Name_Expression_Test";
 
+    // Issue 53548: Naming Pattern with a default value containing a () or {} cannot be saved.
     private static final String DEFAULT_SAMPLE_PARENT_VALUE = "SS" +
-            EscapeUtil.escapeForNameExpression(TestDataGenerator.randomString(3));
+            EscapeUtil.escapeForNameExpression(TestDataGenerator.randomString(3, "{}()_"));
 
     private static final String PARENT_SAMPLE_TYPE = "PS" + DOMAIN_TRICKY_CHARACTERS;
     private static final String PARENT_SAMPLE_TYPE_INPUT = escapeForNameExpression(PARENT_SAMPLE_TYPE);
