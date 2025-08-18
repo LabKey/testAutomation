3a1bb0b32905047779d647e28ad02e40662766e3
Dan Duffek <danield@labkey.com>
2025-08-18 10:39:22 -0700

    BackPort sample type test fix (#2636)

--- src/org/labkey/test/tests/SampleTypeNameExpressionTest.java
+++ src/org/labkey/test/tests/SampleTypeNameExpressionTest.java
@@ -71,7 +71,10 @@ import static org.labkey.test.util.EscapeUtil.escapeForNameExpression;
 public class SampleTypeNameExpressionTest extends BaseWebDriverTest
 {
     private static final String PROJECT_NAME = "SampleType_Name_Expression_Test";
-    private static final String DEFAULT_SAMPLE_PARENT_VALUE = "SS" + TestDataGenerator.randomString(3).replaceAll("[_)]", "."); // '_' is used as delimiter to get batchRandomId and ) is used to close the defaultValue()
+
+    // Issue 53548: Naming Pattern with a default value containing a () or {} cannot be saved.
+    private static final String DEFAULT_SAMPLE_PARENT_VALUE = "SS" +
+            EscapeUtil.escapeForNameExpression(TestDataGenerator.randomString(3, "{}()_"));
 
     private static final String PARENT_SAMPLE_TYPE = "PS" + DOMAIN_TRICKY_CHARACTERS;
     private static final String PARENT_SAMPLE_TYPE_INPUT = escapeForNameExpression(PARENT_SAMPLE_TYPE);
