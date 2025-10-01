75707c649bbfc3923584f2d2bb693a37d4d72a68
labkey-susanh <susanh@labkey.com>
2025-10-01 10:51:08 -0700

    Add randomFileName method

--- src/org/labkey/test/TestFileUtils.java
+++ src/org/labkey/test/TestFileUtils.java
@@ -40,6 +40,7 @@ import org.bouncycastle.openpgp.operator.jcajce.JcaPGPDigestCalculatorProviderBu
 import org.bouncycastle.openpgp.operator.jcajce.JcePBEDataDecryptorFactoryBuilder;
 import org.bouncycastle.util.io.Streams;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.openqa.selenium.NotFoundException;
 
 import java.io.BufferedInputStream;
@@ -75,6 +76,10 @@ import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+
+import static org.labkey.test.util.TestDataGenerator.CHARSET_STRING;
+import static org.labkey.test.util.TestDataGenerator.randomInt;
+import static org.labkey.test.util.TestDataGenerator.randomName;
 
 /**
  * Static methods for finding and reading test-related files
@@ -727,5 +732,18 @@ public abstract class TestFileUtils
     public static String makeLegalFileName(String candidate)
     {
         return badChars.matcher(candidate).replaceAll("_");
+    }
+
+    public static String randomFileName(@NotNull String part, @Nullable String extension)
+    {
+        return randomFileName(part, extension, null, null);
+    }
+
+    public static String randomFileName(@NotNull String part, @Nullable String extension, @Nullable Integer numStartChars, @Nullable Integer numEndChars)
+    {
+        String baseName = makeLegalFileName(randomName(part, numStartChars == null ? randomInt(0, 5) : numStartChars, numEndChars == null ? randomInt(0, 5) : numEndChars, CHARSET_STRING, null).name());
+        if (extension != null)
+            return baseName + extension;
+        return baseName;
     }
 }
