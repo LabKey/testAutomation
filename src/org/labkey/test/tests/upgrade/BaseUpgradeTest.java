97418419338c6b8053e93c1be579194d68642dca
labkey-tchad <tchad@labkey.com>
2025-10-09 14:47:45 -0700

    Add comments and fix version range check

--- src/org/labkey/test/tests/upgrade/BaseUpgradeTest.java
+++ src/org/labkey/test/tests/upgrade/BaseUpgradeTest.java
@@ -10,15 +10,33 @@ import org.junit.runners.model.Statement;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.TestProperties;
 import org.labkey.test.util.TestLogger;
+import org.labkey.test.util.Version;
+import org.labkey.test.util.VersionRange;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static org.apache.commons.lang3.StringUtils.trimToNull;
+
+/**
+ * Base test class for tests that setup data and configure a server then verify the persistence or modification of those
+ * data and configurations after upgrading to a newer version of LabKey.<br>
+ * The {@code EariestVersion} and {@code LatestVersion} annotations can be used to skip particular tests when they are
+ * not relevant to the version of LabKey being upgraded from (specified in the {@code webtest.upgradePreviousVersion}
+ * system property).<br>
+ * The setup steps will be skipped if the {@code webtest.upgradeSetup} system property is set to {@code false}.
+ */
 public abstract class BaseUpgradeTest extends BaseWebDriverTest
 {
 
     protected static final boolean isUpgradeSetupPhase = TestProperties.getBooleanProperty("webtest.upgradeSetup", true);
-    protected static final double previousVersion = TestProperties.getDoubleProperty("webtest.upgradePreviousVersion", 0.0);
+    protected static final Version previousVersion = Optional.ofNullable(trimToNull(System.getProperty("webtest.upgradePreviousVersion")))
+        .map(Version::new).orElse(null);
 
     @Override
     protected boolean skipCleanup(boolean afterTest)
@@ -52,12 +70,26 @@ public abstract class BaseUpgradeTest extends BaseWebDriverTest
         return Arrays.asList();
     }
 
+    /**
+     * Annotates test methods that should only run when upgrading from particular LabKey versions, as specified in
+     * {@code webtest.upgradePreviousVersion}.<br>
+     * Specifies the earliest version of the test class that performed the required setup for the annotated method.
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.METHOD})
     protected @interface EariestVersion {
-        double value();
+        String value();
     }
 
+    /**
+     * Annotates test methods that should only run when upgrading from particular LabKey versions, as specified in
+     * {@code webtest.upgradePreviousVersion}.<br>
+     * Specifies the latest version of the test class that performed the required setup for the annotated method.
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.METHOD})
     protected @interface LatestVersion {
-        double value();
+        String value();
     }
 
     private static class UpgradeVersionCheck implements TestRule
@@ -66,9 +98,12 @@ public abstract class BaseUpgradeTest extends BaseWebDriverTest
         @Override
         public @NotNull Statement apply(Statement base, Description description)
         {
-            EariestVersion eariestVersion = description.getAnnotation(EariestVersion.class);
-            LatestVersion latestVersion = description.getAnnotation(LatestVersion.class);
-            if (isUpgradeSetupPhase || previousVersion <= 0)
+            String eariestVersion = Optional.ofNullable(description.getAnnotation(EariestVersion.class))
+                .map(EariestVersion::value).orElse(null);
+            String latestVersion = Optional.ofNullable(description.getAnnotation(LatestVersion.class))
+                .map(LatestVersion::value).orElse(null);
+
+            if (isUpgradeSetupPhase || previousVersion == null || (eariestVersion == null && latestVersion == null))
             {
                 return base; // Run the test normally
             }
@@ -79,8 +114,7 @@ public abstract class BaseUpgradeTest extends BaseWebDriverTest
                 public void evaluate() throws Throwable
                 {
                     Assume.assumeTrue("Test doesn't support upgrading from version: " + previousVersion,
-                        (eariestVersion == null || eariestVersion.value() <= previousVersion) &&
-                            (latestVersion == null || previousVersion <= latestVersion.value())
+                        VersionRange.versionRange(eariestVersion, latestVersion).contains(previousVersion)
                     );
                     base.evaluate();
                 }
