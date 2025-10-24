98d97439f6609fde2091261781f363951fb52634
cnathe <cnathe@labkey.com>
2025-10-24 10:37:15 -0500

    make TestDataGenerator.randomBoolean() static

--- src/org/labkey/test/util/TestDataGenerator.java
+++ src/org/labkey/test/util/TestDataGenerator.java
@@ -469,7 +469,7 @@ public class TestDataGenerator
                     case "double":
                         return () -> randomDouble(0, 20);
                     case "boolean":
-                        return this::randomBoolean;
+                        return TestDataGenerator::randomBoolean;
                     case "date":
                     case "datetime":
                         return () -> randomDateString(DateUtils.addWeeks(new Date(), -39), new Date());
@@ -797,7 +797,7 @@ public class TestDataGenerator
         return new SimpleDateFormat(dateFormat).format(date);
     }
 
-    public boolean randomBoolean()
+    public static boolean randomBoolean()
     {
         return ThreadLocalRandom.current().nextBoolean();
     }
