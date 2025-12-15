16912e4712f00973103dbd9ea78a0a8fa31d8990
labkey-danield <danield@labkey.com>
2025-12-15 11:47:40 -0800

    Remove the code that finds and clicks the row label in CustomizeView.addItem.

--- src/org/labkey/test/components/CustomizeView.java
+++ src/org/labkey/test/components/CustomizeView.java
@@ -391,8 +391,6 @@ public class CustomizeView extends WebDriverComponent<CustomizeView.Elements>
         // Expand all nodes necessary to reveal the desired node.
         WebElement fieldRow = expandPivots(fieldKey);
         WebElement checkbox = Locator.css("input[type=button]").findElement(fieldRow);
-        WebElement rowLabel = Locator.byClass("x4-tree-node-text").findElement(fieldRow);
-        rowLabel.click();
         ScrollUtils.scrollIntoView(checkbox);
         new Checkbox(checkbox).check();
         itemXPath(type, fieldKey).waitForElement(this, 2_000);
