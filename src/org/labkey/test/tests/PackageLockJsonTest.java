06e1b49d464da1efab50ee6bcf0463543b3792c6
labkey-tchad <tchad@labkey.com>
2026-01-19 15:24:45 -0800

    Scan all package.json files for URL dependencies

--- src/org/labkey/test/tests/PackageLockJsonTest.java
+++ src/org/labkey/test/tests/PackageLockJsonTest.java
@@ -11,24 +11,90 @@ import org.labkey.test.TestFileUtils;
 import org.labkey.test.util.TestLogger;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
 import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 @Category({})
 public class PackageLockJsonTest
 {
     private static final Set<String> ALLOWED_SOURCES = Set.of("registry.npmjs.org", "labkey.jfrog.io");
+    private static final Pattern STARTS_WITH_ALPHA = Pattern.compile("^[a-zA-Z]");
     private final Map<String, AtomicInteger> depCounts = new HashMap<>();
     private final List<String> badSources = new ArrayList<>();
 
     @Test
-    public void test() throws Exception
+    public void packageJsonTest() throws Exception
+    {
+        Path modulesDir = new File(TestFileUtils.getLabKeyRoot(), "server/modules").toPath();
+        PathMatcher packageJsonMatcher = FileSystems.getDefault().getPathMatcher("glob:**/package.json");
+
+        try (Stream<Path> paths = Files.walk(modulesDir))
+        {
+            paths
+                .filter(path -> !path.toString().contains("/.gradle/"))
+                .filter(packageJsonMatcher::matches)
+                .forEach(this::scanPackageJson);
+        }
+        Assert.assertTrue("Bad sources: " + String.join("\n", badSources), badSources.isEmpty());
+    }
+
+    public void scanPackageJson(Path packageJson)
+    {
+        TestLogger.log("Scanning " + packageJson);
+
+        try (InputStream is = Files.newInputStream(packageJson))
+        {
+            JSONObject jsonObject = new JSONObject(new JSONTokener(is));
+            JSONObject dependencies = jsonObject.optJSONObject("dependencies");
+            JSONObject devDependencies = jsonObject.optJSONObject("devDependencies");
+            if (dependencies != null)
+            {
+                dependencies.keySet().forEach(key -> {
+                    String val = dependencies.getString(key);
+                    if (STARTS_WITH_ALPHA.matcher(val).matches())
+                    {
+                        badSources.add(key + " = " + val + " - " + packageJson);
+                    }
+                });
+            }
+            if (devDependencies != null)
+            {
+                devDependencies.keySet().forEach(key -> {
+                    String val = devDependencies.getString(key);
+                    if (STARTS_WITH_ALPHA.matcher(val).matches())
+                    {
+                        badSources.add(key + " = " + val + " - " + packageJson);
+                    }
+                });
+            }
+        }
+        catch (JSONException ex)
+        {
+            TestLogger.log("  JSONException: " + ex.getMessage());
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("Error reading " + packageJson, e);
+        }
+
+    }
+
+    @Test
+    public void testPackageLockJson() throws Exception
     {
         File modulesDir = new File(TestFileUtils.getLabKeyRoot(), "server/modules");
         File[] files = modulesDir.listFiles();
