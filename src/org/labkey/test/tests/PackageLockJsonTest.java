9901db0c0b164f094293d2de093cfd0c8007f616
labkey-tchad <tchad@labkey.com>
2026-01-28 12:10:58 -0800

    Update PackageLockJsonTest to be parameterized

--- src/org/labkey/test/tests/PackageLockJsonTest.java
+++ src/org/labkey/test/tests/PackageLockJsonTest.java
@@ -5,98 +5,49 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.labkey.serverapi.reader.Readers;
 import org.labkey.test.TestFileUtils;
 import org.labkey.test.util.TestLogger;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.Reader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.PathMatcher;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 @Category({})
+@RunWith(Parameterized.class)
 public class PackageLockJsonTest
 {
     private static final Set<String> ALLOWED_SOURCES = Set.of("registry.npmjs.org", "labkey.jfrog.io");
+    // Allow-list of '@isaacs/cliui' dependencies
+    private static final Set<String> ALLOWED_NONSTANDARD_VERSIONS = Set.of("npm:string-width@^4.2.0", "npm:strip-ansi@^6.0.1", "npm:wrap-ansi@^7.0.0");
     private final Map<String, AtomicInteger> depCounts = new HashMap<>();
     private final List<String> errors = new ArrayList<>();
 
-    @Test @Ignore("package-lock check seems sufficient")
-    public void packageJsonTest() throws Exception
-    {
-        Path modulesDir = new File(TestFileUtils.getLabKeyRoot(), "server/modules").toPath();
-        PathMatcher packageJsonMatcher = FileSystems.getDefault().getPathMatcher("glob:**/package.json");
+    private final File moduleDir;
 
-        try (Stream<Path> paths = Files.walk(modulesDir))
-        {
-            paths
-                .filter(path -> !path.toString().contains("/.gradle/"))
-                .filter(packageJsonMatcher::matches)
-                .forEach(this::scanPackageJson);
-        }
-        Assert.assertTrue("Bad sources: " + String.join("\n", errors), errors.isEmpty());
+    public PackageLockJsonTest(File moduleDir, String name)
+    {
+        this.moduleDir = moduleDir;
     }
 
-    public void scanPackageJson(Path packageJson)
-    {
-        TestLogger.log("Scanning " + packageJson);
+    @Parameterized.Parameters(name = "{1}")
+    public static Collection<Object[]> data() {
+        List<File> allModules = new ArrayList<>();
 
-        try (InputStream is = Files.newInputStream(packageJson))
-        {
-            JSONObject jsonObject = new JSONObject(new JSONTokener(is));
-            JSONObject dependencies = jsonObject.optJSONObject("dependencies");
-            JSONObject devDependencies = jsonObject.optJSONObject("devDependencies");
-            if (dependencies != null)
-            {
-                dependencies.keySet().forEach(key -> {
-                    String val = dependencies.getString(key);
-                    if (val.contains(":")) // URL, file, or workspace dependency
-                    {
-                        errors.add(key + " = " + val + " - " + packageJson);
-                    }
-                });
-            }
-            if (devDependencies != null)
-            {
-                devDependencies.keySet().forEach(key -> {
-                    String val = devDependencies.getString(key);
-                    if (val.contains(":")) // URL, file, or workspace dependency
-                    {
-                        errors.add(key + " = " + val + " - " + packageJson);
-                    }
-                });
-            }
-        }
-        catch (JSONException ex)
-        {
-            TestLogger.log("  JSONException: " + ex.getMessage());
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException("Error reading " + packageJson, e);
-        }
-
-    }
-
-    @Test
-    public void testPackageLockJson() throws Exception
-    {
         File modulesDir = new File(TestFileUtils.getLabKeyRoot(), "server/modules");
         File[] files = modulesDir.listFiles();
         if (files == null)
@@ -108,58 +59,55 @@ public class PackageLockJsonTest
             if (file.isDirectory())
             {
                 if (new File(file, "module.properties").exists())
-                    testModule(file);
+                {
+                    allModules.add(file);
+                }
                 else
-                    testModuleContainer(file);
+                {
+                    allModules.addAll(Arrays.stream(Objects.requireNonNull(file.listFiles(), () -> "No files found in " + file.getAbsolutePath()))
+                        .filter(f -> f.isDirectory() && new File(f, "module.properties").isFile()).toList());
+                }
             }
         }
-        TestLogger.log("Package lock JSON dependencies:" + depCounts);
-        Assert.assertTrue("Bad sources: " + errors, errors.isEmpty());
+
+        return allModules.stream().filter(file -> new File(file, "package-lock.json").exists())
+            .map(f -> new Object[]{f, f.getName()}).toList();
     }
 
-    private void testModuleContainer(File moduleContainer) throws Exception
+    @Test
+    public void testPackageLock() throws Exception
     {
-        File[] modules = moduleContainer.listFiles();
-        if (modules == null)
-        {
-            throw new RuntimeException("No files found in modules directory: " + moduleContainer.getAbsolutePath());
-        }
-        for (File module : modules)
-        {
-            if (module.isDirectory() && new File(module, "module.properties").isFile())
-            {
-                testModule(module);
-            }
-        }
+        testModule(moduleDir);
+        Assert.assertTrue("Bad sources: " + errors, errors.isEmpty());
     }
 
     private void testModule(File module) throws Exception
     {
         File packageLock = new File(module, "package-lock.json");
-        if (packageLock.isFile())
-        {
-            TestLogger.log("Testing module: " + module.getAbsolutePath());
+        Assert.assertTrue("No package-lock.json found in module: " + module.getAbsolutePath(), packageLock.isFile()); // Should be unfailable
 
-            JSONObject packages;
-            try (Reader reader = Readers.getReader(packageLock))
+        TestLogger.log("Testing module: " + module.getAbsolutePath());
+
+        JSONObject packages;
+        try (Reader reader = Readers.getReader(packageLock))
+        {
+            JSONObject jsonObject = new JSONObject(new JSONTokener(reader));
+            packages = jsonObject.optJSONObject("packages");
+            if (packages == null)
+                packages = jsonObject.getJSONObject("dependencies"); // old lockfile version
+        }
+        catch (JSONException e)
+        {
+            TestLogger.error("Testing module: " + module.getName() + " failed to parse package-lock.json: " + e.getMessage());
+            return;
+        }
+
+        for (String packageName : packages.keySet())
+        {
+            if (!packageName.isBlank())
             {
-                JSONObject jsonObject = new JSONObject(new JSONTokener(reader));
-                packages = jsonObject.optJSONObject("packages");
-                if (packages == null)
-                    packages = jsonObject.getJSONObject("dependencies"); // old lockfile version
-            }
-            catch (JSONException e)
-            {
-                TestLogger.error("Testing module: " + module.getName() + " failed to parse package-lock.json: " + e.getMessage());
-                return;
-            }
-            for (String packageName : packages.keySet())
-            {
-                if (!packageName.isBlank())
-                {
-                    JSONObject packageJson = packages.getJSONObject(packageName);
-                    verifyPackage(packageName, packageJson, packageLock);
-                }
+                JSONObject packageJson = packages.getJSONObject(packageName);
+                verifyPackage(packageName, packageJson, packageLock);
             }
         }
     }
@@ -182,7 +130,7 @@ public class PackageLockJsonTest
                 errors.add(message);
                 TestLogger.error(message);
             }
-            depCounts.computeIfAbsent(host, k -> new AtomicInteger()).incrementAndGet();
+            depCounts.computeIfAbsent(host, _ -> new AtomicInteger()).incrementAndGet();
         }
 
         String version = packageJson.optString("version");
@@ -199,12 +147,12 @@ public class PackageLockJsonTest
             JSONObject packageJsonDep = transitiveDeps.optJSONObject(tDep);
             if (packageJsonDep != null)
             {
-                verifyPackage(tDep, packageJsonDep, packageLock);
+                verifyPackage(tDep, packageJsonDep, packageLock); // belt and suspenders
             }
             else
             {
                 String tVer = transitiveDeps.optString(tDep);
-                if (tVer == null || tVer.contains(":") && !tVer.startsWith("npm:")) // URL, file, or workspace dependency
+                if (tVer == null || tVer.contains(":") && !ALLOWED_NONSTANDARD_VERSIONS.contains(tVer)) // URL, file, or workspace dependency
                 {
                     String message = "Package " + packageName + " has bad transitive dependency [" + tVer + "] in " + packageLock.getAbsolutePath();
                     errors.add(message);
