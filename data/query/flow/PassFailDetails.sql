f3e3349704088030fd397bad26dfabbf478a7f87
Adam Rauch <adam@labkey.com>
2026-05-26 15:15:09 -0700

    Update copyrights and licenses

--- data/query/flow/PassFailDetails.sql
+++ data/query/flow/PassFailDetails.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2020-2026 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 SELECT FCSAnalyses.RowId,
 FCSAnalyses.Run.Name||'-' ||FCSAnalyses.FCSFile.Keyword."Sample Order" AS Key,
 FCSAnalyses.FCSFile.Keyword.Stim AS Stim,
