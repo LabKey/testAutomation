7d3a9242930d56df19a3806b249e4266fa3dccea
labkey-tchad <tchad@labkey.com>
2026-07-22 14:44:30 -0700

    Remove pointless annotations

--- src/org/labkey/test/util/AbstractUserHelper.java
+++ src/org/labkey/test/util/AbstractUserHelper.java
@@ -144,8 +144,7 @@ public abstract class AbstractUserHelper
     }
 
     public abstract void ensureUsersExist(List<String> userEmails);
-    @LogMethod (quiet = true)
-    public abstract CreateUserResponse createUser(@LoggedParam String userName, boolean sendEmail, boolean verifySuccess);
+    public abstract CreateUserResponse createUser(String userName, boolean sendEmail, boolean verifySuccess);
     public abstract String setInitialPassword(int userId);
     protected abstract void _deleteUser(String userEmail);
     protected abstract void _deleteUsers(boolean failIfNotFound, String... userEmails);
