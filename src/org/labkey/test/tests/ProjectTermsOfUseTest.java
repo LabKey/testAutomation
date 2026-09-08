059ab62bfdeb4195dc6e6ad12e01efade602da5e
labkey-danield <danield@labkey.com>
2026-09-07 18:25:48 -0700

    Add check for 'Agreed to terms of use' audit log entries.

--- src/org/labkey/test/tests/ProjectTermsOfUseTest.java
+++ src/org/labkey/test/tests/ProjectTermsOfUseTest.java
@@ -17,14 +17,28 @@ package org.labkey.test.tests;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.labkey.remoteapi.CommandException;
+import org.labkey.remoteapi.Connection;
+import org.labkey.remoteapi.query.Filter;
+import org.labkey.remoteapi.query.SelectRowsCommand;
+import org.labkey.remoteapi.query.SelectRowsResponse;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.Locator;
 import org.labkey.test.Locators;
 import org.labkey.test.WebTestHelper;
 import org.labkey.test.categories.Daily;
 import org.labkey.test.util.Maps;
+import org.labkey.test.util.PasswordUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.labkey.api.security.UserManager.USER_AUDIT_EVENT;
 
 @Category({Daily.class})
 @BaseWebDriverTest.ClassTimeout(minutes = 6)
@@ -40,7 +54,7 @@ public class ProjectTermsOfUseTest extends BaseTermsOfUseTest
     }
 
     @Test
-    public void projectTermsOfUseTest()
+    public void projectTermsOfUseTest() throws IOException, CommandException
     {
         log("Terms don't come into play until you log out");
         clickProject(NON_PUBLIC_TERMS_PROJECT2_NAME);
@@ -121,6 +135,29 @@ public class ProjectTermsOfUseTest extends BaseTermsOfUseTest
         clickAndWait(Locator.linkWithText("Edit"));
         deleteWikiPage();
         assertTextNotPresent(WIKI_TERMS_TITLE);
+
+        log("Validate the audit logs show 'agreement to terms' entry.");
+
+        // No terms for this project.
+        getAuditLog("/" + PUBLIC_NO_TERMS_PROJECT_NAME, new ArrayList<>());
+
+        // The remaining three projects should have the same audit log entries.
+        int defaultUserId = _userHelper.getUserId(PasswordUtil.getUsername());
+        int impersonatedUserId = _userHelper.getUserId(USER);
+        List<Map<String, Object>> expected = new ArrayList<>();
+        expected.add(Map.of("CreatedBy", impersonatedUserId,
+                "User", impersonatedUserId,
+                "ImpersonatedBy", defaultUserId));
+        Map<String, Object> row = new HashMap<>();
+        row.put("CreatedBy", defaultUserId);
+        row.put("User", defaultUserId);
+        row.put("ImpersonatedBy", null);
+        expected.add(row);
+        expected.add(row);
+
+        getAuditLog("/" + PUBLIC_TERMS_PROJECT_NAME, expected);
+        getAuditLog("/" + NON_PUBLIC_TERMS_PROJECT2_NAME, expected);
+        getAuditLog("/" + NON_PUBLIC_TERMS_PROJECT_NAME, expected);
     }
 
     protected void deleteWikiPage()
@@ -129,5 +166,38 @@ public class ProjectTermsOfUseTest extends BaseTermsOfUseTest
         clickButton("Delete Page");
         clickButton("Delete");
     }
+
+    private void getAuditLog(String path, List<Map<String, Object>> expected) throws IOException, CommandException
+    {
+        Connection cn = new Connection(WebTestHelper.getBaseURL(), PasswordUtil.getUsername(), PasswordUtil.getPassword());
+
+        SelectRowsCommand cmd = new SelectRowsCommand("auditLog", USER_AUDIT_EVENT);
+        cmd.setColumns(List.of("CreatedBy", "ImpersonatedBy", "User", "Comment"));
+        cmd.addFilter("Comment", "Agreed to terms of use", Filter.Operator.EQUAL);
+        SelectRowsResponse response = cmd.execute(cn, path);
+
+        List<Map<String, Object>> actualLog = response.getRows();
+
+        checker().verifyEquals("Number of audit log entries not as expected.",
+                expected.size(), actualLog.size());
+
+        List<Map<String, Object>> unmatched = expected.stream()
+                .filter(expectedRow -> {
+                    Set<String> keysOfInterest = expectedRow.keySet();
+                    return actualLog.stream().noneMatch(actualRow -> {
+                        Map<String, Object> actualFiltered = actualRow.entrySet().stream()
+                                .filter(e -> keysOfInterest.contains(e.getKey()))
+                                .collect(HashMap::new,
+                                        (m, e) -> m.put(e.getKey(), e.getValue()),
+                                        HashMap::putAll);
+                        return actualFiltered.equals(expectedRow);
+                    });
+                })
+                .toList();
+
+        checker().verifyTrue("Expected rows with no match in actual: " + unmatched, unmatched.isEmpty());
+
+    }
+
 }
 
